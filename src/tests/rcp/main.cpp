e00d52b9c2b46bb8453bcb6dea894f1492a8f198
Ondrej Certik <ondrej.certik@gmail.com>
2012-07-10 04:00:18 -0700

    Add a simple test for RCP
    The purpose of this test is to make sure the all the compiling and linking
    works and that there are no missing symbols.

---
+++ src/tests/rcp/main.cpp
@@ -0,0 +1,21 @@
+#include "Teuchos_RCP.hpp"
+
+using Teuchos::RCP;
+using Teuchos::Ptr;
+using Teuchos::rcp;
+using Teuchos::null;
+
+class Mesh {
+    int x, y;
+};
+
+int main(int argc, char* argv[])
+{
+
+    RCP<Mesh> m = rcp(new Mesh());
+    Ptr<Mesh> p = m.ptr();
+    if (m == null) return 1;
+    if (p == null) return 1;
+
+    return 0;
+}
