038b91bca24578d20939789acc2a639830527a4c
Ondrej Certik <ondrej.certik@gmail.com>
2012-07-11 12:55:49 -0700

    Test ==

--- src/tests/basic/main.cpp
+++ src/tests/basic/main.cpp
@@ -13,13 +13,17 @@ using CSymPy::Symbol;
 
 void test_symbol_hash()
 {
-    RCP<Symbol> x  = rcp(new Symbol("x"));
-    RCP<Symbol> x2 = rcp(new Symbol("x"));
-    RCP<Symbol> y  = rcp(new Symbol("y"));
+    Symbol x  = Symbol("x");
+    Symbol x2 = Symbol("x");
+    Symbol y  = Symbol("y");
+
+    assert(x == x);
+    assert(x == x2);
+    assert(!(x == y));
 
     std::hash<Basic> hash_fn;
-    assert(hash_fn(*x) != hash_fn(*y));
-    assert(hash_fn(*x) == hash_fn(*x2));
+    assert(hash_fn(x) != hash_fn(y));
+    assert(hash_fn(x) == hash_fn(x2));
 
 
     std::size_t seed1 = 0;
@@ -27,8 +31,8 @@ void test_symbol_hash()
     hash_combine<std::string>(seed1, "y");
 
     std::size_t seed2 = 0;
-    hash_combine<Basic>(seed2, *x);
-    hash_combine<Basic>(seed2, *y);
+    hash_combine<Basic>(seed2, x);
+    hash_combine<Basic>(seed2, y);
 
     assert(seed1 == seed2);
 }
@@ -37,8 +41,17 @@ int main(int argc, char* argv[])
 {
     test_symbol_hash();
 
+    /*
     std::unordered_map<int, int> m;
     RCP<Add> a = rcp(new Add(m));
+    */
+
+    /*
+    std::unordered_map<Symbol, int> d;
+    RCP<Symbol> x  = rcp(new Symbol("x"));
+    RCP<Symbol> y  = rcp(new Symbol("y"));
+    d[*x] = 2;
+    */
 
     return 0;
 }
