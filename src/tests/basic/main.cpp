68beaa4e12f4c28d7d4e525c4cc8fc8826fd4dfb
Ondrej Certik <ondrej.certik@gmail.com>
2012-07-11 10:30:48 -0700

    Add hash_combine()

--- src/tests/basic/main.cpp
+++ src/tests/basic/main.cpp
@@ -8,6 +8,13 @@ using Teuchos::rcp;
 using CSymPy::Add;
 using CSymPy::Symbol;
 
+template <class T>
+inline void hash_combine(std::size_t& seed, const T& v)
+{
+    std::hash<T> hasher;
+    seed ^= hasher(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+}
+
 int main(int argc, char* argv[])
 {
     RCP<Symbol> x = rcp(new Symbol("x"));
