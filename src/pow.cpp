56537c9e8ba166d4826c477db42a0307d89c10a4
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-07 17:17:36 -0600

    Implement multinomial_coefficients_mpz()

--- src/pow.cpp
+++ src/pow.cpp
@@ -57,13 +57,57 @@ RCP<Basic> pow(const RCP<Basic> &a, const RCP<Basic> &b)
     return rcp(new Pow(a, b));
 }
 
-// TODO: m, n should stay ints, but the calculted coefficients should be
-// converted from int to Integers
+// This function can overflow, but it is fast.
+// TODO: figure out condition for (m, n) when it overflows and raise an
+// exception.
 void multinomial_coefficients(int m, int n, map_vec_int &r)
 {
     vec_int t;
     int j, tj, start, k;
     long long int v;
+    if (m < 2)
+        throw std::runtime_error("multinomial_coefficients: m >= 2 must hold.");
+    if (n < 0)
+        throw std::runtime_error("multinomial_coefficients: n >= 0 must hold.");
+    t.assign(m, 0);
+    t[0] = n;
+    r[t] = 1;
+    if (n == 0) return;
+    j = 0;
+    while (j < m - 1) {
+        tj = t[j];
+        if (j) {
+            t[j] = 0;
+            t[0] = tj;
+        }
+        if (tj > 1) {
+            t[j+1] += 1;
+            j = 0;
+            start = 1;
+            v = 0;
+        } else {
+            j += 1;
+            start = j + 1;
+            v = r[t];
+            t[j] += 1;
+        }
+        for (k=start; k<m; k++) {
+            if (t[k]) {
+                t[k] -= 1;
+                v += r[t];
+                t[k] += 1;
+            }
+        }
+        t[0] -= 1;
+        r[t] = (v*tj) / (n-t[0]);
+    }
+}
+
+void multinomial_coefficients_mpz(int m, int n, map_vec_mpz &r)
+{
+    vec_int t;
+    int j, tj, start, k;
+    mpz_class v;
     if (m < 2)
         throw std::runtime_error("multinomial_coefficients: m >= 2 must hold.");
     if (n < 0)
