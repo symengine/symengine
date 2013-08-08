16b2604977700cf124f2e123dd3f18e827d59a26
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-08 16:29:20 -0600

    Keep incrementing

--- src/rings.cpp
+++ src/rings.cpp
@@ -61,7 +61,12 @@ void poly_mul(const map_vec_mpz &A, const map_vec_mpz &B, map_vec_mpz &C)
     for (auto &a: A) {
         for (auto &b: B) {
             monomial_mul(a.first, b.first, exp);
-            C[exp] = a.second*b.second;
+            auto e = C.find(exp);
+            if (e == C.end()) {
+                C[exp] = a.second*b.second;
+            } else {
+                (e->second) += a.second*b.second;
+            }
         }
     }
 }
