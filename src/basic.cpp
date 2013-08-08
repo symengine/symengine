1cd725c02ea57f386c659a5331813aa7f33d24fc
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-08 01:23:00 -0600

    Implement expand()

--- src/basic.cpp
+++ src/basic.cpp
@@ -1,1 +1,17 @@
 #include "basic.h"
+#include "mul.h"
+#include "pow.h"
+
+using Teuchos::RCP;
+using Teuchos::rcp_dynamic_cast;
+
+namespace CSymPy {
+
+RCP<Basic> expand(const RCP<Basic> &self)
+{
+    if (is_a<Mul>(*self)) return mul_expand(rcp_dynamic_cast<Mul>(self));
+    if (is_a<Pow>(*self)) return pow_expand(rcp_dynamic_cast<Pow>(self));
+    throw std::runtime_error("expand: type not implemented");
+}
+
+} // CSymPy
