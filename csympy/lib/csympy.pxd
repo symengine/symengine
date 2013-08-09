ca75f30af561d972b26c02f0f2d4eedc86711201
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-09 13:39:22 -0600

    Use Teuchos::RCP in the wrappers

--- csympy/lib/csympy.pxd
+++ csympy/lib/csympy.pxd
@@ -5,3 +5,15 @@ cdef extern from "symbol.h" namespace "CSymPy":
     cdef cppclass Symbol:
         Symbol(string name)
         string __str__()
+
+    cdef cppclass Integer:
+        Integer(int i)
+        string __str__()
+
+cdef extern from "basic.h" namespace "Teuchos":
+
+    cdef cppclass RCP[T]:
+        T& operator*() nogil except +
+
+    cdef RCP[Symbol] rcp(Symbol *p)
+    cdef RCP[Integer] rcp(Integer *p)
--- csympy/lib/csympy_wrapper.pyx
+++ csympy/lib/csympy_wrapper.pyx
@@ -1,13 +1,12 @@
+from cython.operator cimport dereference as deref
 cimport csympy
+from csympy cimport rcp, RCP
 
 cdef class Symbol(object):
-    cdef csympy.Symbol *thisptr
+    cdef RCP[csympy.Symbol] thisptr
 
     def __cinit__(self, name):
-        self.thisptr = new csympy.Symbol(name);
-
-    def __dealloc__(self):
-        del self.thisptr
+        self.thisptr = rcp(new csympy.Symbol(name))
 
     def __str__(self):
-        return self.thisptr.__str__()
+        return deref(self.thisptr).__str__()
