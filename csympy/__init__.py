e51b70bd732e958adb3d37318589d773ec22f348
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-09 17:23:53 -0600

    Implement var()

--- csympy/__init__.py
+++ csympy/__init__.py
@@ -1,2 +1,3 @@
-from lib.csympy_wrapper import (Symbol, Integer, sympify, SympifyError, Add,
+from .lib.csympy_wrapper import (Symbol, Integer, sympify, SympifyError, Add,
         Mul, Pow)
+from .utilities import var
--- csympy/tests/test_symbol.py
+++ csympy/tests/test_symbol.py
@@ -1,6 +1,10 @@
-from csympy import Symbol
+from csympy import Symbol, var
 
 def test_symbol():
     x = Symbol("x")
     assert str(x) == "x"
     assert str(x) != "y"
+
+def test_var():
+    var("z")
+    assert z == Symbol("z")
