7e026dc54f60b11d10aadc803555e558cde1e392
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-09 16:49:15 -0600

    Test for types in __mul__

--- csympy/tests/test_arit.py
+++ csympy/tests/test_arit.py
@@ -1,3 +1,5 @@
+from nose.tools import raises
+
 from csympy import Symbol, Integer
 
 def test_arit1():
@@ -14,3 +16,9 @@ def test_arit2():
     assert x+x == Integer(2) * x
     assert x+x != Integer(3) * x
     assert x+x == 2 * x
+
+@raises(TypeError)
+def test_arit3():
+    x = Symbol("x")
+    y = Symbol("y")
+    e = "x"*x
