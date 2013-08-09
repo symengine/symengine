630f7fa3290c14e348390a1f7a802cab9075f972
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-09 16:21:02 -0600

    Add more tests

--- csympy/tests/test_arit.py
+++ csympy/tests/test_arit.py
@@ -1,7 +1,15 @@
-from csympy import Symbol
+from csympy import Symbol, Integer
 
-def test_arit():
+def test_arit1():
     x = Symbol("x")
     y = Symbol("y")
     e = x + y
     e = x * y
+    e = Integer(2)*x
+    #e = 2*x
+
+def test_arit2():
+    x = Symbol("x")
+    y = Symbol("y")
+    #assert x+x == Integer(2) * x
+    #assert x+x == 2 * x
