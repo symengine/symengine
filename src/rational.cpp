03e741f896296fee65494de82012d4fdee26264e
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-12 17:00:22 -0600

    Rational: Add switches everywhere
    This speeds up things a bit

--- src/rational.cpp
+++ src/rational.cpp
@@ -6,34 +6,43 @@ using Teuchos::rcp;
 namespace CSymPy {
 
 Rational::Rational(int i)
+    : iz{i}, is_integer{true}
 {
-    this->i = i;
 }
 
 Rational::Rational(mpz_class i)
+    : iz{i}, is_integer{true}
 {
-    this->i = i;
 }
 
+/*
 Rational::Rational(mpq_class i)
+    : i{i}, is_integer{false}
 {
-    this->i = i;
 }
+*/
 
+/*
 Rational::Rational(mpz_class num, mpz_class den)
+    : is_integer{false}
 {
-    this->i = mpq_class(num, den);
+    this->iq = mpq_class(num, den);
     // TODO: investigate if this is needed here:
-    this->i.canonicalize();
+    this->iq.canonicalize();
 }
+*/
 
 std::size_t Rational::__hash__() const
 {
     // only the least significant bits that fit into "signed long int" are
     // hashed:
     std::size_t seed = 0;
-    hash_combine<long long int>(seed, this->i.get_num().get_si());
-    hash_combine<long long int>(seed, this->i.get_den().get_si());
+    if (this->is_integer) {
+        hash_combine<long long int>(seed, this->iz.get_si());
+    } else {
+        hash_combine<long long int>(seed, this->iq.get_num().get_si());
+        hash_combine<long long int>(seed, this->iq.get_den().get_si());
+    }
     return seed;
 }
 
@@ -41,7 +50,10 @@ bool Rational::__eq__(const Basic &o) const
 {
     if (is_a<Rational>(o)) {
         const Rational &s = static_cast<const Rational &>(o);
-        return this->i == s.i;
+        if (this->is_integer && s.is_integer)
+            return this->iz == s.iz;
+        else
+            throw std::runtime_error("Not implemented.");
     }
     return false;
 }
@@ -49,7 +61,10 @@ bool Rational::__eq__(const Basic &o) const
 std::string Rational::__str__() const
 {
     std::ostringstream s;
-    s << this->i;
+    if (is_integer)
+        s << this->iz;
+    else
+        s << this->iq;
     return s.str();
 }
 
--- src/rational.h
+++ src/rational.h
@@ -9,13 +9,16 @@ namespace CSymPy {
 
 class Rational : public Basic {
 public:
-    mpq_class i;
+    mpz_class iz;
+    mpq_class iq;
+    bool is_integer; // if true use iz, otherwise use iq. Note: If 'false',
+                     // then iq is *not* an integer (iq.get_den() != 1).
 
 public:
     Rational(int i);
     Rational(mpz_class i);
-    Rational(mpq_class i);
-    Rational(mpz_class num, mpz_class den);
+//    Rational(mpq_class i);
+//    Rational(mpz_class num, mpz_class den);
     virtual std::size_t __hash__() const;
     virtual bool __eq__(const Basic &o) const;
     virtual std::string __str__() const;
@@ -23,55 +26,63 @@ public:
     // Convert to "int", raise an exception if it does not fit
     signed long int as_int();
     inline mpz_class as_mpz() {
-        if (!(this->is_int())) throw std::runtime_error("Not an integer.");
-        return this->i.get_num();
+        if (!(is_integer)) throw std::runtime_error("Not an integer.");
+        return this->iz;
     }
-    inline mpq_class as_mpq() { return this->i; }
-    inline bool is_zero() { return this->i == 0; }
-    inline bool is_one() { return this->i == 1; }
-    inline bool is_int() { return this->i.get_den() == 1; }
+    inline bool is_zero() { return is_integer && this->iz == 0; }
+    inline bool is_one() { return is_integer && this->iz == 1; }
+    inline bool is_int() { return is_integer; }
 };
 
 inline Teuchos::RCP<Rational> addint(const Teuchos::RCP<Rational> &self,
     const Teuchos::RCP<Rational> &other)
 {
-    return Teuchos::rcp(new CSymPy::Rational(self->i + other->i));
+    if (self->is_integer && other->is_integer)
+        return Teuchos::rcp(new CSymPy::Rational(self->iz + other->iz));
+    else
+        throw std::runtime_error("Not implemented.");
 }
 
 inline Teuchos::RCP<Rational> subint(const Teuchos::RCP<Rational> &self,
     const Teuchos::RCP<Rational> &other)
 {
-    return Teuchos::rcp(new CSymPy::Rational(self->i - other->i));
+    if (self->is_integer && other->is_integer)
+        return Teuchos::rcp(new CSymPy::Rational(self->iz - other->iz));
+    else
+        throw std::runtime_error("Not implemented.");
 }
 
 inline Teuchos::RCP<Rational> mulint(const Teuchos::RCP<Rational> &self,
     const Teuchos::RCP<Rational> &other)
 {
-    return Teuchos::rcp(new CSymPy::Rational(self->i * other->i));
+    if (self->is_integer && other->is_integer)
+        return Teuchos::rcp(new CSymPy::Rational(self->iz * other->iz));
+    else
+        throw std::runtime_error("Not implemented.");
 }
 
 inline Teuchos::RCP<Rational> divint(const Teuchos::RCP<Rational> &self,
     const Teuchos::RCP<Rational> &other)
 {
-    return Teuchos::rcp(new CSymPy::Rational(self->i / other->i));
+    if (self->is_integer && other->is_integer)
+        return Teuchos::rcp(new CSymPy::Rational(self->iz / other->iz));
+    else
+        throw std::runtime_error("Not implemented.");
 }
 
 inline Teuchos::RCP<Rational> powint(const Teuchos::RCP<Rational> &self,
     const Teuchos::RCP<Rational> &other)
 {
-    if (other->i.get_den() == 1) {
-        if (!(other->i.get_num().fits_ulong_p()))
+    if (other->is_integer) {
+        if (!(other->iz.fits_ulong_p()))
             throw std::runtime_error("powint: 'exp' does not fit unsigned int.");
         mpz_class tmp;
-        mpz_pow_ui(tmp.get_mpz_t(), self->i.get_num().get_mpz_t(),
-                other->i.get_num().get_ui());
-        if (self->i.get_den() == 1) {
+        if (self->is_integer) {
+            mpz_pow_ui(tmp.get_mpz_t(), self->iz.get_mpz_t(),
+                    other->iz.get_ui());
             return Teuchos::rcp(new CSymPy::Rational(tmp));
         } else {
-            mpz_class tmp2;
-            mpz_pow_ui(tmp2.get_mpz_t(), self->i.get_den().get_mpz_t(),
-                    other->i.get_num().get_ui());
-            return Teuchos::rcp(new CSymPy::Rational(tmp, tmp2));
+            throw std::runtime_error("powint: non-integer 'self' not implemented.");
         }
     } else {
         throw std::runtime_error("powint: 'exp' must be an integer.");
@@ -110,7 +121,10 @@ inline void ipowint(const Teuchos::Ptr<Teuchos::RCP<Rational>> &self,
 
 inline Teuchos::RCP<Rational> negint(const Teuchos::RCP<Rational> &self)
 {
-    return Teuchos::rcp(new CSymPy::Rational(-(self->i)));
+    if (self->is_integer)
+        return Teuchos::rcp(new CSymPy::Rational(-(self->iz)));
+    else
+        throw std::runtime_error("Not implemented.");
 }
 
 
