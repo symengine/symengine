7265a842b5f9f86618c7586b8ecde60dc8717d9a
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-14 14:00:29 -0600

    Add a test for sqrt(x).diff(x).diff(x)

--- src/tests/basic/test_arit.cpp
+++ src/tests/basic/test_arit.cpp
@@ -220,7 +220,7 @@ void test_div()
 
 void test_pow()
 {
-    RCP<Basic> x = rcp(new Symbol("x"));
+    RCP<Symbol> x = rcp(new Symbol("x"));
     RCP<Basic> y = rcp(new Symbol("y"));
     RCP<Basic> z = rcp(new Symbol("z"));
     RCP<Basic> im1 = rcp(new Integer(-1));
@@ -283,6 +283,12 @@ void test_pow()
 
     r1 = pow(mul(i3, mul(pow(x, i2), pow(y, im1))), i3);
     r2 = mul(i27, mul(pow(x, i6), pow(y, im3)));
+    assert(eq(r1, r2));
+
+    /*    */
+    r1 = sqrt(x);
+    r1 = r1->diff(x)->diff(x);
+    r2 = mul(div(im1, i4), pow(x, div(im3, i2)));
     assert(eq(r1, r2));
 }
 
