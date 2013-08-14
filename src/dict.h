b5238a2032ddf966af00905640307fb2340c9878
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-14 14:56:06 -0600

    Implement umap_basic_basic

--- src/dict.h
+++ src/dict.h
@@ -38,6 +38,8 @@ typedef struct
 
 typedef std::unordered_map<Teuchos::RCP<Basic>, Teuchos::RCP<Number>,
         RCPBasicHash, RCPBasicKeyEq> umap_basic_int;
+typedef std::unordered_map<Teuchos::RCP<Basic>, Teuchos::RCP<Basic>,
+        RCPBasicHash, RCPBasicKeyEq> umap_basic_basic;
 
 typedef std::vector<int> vec_int;
 typedef std::map<vec_int, long long int> map_vec_int;
@@ -51,6 +53,8 @@ typedef std::map<Teuchos::RCP<Basic>, Teuchos::RCP<Basic>,
 bool dicts_equal(const umap_basic_int &a, const umap_basic_int &b);
 bool map_basic_int_equal(const map_basic_int &a, const map_basic_int &b);
 bool map_basic_basic_equal(const map_basic_basic &a, const map_basic_basic &b);
+bool umap_basic_basic_equal(const umap_basic_basic &a,
+        const umap_basic_basic &b);
 
 
 
@@ -85,5 +89,6 @@ std::ostream& operator<<(std::ostream& out, const CSymPy::map_vec_mpz& d);
 std::ostream& operator<<(std::ostream& out, const CSymPy::umap_vec_mpz& d);
 std::ostream& operator<<(std::ostream& out, const CSymPy::map_basic_int& d);
 std::ostream& operator<<(std::ostream& out, const CSymPy::map_basic_basic& d);
+std::ostream& operator<<(std::ostream& out, const CSymPy::umap_basic_basic& d);
 
 #endif
