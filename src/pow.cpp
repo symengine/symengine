9e7e546bd16f0591587652e3b83790270ed2a47e
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-15 13:05:30 -0600

    Implement Pow::compare()

--- src/pow.cpp
+++ src/pow.cpp
@@ -68,6 +68,18 @@ bool Pow::__eq__(const Basic &o) const
     return false;
 }
 
+int Pow::compare(const Basic &o) const
+{
+    CSYMPY_ASSERT(is_a<Pow>(o))
+    const Pow &s = static_cast<const Pow &>(o);
+    int base_cmp = base_->compare(*s.base_);
+    if (base_cmp == 0)
+        return exp_->compare(*s.exp_);
+    else
+        return base_cmp;
+}
+
+
 std::string Pow::__str__() const
 {
     std::ostringstream o;
