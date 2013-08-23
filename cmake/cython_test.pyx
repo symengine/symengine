88a2e7324018e56f650c84f25390dc51262fa0d9
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-22 20:52:19 -0600

    Cython: add the missing class

--- cmake/cython_test.pyx
+++ cmake/cython_test.pyx
@@ -72,15 +72,14 @@ cdef extern from "basic.h" namespace "CSymPy":
     bool is_a_Add "CSymPy::is_a<CSymPy::Add>"(const Basic &b) nogil
     bool is_a_Mul "CSymPy::is_a<CSymPy::Mul>"(const Basic &b) nogil
 
+cdef extern from "symbol.h" namespace "CSymPy":
+    cdef cppclass Symbol(Basic):
+        Symbol(string name) nogil
+        string get_name() nogil
+
 cdef extern from "add.h" namespace "CSymPy":
     cdef RCP[Basic] add(RCP[Basic] &a, RCP[Basic] &b) nogil except+
     cdef RCP[Basic] sub(RCP[Basic] &a, RCP[Basic] &b) nogil except+
 
     cdef cppclass Add(Basic):
         void as_two_terms(const Ptr[RCP[Basic]] &a, const Ptr[RCP[Basic]] &b)
-
-cdef extern from "mul.h" namespace "CSymPy":
-    cdef RCP[Basic] mul(RCP[Basic] &a, RCP[Basic] &b) nogil except+
-    cdef RCP[Basic] div(RCP[Basic] &a, RCP[Basic] &b) nogil except+
-    cdef RCP[Basic] neg(RCP[Basic] &a) nogil except+
-
