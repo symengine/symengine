1fd7dc51f5c2c15f32717f26a1f2610992914097
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-23 18:20:58 -0600

    Polish the RCP implementation
    We have removed unnecessary functions too.

--- src/csympy_rcp.h
+++ src/csympy_rcp.h
@@ -24,33 +24,34 @@ namespace CSymPy {
 
 enum ENull { null };
 
+// RCP can be null. Functionally it should be equivalent to Teuchos::RCP.
+
 template<class T>
 class RCP {
 public:
-    // TODO: remove this default constructor to ensure that RCP is never null.
-    // Also go over this class and remove all checks for ptr_ == 0 or NULL
     RCP(ENull null_arg = null) : ptr_(NULL) {}
     explicit RCP(T *p) : ptr_(p) {
-        CSYMPY_ASSERT(p != NULL)
+        CSYMPY_ASSERT(ptr_ != NULL)
         (ptr_->refcount_)++;
     }
     RCP(const RCP<T> &rp) : ptr_(rp.ptr_) {
-        // TODO: remove the is_null() condition
         if (!is_null()) (ptr_->refcount_)++;
     }
     template<class T2> RCP(const RCP<T2>& r_ptr) : ptr_(r_ptr.get()) {
-        // TODO: remove the is_null() condition
         if (!is_null()) (ptr_->refcount_)++;
     }
     ~RCP() {
-        // TODO: remove the ptr_ != 0 condition
-        if (ptr_ != NULL)
-        if (--(ptr_->refcount_) == 0) delete ptr_;
+        if (ptr_ != NULL && --(ptr_->refcount_) == 0) delete ptr_;
     }
-    T* operator->() const { return ptr_; }
-    T& operator*() const { return *ptr_; }
+    T* operator->() const {
+        CSYMPY_ASSERT(ptr_ != NULL)
+        return ptr_;
+    }
+    T& operator*() const {
+        CSYMPY_ASSERT(ptr_ != NULL)
+        return *ptr_;
+    }
     T* get() const { return ptr_; }
-    // Remove the is_null() condition
     bool is_null() const { return ptr_ == NULL; }
     template<class T2> bool operator==(const RCP<T2> &p2) {
         return ptr_ == p2.ptr_;
@@ -60,22 +61,14 @@ public:
     }
     RCP<T>& operator=(const RCP<T> &r_ptr) {
         T *r_ptr_ptr_ = r_ptr.ptr_;
-        // TODO: remove the is_null() condition
-        if (!r_ptr.is_null())
-        (r_ptr_ptr_->refcount_)++;
-        // TODO: remove the is_null() condition
-        if (!is_null())
-        if (--(ptr_->refcount_) == 0) delete ptr_;
+        if (!r_ptr.is_null()) (r_ptr_ptr_->refcount_)++;
+        if (!is_null() && --(ptr_->refcount_) == 0) delete ptr_;
         ptr_ = r_ptr_ptr_;
         return *this;
     }
-    // TODO: Remove this:
-    RCP<T>& operator=(ENull) { reset(); return *this; }
-    // TODO: Remove this:
     void reset() {
-        if (!is_null())
-        if (--(ptr_->refcount_) == 0) delete ptr_;
-        ptr_ = 0;
+        if (!is_null() && --(ptr_->refcount_) == 0) delete ptr_;
+        ptr_ = NULL;
     }
 private:
     T *ptr_;
@@ -95,17 +88,10 @@ inline RCP<T2> rcp_static_cast(const RCP<T1>& p1)
     return RCP<T2>(check);
 }
 
-template<class T>
-inline bool is_null(const RCP<T> &p)
-{
-    return p.is_null();
-}
-
-
 template<class T2, class T1>
 inline RCP<T2> rcp_dynamic_cast(const RCP<T1>& p1)
 {
-    if (!is_null(p1)) {
+    if (!p1.is_null()) {
         T2 *p = NULL;
         // Make the compiler check if the conversion is legal
         p = dynamic_cast<T2*>(p1.get());
