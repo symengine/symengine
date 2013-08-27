8e3c2a258d8248afb094e2b2c63cddcb24adf556
Ondřej Čertík <ondrej.certik@gmail.com>
2013-08-27 17:40:32 -0600

    Add::__str__(): Improve space handling around "-"

--- src/add.cpp
+++ src/add.cpp
@@ -101,14 +101,34 @@ std::string Add::__str__() const
     std::ostringstream o;
     if (neq(coef_, zero))
         o << *coef_ << " + ";
+    int counter = 0;
     for (auto &p: dict_) {
         if (eq(p.second, one))
             o << *(p.first);
         else {
-            if (eq(p.second, minus_one))
-                o << "-";
-            else
-                o << *(p.second);
+            // TODO: extend this for Rationals as well:
+            if (is_a<Integer>(*p.second) &&
+                    rcp_static_cast<Integer>(p.second)->is_negative()
+                    && o.tellp() >= 3)
+                o.seekp(-3, std::ios_base::cur);
+            if (eq(p.second, minus_one)) {
+                if (counter >= 1)
+                    o << " - ";
+                else
+                    o << "-";
+            } else {
+                // TODO: extend this for Rationals as well:
+                if (is_a<Integer>(*p.second) &&
+                        rcp_static_cast<Integer>(p.second)->is_negative()) {
+                    if (counter >= 1)
+                        o << " - ";
+                    else
+                        o << "-";
+                    o << -(rcp_static_cast<Integer>(p.second))->i;
+                } else {
+                    o << *(p.second);
+                }
+            }
             if (is_a<Add>(*p.first)) {
                 if (!eq(p.second, minus_one)) o << "*";
                 o << "(";
@@ -117,6 +137,7 @@ std::string Add::__str__() const
             if (is_a<Add>(*p.first)) o << ")";
         }
         o << " + ";
+        counter++;
     }
     std::string s = o.str();
     return s.substr(0, s.size()-3);
