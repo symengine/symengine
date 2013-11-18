361fdd7feb89bc5b0770813bc14aa2dc4a0cb9d6
Ondřej Čertík <ondrej.certik@gmail.com>
2013-11-18 16:43:27 -0700

    Travis polishing: use [[ instead of [
    And remove unnecessary semicolons.

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -5,9 +5,9 @@ set -e
 # Echo each command
 set -x
 
-if [ "${WITH_CSYMPY_RCP}" == "yes" ]; then
-    ctest --output-on-failure;
-    if [ "${WITH_PYTHON}" == "yes" ]; then
-        nosetests -v;
+if [[ "${WITH_CSYMPY_RCP}" == "yes" ]]; then
+    ctest --output-on-failure
+    if [[ "${WITH_PYTHON}" == "yes" ]]; then
+        nosetests -v
     fi
 fi
