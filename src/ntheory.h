73b8ea66851d3b6102435f92731a5aa62b2982dc
Thilina Bandara Rathnayake <thilinarmtb@gmail.com>
2013-12-23 22:42:27 +0530

    Implemented lcm

--- src/ntheory.h
+++ src/ntheory.h
@@ -2,18 +2,39 @@
 #define CSYMPY_NTHEORY_H
 
 #include <gmpxx.h>
+#include <initializer_list>
 
-#include "basic.h"
 #include "number.h"
 #include "integer.h"
 
 namespace CSymPy {
 
-inline RCP<Integer> gcd(Integer a, Integer b)
+inline RCP<Integer> gcd(const Integer &a, const Integer &b)
+{
+    mpz_class g;
+    
+    mpz_gcd(g.get_mpz_t(), static_cast<Integer>(a).as_mpz().get_mpz_t(), 
+                                 static_cast<Integer>(b).as_mpz().get_mpz_t());
+
+    return rcp(new Integer(g));
+}
+
+inline RCP<Integer> lcm(const Integer &a, const Integer &b)
 {
     mpz_class c;
 
-    mpz_gcd(c.get_mpz_t(), a.as_mpz().get_mpz_t(), b.as_mpz().get_mpz_t());
+    mpz_lcm(c.get_mpz_t(), static_cast<Integer>(a).as_mpz().get_mpz_t(),
+                                static_cast<Integer>(b).as_mpz().get_mpz_t());
+
+    return rcp(new Integer(c));
+}
+
+inline RCP<Integer> nextprime(const Integer &a)
+{
+    mpz_class c;
+
+    mpz_nextprime(c.get_mpz_t(), static_cast<Integer>(a).as_mpz().get_mpz_t());
+
     return rcp(new Integer(c));
 }
 
--- src/tests/ntheory/test_ntheory.cpp
+++ src/tests/ntheory/test_ntheory.cpp
@@ -8,19 +8,30 @@ using CSymPy::Integer;
 using CSymPy::print_stack_on_segfault;
 using CSymPy::rcp;
 
-void test_gcd()
+void test_gcd_lcm()
 {
+    Integer i1 (1);
     Integer i2 (2);
+    Integer i3 (3);
     Integer i4 (4);
+    Integer i6 (6);
 
     assert(eq(gcd(i2, i4), rcp(new Integer(2))));
+    assert(eq(gcd(i2, i3), rcp(new Integer(1))));
+    assert(eq(gcd(i2, i6), rcp(new Integer(2))));
+    assert(eq(gcd(i3, i6), rcp(new Integer(3))));
+
+    assert(eq(lcm(i2, i4), rcp(new Integer(4))));
+    assert(eq(lcm(i2, i3), rcp(new Integer(6))));
+    assert(eq(lcm(i2, i6), rcp(new Integer(6))));
+    assert(eq(lcm(i3, i6), rcp(new Integer(6))));
 }
 
 int main(int argc, char* argv[])
 {
     print_stack_on_segfault();
 
-    test_gcd();
+    test_gcd_lcm();
 
     return 0;
 }
