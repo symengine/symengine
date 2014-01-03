8903e73588d484898177e89250451760d4906d2b
Ondřej Čertík <ondrej.certik@gmail.com>
2014-01-03 15:49:06 -0800

    ntheory: move implementation of fns to .cpp file
    This way the header file is small and compiles faster. Only if it turns out
    that using "inline" is faster for some particular algorithm, should we use
    inline functions.

---
+++ src/ntheory.cpp
@@ -0,0 +1,37 @@
+#include "ntheory.h"
+
+namespace CSymPy {
+
+RCP<Integer> gcd(const Integer &a, const Integer &b)
+{
+    mpz_class g;
+
+    mpz_gcd(g.get_mpz_t(), a.as_mpz().get_mpz_t(), b.as_mpz().get_mpz_t());
+
+    return integer(g);
+}
+
+RCP<Integer> lcm(const Integer &a, const Integer &b)
+{
+    mpz_class c;
+
+    mpz_lcm(c.get_mpz_t(), a.as_mpz().get_mpz_t(), b.as_mpz().get_mpz_t());
+
+    return integer(c);
+}
+
+int probab_prime_p(const Integer &a, int reps)
+{
+    return mpz_probab_prime_p(a.as_mpz().get_mpz_t(), reps);
+}
+
+RCP<Integer> nextprime(const Integer &a)
+{
+    mpz_class c;
+
+    mpz_nextprime(c.get_mpz_t(), a.as_mpz().get_mpz_t());
+
+    return integer(c);
+}
+
+} // CSymPy
--- src/ntheory.h
+++ src/ntheory.h
@@ -5,44 +5,14 @@
 #ifndef CSYMPY_NTHEORY_H
 #define CSYMPY_NTHEORY_H
 
-#include <gmpxx.h>
-
-#include "number.h"
 #include "integer.h"
 
 namespace CSymPy {
 
-inline RCP<Integer> gcd(const Integer &a, const Integer &b)
-{
-    mpz_class g;
-
-    mpz_gcd(g.get_mpz_t(), a.as_mpz().get_mpz_t(), b.as_mpz().get_mpz_t());
-
-    return integer(g);
-}
-
-inline RCP<Integer> lcm(const Integer &a, const Integer &b)
-{
-    mpz_class c;
-
-    mpz_lcm(c.get_mpz_t(), a.as_mpz().get_mpz_t(), b.as_mpz().get_mpz_t());
-
-    return integer(c);
-}
-
-inline int probab_prime_p(const Integer &a, int reps = 25)
-{
-    return mpz_probab_prime_p(a.as_mpz().get_mpz_t(), reps);
-}
-
-inline RCP<Integer> nextprime(const Integer &a)
-{
-    mpz_class c;
-
-    mpz_nextprime(c.get_mpz_t(), a.as_mpz().get_mpz_t());
-
-    return integer(c);
-}
+RCP<Integer> gcd(const Integer &a, const Integer &b);
+RCP<Integer> lcm(const Integer &a, const Integer &b);
+int probab_prime_p(const Integer &a, int reps = 25);
+RCP<Integer> nextprime(const Integer &a);
 
 }
 #endif
