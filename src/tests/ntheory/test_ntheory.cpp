7175720b91a3bf908f845acd1c0cff8b98aacc3f
Thilina Bandara Rathnayake <thilinarmtb@gmail.com>
2014-01-05 14:58:52 +0530

    Use `integer()` instead of `Integer`

--- src/tests/ntheory/test_ntheory.cpp
+++ src/tests/ntheory/test_ntheory.cpp
@@ -6,46 +6,47 @@
 using CSymPy::Integer;
 using CSymPy::print_stack_on_segfault;
 using CSymPy::rcp;
+using CSymPy::RCP;
+using CSymPy::integer;
 
 void test_gcd_lcm()
 {
-    Integer i1 (1);
-    Integer i2 (2);
-    Integer i3 (3);
-    Integer i4 (4);
-    Integer i6 (6);
+    RCP<Integer> i2 = integer(2);
+    RCP<Integer> i3 = integer(3);
+    RCP<Integer> i4 = integer(4);
+    RCP<Integer> i6 = integer(6);
 
-    assert(eq(gcd(i2, i4), rcp(new Integer(2))));
-    assert(eq(gcd(i2, i3), rcp(new Integer(1))));
-    assert(eq(gcd(i2, i6), rcp(new Integer(2))));
-    assert(eq(gcd(i3, i6), rcp(new Integer(3))));
+    assert(eq(gcd(*i2, *i4), integer(2)));
+    assert(eq(gcd(*i2, *i3), integer(1)));
+    assert(eq(gcd(*i2, *i6), integer(2)));
+    assert(eq(gcd(*i3, *i6), integer(3)));
 
-    assert(eq(lcm(i2, i4), rcp(new Integer(4))));
-    assert(eq(lcm(i2, i3), rcp(new Integer(6))));
-    assert(eq(lcm(i2, i6), rcp(new Integer(6))));
-    assert(eq(lcm(i3, i6), rcp(new Integer(6))));
+    assert(eq(lcm(*i2, *i4), integer(4)));
+    assert(eq(lcm(*i2, *i3), integer(6)));
+    assert(eq(lcm(*i2, *i6), integer(6)));
+    assert(eq(lcm(*i3, *i6), integer(6)));
 }
 
 void test_nextprime()
 {
-    Integer i1 (1);
-    Integer i5 (5);
-    Integer i6 (6);
+    RCP<Integer> i1 = integer(1);
+    RCP<Integer> i5 = integer(5);
+    RCP<Integer> i6 = integer(6);
 
-    assert(eq(nextprime(i1), rcp(new Integer(2))));
-    assert(eq(nextprime(i5), rcp(new Integer(7))));
-    assert(eq(nextprime(i6), rcp(new Integer(7))));
+    assert(eq(nextprime(*i1), integer(2)));
+    assert(eq(nextprime(*i5), integer(7)));
+    assert(eq(nextprime(*i6), integer(7)));
 }
 
 void test_probab_prime_p()
 {
-    Integer i1 (1);
-    Integer i5 (5);
-    Integer i6 (6);
+    RCP<Integer> i1 = integer(1);
+    RCP<Integer> i5 = integer(5);
+    RCP<Integer> i6 = integer(6);
 
-    assert(probab_prime_p(i1) == 0);
-    assert(probab_prime_p(i5) == 2);
-    assert(probab_prime_p(i6) == 0);
+    assert(probab_prime_p(*i1) == 0);
+    assert(probab_prime_p(*i5) == 2);
+    assert(probab_prime_p(*i6) == 0);
 }
 
 int main(int argc, char* argv[])
