810e90498c2b5546505f894ae27350cadec30a9f
Ondřej Čertík <ondrej.certik@gmail.com>
2014-01-23 21:55:59 -0700

    Fix a typo in FindECM.cmake
    In the main CMakeLists.txt we use ECM_LIBRARY, so we need to use it here as
    well.

--- cmake/FindECM.cmake
+++ cmake/FindECM.cmake
@@ -1,9 +1,9 @@
 find_path(ECM_INCLUDE_DIR ecm.h $ENV{PYTHONHPC}/include)
 find_library(ECM ecm $ENV{PYTHONHPC}/lib)
-set(ECM_LIBRARIES ${ECM})
+set(ECM_LIBRARY ${ECM})
 set(ECM_INCLUDE_DIRS ${ECM_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ECM DEFAULT_MSG ECM_LIBRARIES ECM_INCLUDE_DIR)
+find_package_handle_standard_args(ECM DEFAULT_MSG ECM_LIBRARY ECM_INCLUDE_DIR)
 
 mark_as_advanced(ECM_INCLUDE_DIR ECM)
