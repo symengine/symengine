b138f8d1cf57bd82241002cae229bdee1ffa9bf9
sushant <hiraysushant@gmail.com>
2014-04-02 20:27:58 +0530

    Untracked csympy_config.h file.

--- src/csympy_config.h
+++
@@ -1,8 +0,0 @@
-/* Define if you want to enable ASSERT testing in CSymPy */
-/* #undef WITH_CSYMPY_ASSERT */
-
-/* Define if you want to enable CSYMPY_RCP support in CSymPy */
-#define WITH_CSYMPY_RCP
-
-/* Define if you want to enable ECM support in CSymPy */
-/* #undef HAVE_CSYMPY_ECM */
