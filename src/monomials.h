9bc5e5c1bc7a846a710926db425d50e07f5d6e67
sushant <hiraysushant@gmail.com>
2014-04-03 16:28:47 +0530

    [WIP] Contd: Documenting source files
    Documented
    * monomials.h
    * mul.h
    * ntheory.h
    * number.h

--- src/monomials.h
+++ src/monomials.h
@@ -10,7 +10,7 @@
 #include "basic.h"
 
 namespace CSymPy {
-
+//! Monomial multiplication
 void monomial_mul(const vec_int &A, const vec_int &B, vec_int &C);
 
 } // CSymPy
--- src/number.h
+++ src/number.h
@@ -13,44 +13,50 @@ namespace CSymPy {
 
 class Number : public Basic {
 public:
+	//! \return true if `0`
     virtual bool is_zero() const = 0;
+    //! \return true if `1`
     virtual bool is_one() const = 0;
-
+	//! Addition
     virtual RCP<const Number> add(const Number &other) const = 0;
+    //! Subtraction
     virtual RCP<const Number> sub(const Number &other) const = 0;
     virtual RCP<const Number> rsub(const Number &other) const = 0;
+    //! Multiplication
     virtual RCP<const Number> mul(const Number &other) const = 0;
+    //! Division
     virtual RCP<const Number> div(const Number &other) const = 0;
     virtual RCP<const Number> rdiv(const Number &other) const = 0;
+    //! Power
     virtual RCP<const Number> pow(const Number &other) const = 0;
-
+	//! Differentiation w.r.t Symbol `x`
     virtual RCP<const Basic> diff(const RCP<const Symbol> &x) const;
 };
-
+//! Add `self` and `other`
 inline RCP<const Number> addnum(const RCP<const Number> &self,
     const RCP<const Number> &other)
 {
     return self->add(*other);
 }
-
+//! Subtract `self` and `other`
 inline RCP<const Number> subnum(const RCP<const Number> &self,
     const RCP<const Number> &other)
 {
     return self->sub(*other);
 }
-
+//! Multiply `self` and `other`
 inline RCP<const Number> mulnum(const RCP<const Number> &self,
     const RCP<const Number> &other)
 {
     return self->mul(*other);
 }
-
+//! Divide `self` and `other`
 inline RCP<const Number> divnum(const RCP<const Number> &self,
     const RCP<const Number> &other)
 {
     return self->div(*other);
 }
-
+//! Raise `self` to power `other`
 inline RCP<const Number> pownum(const RCP<const Number> &self,
     const RCP<const Number> &other)
 {
