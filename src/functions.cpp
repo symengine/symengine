9981d3555841dcbf7708e41615d39161ba7237af
sushant <hiraysushant@gmail.com>
2014-04-25 20:35:25 +0530

    Added class `sec`
    Added class `sec` and updated all the methods in the basic
    trignometric API

--- src/functions.cpp
+++ src/functions.cpp
@@ -269,17 +269,70 @@ int Csc::compare(const Basic &o) const
 std::string Csc::__str__() const
 {
     std::ostringstream o;
-    o << "Csc(" << *arg_ << ")";
+    o << "csc(" << *arg_ << ")";
     return o.str();
 }
 
 RCP<const Basic> csc(const RCP<const Basic> &arg)
 {
-    if (eq(arg, zero)) return one;
     return rcp(new Csc(arg));
 }
 
 /* ---------------------------- */
+
+Sec::Sec(const RCP<const Basic> &arg)
+    : arg_{arg}
+{
+    CSYMPY_ASSERT(is_canonical(arg))
+}
+
+bool Sec::is_canonical(const RCP<const Basic> &arg)
+{
+    // e.g. Sec(0)
+    if (is_a<Integer>(*arg) &&
+            rcp_static_cast<const Integer>(arg)->is_zero())
+        return false;
+    // TODO: add things like Sec(k*pi) etc.
+    // Update for +inf/-inf constraints
+    return true;
+}
+
+std::size_t Sec::__hash__() const
+{
+    std::size_t seed = 0;
+    hash_combine<Basic>(seed, *arg_);
+    return seed;
+}
+
+bool Sec::__eq__(const Basic &o) const
+{
+    if (is_a<Sec>(o) &&
+        eq(arg_, static_cast<const Sec &>(o).arg_))
+        return true;
+    return false;
+}
+
+int Sec::compare(const Basic &o) const
+{    CSYMPY_ASSERT(is_a<Sec>(o))
+    const Sec &s = static_cast<const Sec &>(o);
+    return arg_->__cmp__(s);
+}
+
+std::string Sec::__str__() const
+{
+    std::ostringstream o;
+    o << "sec(" << *arg_ << ")";
+    return o.str();
+}
+
+RCP<const Basic> sec(const RCP<const Basic> &arg)
+{
+    if (eq(arg, zero)) return one;
+    return rcp(new Sec(arg));
+}
+
+/* ---------------------------- */
+
 RCP<const Basic> Sin::diff(const RCP<const Symbol> &x) const
 {
     return mul(cos(arg_), arg_->diff(x));
@@ -305,6 +358,11 @@ RCP<const Basic> Cot::diff(const RCP<const Symbol> &x) const
 RCP<const Basic> Csc::diff(const RCP<const Symbol> &x) const
 {
     return mul(mul(mul(cot(arg_), csc(arg_)), minus_one), arg_->diff(x));
+}
+
+RCP<const Basic> Sec::diff(const RCP<const Symbol> &x) const
+{
+    return mul(mul(tan(arg_), sec(arg_)), arg_->diff(x));
 }
 
 RCP<const Basic> Sin::subs(const map_basic_basic &subs_dict) const
@@ -370,6 +428,19 @@ RCP<const Basic> Csc::subs(const map_basic_basic &subs_dict) const
         return self;
     else
         return csc(arg);
+}
+
+RCP<const Basic> Sec::subs(const map_basic_basic &subs_dict) const
+{
+    RCP<const Sec> self = rcp_const_cast<Sec>(rcp(this));
+    auto it = subs_dict.find(self);
+    if (it != subs_dict.end())
+        return it->second;
+    RCP<const Basic> arg = arg_->subs(subs_dict);
+    if (arg == arg_)
+        return self;
+    else
+        return sec(arg);
 }
 /* ---------------------------- */
 
