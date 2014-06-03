7a77830ef0c3a0035fb2711b1ef652b5542b1435
sushant <hiraysushant@gmail.com>
2014-06-04 03:15:28 +0530

    Basic changes to `pow`
    Removed quite a bit of seg-faults. With this change all earlier
    tests pass, however the `sqrt(2)` test still fails.
    Need a couple of more cases to handle them.

--- src/pow.cpp
+++ src/pow.cpp
@@ -98,8 +98,17 @@ RCP<const Basic> pow(const RCP<const Basic> &a, const RCP<const Basic> &b)
     if (eq(a, one)) return one;
     if (eq(a, minus_one) && is_a<Integer>(*b))
         return is_a<Integer>(*div(b, integer(2))) ? one : minus_one;
-    if (is_a_Number(*a) && is_a<Integer>(*b))
-        return pownum(rcp_static_cast<const Number>(a), rcp_static_cast<const Integer>(b));
+    if (is_a_Number(*a) && is_a<Integer>(*b)) {
+        RCP<const Number> exp_new = rcp_static_cast<const Number>(b);
+        if(exp_new->is_negative()) {
+            exp_new = exp_new->mul(*(rcp_static_cast<const Number>(minus_one)));
+            return pownum(rcp_static_cast<const Number>(a)->rdiv(*rcp_static_cast<const Number>(one)),
+                rcp_static_cast<const Integer>(b)->mul(*rcp_static_cast<const Number>(minus_one)));
+        }
+        else {
+            return pownum(rcp_static_cast<const Number>(a), rcp_static_cast<const Number>(b));
+        }
+    }
     if (is_a<Mul>(*a))
         return rcp_static_cast<const Mul>(a)->power_all_terms(b);
     if (is_a<Pow>(*a)) {
