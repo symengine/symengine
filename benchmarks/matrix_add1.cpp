48247b7fa0dd8d84e9e9f27ea352afaff276b41e
Thilina Bandara Rathanyake <thilinarmtb@gmail.com>
2014-06-03 10:52:24 +0530

    Refactor the tests and take the average value for N tests

--- benchmarks/matrix_add1.cpp
+++ benchmarks/matrix_add1.cpp
@@ -14,34 +14,37 @@ using CSymPy::RCP;
 using CSymPy::rcp;
 using CSymPy::integer;
 using CSymPy::DenseMatrix;
+using CSymPy::densematrix;
 
 int main(int argc, char* argv[])
 {
     Teuchos::print_stack_on_segfault();
 
-    std::vector<RCP<const Basic>> l, m, n;
     RCP<const DenseMatrix> A, B, C;
 
-    l = {integer(1), integer(2), integer(3), integer(4), integer(5), integer(6),
-        integer(7), integer(8), integer(9), integer(10), integer(11), integer(12),
-        integer(13), integer(14), integer(15), integer(16)};
-    A = densematrix(4, 4, l);
+    A = densematrix(4, 4, {integer(1), integer(2), integer(3), integer(4),
+        integer(5), integer(6), integer(7), integer(8), integer(9), integer(10),
+        integer(11), integer(12), integer(13), integer(14), integer(15),
+        integer(16)});
 
-    m = {integer(1), integer(2), integer(3), integer(4), integer(5), integer(6),
-        integer(7), integer(8), integer(9), integer(10), integer(11), integer(12),
-        integer(13), integer(14), integer(15), integer(16)};
-    B = densematrix(4, 4, m);
+    B = densematrix(4, 4, {integer(1), integer(2), integer(3), integer(4),
+        integer(5), integer(6), integer(7), integer(8), integer(9), integer(10),
+        integer(11), integer(12), integer(13), integer(14), integer(15),
+        integer(16)});
 
-    std::cout << "Adding Two Matrices" << std::endl;
+    std::cout << "Adding Two Matrices; matrix dimensions: 4 x 4" << std::endl;
 
+    // We are taking an average time since time for a single addition varied in
+    // a range of 40-50 microseconds
+    unsigned N = 1000;
     auto t1 = std::chrono::high_resolution_clock::now();
-    C = add_dense_dense(*A, *B);
+    for (unsigned i = 0; i < N; i++)
+        C = add_dense_dense(*A, *B);
     auto t2 = std::chrono::high_resolution_clock::now();
-    
+
     std::cout
-        << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count()
+        << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count()/N
         << " microseconds" << std::endl;
-    std::cout << "matrix dimensions: 4 x 4" << std::endl;
 
     return 0;
 }
