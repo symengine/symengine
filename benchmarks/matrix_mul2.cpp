0af017401f5a04410a08d9b41352a7cd25b9363e
Thilina Bandara Rathanyake <thilinarmtb@gmail.com>
2014-06-07 16:48:04 +0530

    Add new matrix multiplication benchmarks

--- benchmarks/matrix_mul2.cpp
+++ benchmarks/matrix_mul2.cpp
@@ -12,28 +12,27 @@ using CSymPy::RCP;
 using CSymPy::rcp;
 using CSymPy::DenseMatrix;
 using CSymPy::symbol;
-using CSymPy::add_dense_dense;
+using CSymPy::mul_dense_dense;
 
 int main(int argc, char* argv[])
 {
     Teuchos::print_stack_on_segfault();
 
-    DenseMatrix A = DenseMatrix(3, 3, {symbol("a"), symbol("b"), symbol("c"), symbol("d"),
-        symbol("e"), symbol("f"), symbol("g"), symbol("h"), symbol("i")});
+    DenseMatrix A = DenseMatrix(3, 3, {symbol("a"), symbol("b"), symbol("c"),
+        symbol("d"), symbol("e"), symbol("f"), symbol("g"), symbol("h"), symbol("i")});
+
 
     DenseMatrix B = DenseMatrix(3, 3, {symbol("x"), symbol("y"), symbol("z"), symbol("p"),
         symbol("q"), symbol("r"), symbol("u"), symbol("v"), symbol("w")});
 
     DenseMatrix C(3, 3);
 
-    std::cout << "Adding Two Matrices; matrix dimensions: 3 x 3" << std::endl;
+    std::cout << "Multiplying Two Matrices; matrix dimensions: 3 x 3" << std::endl;
 
-    // We are taking an average time since time for a single addition varied in
-    // a range of 40-50 microseconds
     unsigned N = 10000;
     auto t1 = std::chrono::high_resolution_clock::now();
     for (unsigned i = 0; i < N; i++)
-        add_dense_dense(A, B, C);
+        mul_dense_dense(A, B, C);
     auto t2 = std::chrono::high_resolution_clock::now();
 
     std::cout
