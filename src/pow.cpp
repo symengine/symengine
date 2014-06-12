29666b047522b32810b6f6f5dda92506f5e49539
sushant <hiraysushant@gmail.com>
2014-06-12 11:45:26 +0530

    Added basic methods for `Exp` class

--- src/pow.cpp
+++ src/pow.cpp
@@ -321,4 +321,23 @@ RCP<const Basic> Pow::subs(const map_basic_basic &subs_dict) const
         return pow(base_new, exp_new);
 }
 
+Exp::Exp(const RCP<const Basic> &exp)
+    : Pow(E, exp)
+{
+    CSYMPY_ASSERT(is_canonical(E, exp))
+}
+
+RCP<const Basic> Exp::diff(const RCP<const Symbol> &x) const
+{
+    return mul(pow(E, exp_), exp_->diff(x));
+
+}
+
+RCP<const Basic> exp(const RCP<const Basic> &e)
+{
+    if (eq(e, zero)) return one;
+    else if (eq(e, one)) return E;
+    else return Exp(e);
+}
+
 } // CSymPy
