6049ebd92c3c7da7a26276e776bfde414068f72c
Ondřej Čertík <ondrej.certik@gmail.com>
2014-06-16 22:52:42 -0600

    Only pass nonempty options to cmake

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -15,7 +15,28 @@ fi
 echo "Current directory:"
 pwd
 echo "Running cmake:"
-cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DWITH_BFD=${WITH_BFD} -DWITH_PYTHON=${WITH_PYTHON} -DWITH_CSYMPY_ASSERT=${WITH_CSYMPY_ASSERT} -DWITH_CSYMPY_RCP=${WITH_CSYMPY_RCP} -DWITH_ECM=${WITH_ECM} ${SOURCE_DIR}
+# We build the command line here. If the variable is empty, we skip it,
+# otherwise we pass it to cmake.
+cmake_line=""
+if [[ "${BUILD_TYPE}" != "" ]]; then
+    cmake_line="$cmake_line -DCMAKE_BUILD_TYPE=${BUILD_TYPE}"
+fi
+if [[ "${WITH_BFD}" != "" ]]; then
+    cmake_line="$cmake_line -DWITH_BFD=${WITH_BFD}"
+fi
+if [[ "${WITH_PYTHON}" != "" ]]; then
+    cmake_line="$cmake_line -DWITH_PYTHON=${WITH_PYTHON}"
+fi
+if [[ "${WITH_CSYMPY_ASSERT}" != "" ]]; then
+    cmake_line="$cmake_line -DWITH_CSYMPY_ASSERT=${WITH_CSYMPY_ASSERT}"
+fi
+if [[ "${WITH_CSYMPY_RCP}" != "" ]]; then
+    cmake_line="$cmake_line -DWITH_CSYMPY_RCP=${WITH_CSYMPY_RCP}"
+fi
+if [[ "${WITH_ECM}" != "" ]]; then
+    cmake_line="$cmake_line -DWITH_ECM=${WITH_ECM}"
+fi
+cmake $cmake_line ${SOURCE_DIR}
 echo "Current directory:"
 pwd
 echo "Running make:"
