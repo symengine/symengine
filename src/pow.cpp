5f69239fcc6460ac140dcd9e90dcfedbad1c28b6
Thilina Bandara Rathanyake <thilinarmtb@gmail.com>
2014-07-03 00:20:53 +0530

    A complete restructuring of `pow`

--- src/pow.cpp
+++ src/pow.cpp
@@ -107,76 +107,123 @@ RCP<const Basic> pow(const RCP<const Basic> &a, const RCP<const Basic> &b)
         return is_a<Integer>(*div(b, integer(2))) ? one : minus_one;
 
     if (is_a_Number(*a) && is_a_Number(*b)) {
-        if (is_a<Rational>(*a)) {
-            RCP<const Rational> exp_new = rcp_static_cast<const Rational>(a);
-            if (is_a<Integer>(*b)) {
-                return pownum(exp_new, rcp_static_cast<const Integer>(b));
-            } else if (is_a<Rational>(*b)) {
-                // b is a Rational
-                mpz_class q, r, num, den;
-                num = rcp_static_cast<const Rational>(b)->i.get_num();
-                den = rcp_static_cast<const Rational>(b)->i.get_den();
-
-                if (num > den || num < 0) {
-                    mpz_cdiv_qr(q.get_mpz_t(), r.get_mpz_t(), num.get_mpz_t(),
-                        den.get_mpz_t());
-
-                    if (r < 0) {
-                        r += den;
-                        q -= 1;
-                    }
-
-                    // Here we make the exponent postive and a fraction between 
-                    // 0 and 1. We multiply numerator and denominator appropriately
-                    // to achieve this
-                    RCP<const Basic> frac =
-                        div(exp_new->powrat(Integer(q)), integer(exp_new->i.get_den()));
-                    RCP<const Basic> surds =
-                        mul(rcp(new Pow(integer(exp_new->i.get_num()), div(integer(r), integer(den)))), 
-                            rcp(new Pow(integer(exp_new->i.get_den()), sub(one, div(integer(r), integer(den))))));
-                    return mul(frac, surds);
-                } else {
-                    return rcp(new Pow(exp_new, b));
-                }
+        if (is_a<Integer>(*b)) {
+            if (is_a<Rational>(*a)) {
+                RCP<const Rational> exp_new = rcp_static_cast<const Rational>(a);
+                return exp_new->powrat(*rcp_static_cast<const Integer>(b));
+            } else if (is_a<Integer>(*a)) {
+                RCP<const Integer> exp_new = rcp_static_cast<const Integer>(a);
+                return exp_new->powint(*rcp_static_cast<const Integer>(b));
             } else {
                 throw std::runtime_error("Not implemented");
             }
-        } else if (is_a<Integer>(*a)) {
-            // a is Integer
-            RCP<const Integer> exp_new = rcp_static_cast<const Integer>(a);
-            if (is_a<Integer>(*b)) {
-                return pownum(exp_new, rcp_static_cast<const Integer>(b));
-            } else if (is_a<Rational>(*b)) {
-                // b is a Rational
-                mpz_class q, r, num, den;
-                num = rcp_static_cast<const Rational>(b)->i.get_num();
-                den = rcp_static_cast<const Rational>(b)->i.get_den();
+        } else if (is_a<Rational>(*b)) {
+            mpz_class q, r, num, den;
+            num = rcp_static_cast<const Rational>(b)->i.get_num();
+            den = rcp_static_cast<const Rational>(b)->i.get_den();
 
-                if (num > den || num < 0) {
-                    mpz_cdiv_qr(q.get_mpz_t(), r.get_mpz_t(), num.get_mpz_t(),
-                        den.get_mpz_t());
+            if (num > den || num < 0) {
+                mpz_cdiv_qr(q.get_mpz_t(), r.get_mpz_t(), num.get_mpz_t(),
+                    den.get_mpz_t());
 
-                    if (r < 0) {
-                        r += den;
-                        q -= 1;
-                    }
-
-                    // Here we make the exponent postive and a fraction between 
-                    // 0 and 1. We multiply numerator and denominator appropriately
-                    // to achieve this
-                    RCP<const Number> frac = exp_new->powint(Integer(q));
-                    map_basic_basic surd;
-                    surd[exp_new] = div(integer(r), integer(den));
-                    return rcp(new Mul(frac, std::move(surd)));
-                } else {
-                    return rcp(new Pow(exp_new, b));
+                if (r < 0) {
+                    r += den;
+                    q -= 1;
                 }
+            } else {
+                return rcp(new Pow(a, b));
+            }
+            // Here we make the exponent postive and a fraction between
+            // 0 and 1. We multiply numerator and denominator appropriately
+            // to achieve this
+            if (is_a<Rational>(*a)) {
+                RCP<const Rational> exp_new = rcp_static_cast<const Rational>(a);
+                RCP<const Basic> frac =
+                    div(exp_new->powrat(Integer(q)), integer(exp_new->i.get_den()));
+                RCP<const Basic> surds =
+                    mul(rcp(new Pow(integer(exp_new->i.get_num()), div(integer(r), integer(den)))),
+                        rcp(new Pow(integer(exp_new->i.get_den()), sub(one, div(integer(r), integer(den))))));
+                return mul(frac, surds);
+            } else if (is_a<Integer>(*a)) {
+                RCP<const Integer> exp_new = rcp_static_cast<const Integer>(a);
+                RCP<const Number> frac = exp_new->powint(Integer(q));
+                map_basic_basic surd;
+                surd[exp_new] = div(integer(r), integer(den));
+                return rcp(new Mul(frac, std::move(surd)));
             } else {
                 throw std::runtime_error("Not implemented");
             }
         } else {
             throw std::runtime_error("Not implemented");
         }
+        // if (is_a<Rational>(*a)) {
+        //     RCP<const Rational> exp_new = rcp_static_cast<const Rational>(a);
+        //     if (is_a<Integer>(*b)) {
+        //         return pownum(exp_new, rcp_static_cast<const Integer>(b));
+        //     } else if (is_a<Rational>(*b)) {
+        //         // b is a Rational
+        //         mpz_class q, r, num, den;
+        //         num = rcp_static_cast<const Rational>(b)->i.get_num();
+        //         den = rcp_static_cast<const Rational>(b)->i.get_den();
+
+        //         if (num > den || num < 0) {
+        //             mpz_cdiv_qr(q.get_mpz_t(), r.get_mpz_t(), num.get_mpz_t(),
+        //                 den.get_mpz_t());
+
+        //             if (r < 0) {
+        //                 r += den;
+        //                 q -= 1;
+        //             }
+
+        //             // Here we make the exponent postive and a fraction between
+        //             // 0 and 1. We multiply numerator and denominator appropriately
+        //             // to achieve this
+        //             RCP<const Basic> frac =
+        //                 div(exp_new->powrat(Integer(q)), integer(exp_new->i.get_den()));
+        //             RCP<const Basic> surds =
+        //                 mul(rcp(new Pow(integer(exp_new->i.get_num()), div(integer(r), integer(den)))),
+        //                     rcp(new Pow(integer(exp_new->i.get_den()), sub(one, div(integer(r), integer(den))))));
+        //             return mul(frac, surds);
+        //         } else {
+        //             return rcp(new Pow(exp_new, b));
+        //         }
+        //     } else {
+        //         throw std::runtime_error("Not implemented");
+        //     }
+        // } else if (is_a<Integer>(*a)) {
+        //     RCP<const Integer> exp_new = rcp_static_cast<const Integer>(a);
+        //     if (is_a<Integer>(*b)) {
+        //         return pownum(exp_new, rcp_static_cast<const Integer>(b));
+        //     } else if (is_a<Rational>(*b)) {
+        //         mpz_class q, r, num, den;
+        //         num = rcp_static_cast<const Rational>(b)->i.get_num();
+        //         den = rcp_static_cast<const Rational>(b)->i.get_den();
+
+        //         if (num > den || num < 0) {
+        //             mpz_cdiv_qr(q.get_mpz_t(), r.get_mpz_t(), num.get_mpz_t(),
+        //                 den.get_mpz_t());
+
+        //             if (r < 0) {
+        //                 r += den;
+        //                 q -= 1;
+        //             }
+
+        //             // Here we make the exponent postive and a fraction between
+        //             // 0 and 1. We multiply numerator and denominator appropriately
+        //             // to achieve this
+        //             RCP<const Number> frac = exp_new->powint(Integer(q));
+        //             map_basic_basic surd;
+        //             surd[exp_new] = div(integer(r), integer(den));
+        //             return rcp(new Mul(frac, std::move(surd)));
+        //         } else {
+        //             return rcp(new Pow(exp_new, b));
+        //         }
+        //     } else {
+        //         throw std::runtime_error("Not implemented");
+        //     }
+        // } else {
+        //     throw std::runtime_error("Not implemented");
+        // }
     }
     if (is_a<Mul>(*a)) {
         return rcp_static_cast<const Mul>(a)->power_all_terms(b);
