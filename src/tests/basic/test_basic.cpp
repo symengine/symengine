ff8a63b146dba71a24d7a0ee4409cb8d96ad5f57
sushant <hiraysushant@gmail.com>
2014-07-08 22:09:32 +0530

    Added tests for complex addition, Shifted printing tests.
    Shifted the tests for pretty printing into the printing module.
    Added tests for complex addition, in which the final result is
    either int or rational or complex.

--- src/tests/basic/test_basic.cpp
+++ src/tests/basic/test_basic.cpp
@@ -27,7 +27,6 @@ using CSymPy::pow;
 using CSymPy::RCP;
 using CSymPy::print_stack_on_segfault;
 using CSymPy::Complex;
-using CSymPy::rcp_static_cast;
 
 void test_symbol_hash()
 {
@@ -431,20 +430,34 @@ void test_print_minus_one(){
 
 void test_complex()
 {
-    RCP<const Number> r1, r2, r3, c1, c2;
+    RCP<const Number> r1, r2, r3, c1, c2, c3;
     r1 = Rational::from_two_ints(integer(2), integer(4));
     r2 = Rational::from_two_ints(integer(5), integer(7));
     r3 = Rational::from_two_ints(integer(-5), integer(7));
 
-    c1 = Complex::from_two_rats(static_cast<const Rational&>(*r1), static_cast<const Rational&>(*r2));
-    c2 = Complex::from_two_rats(static_cast<const Rational&>(*r1), static_cast<const Rational&>(*r3));
-    assert(c1->__str__() == "1/2 + i5/7");
-    assert(c2->__str__() == "1/2 - i5/7");
-
     c1 = Complex::from_two_nums(*r1, *r2);
     c2 = Complex::from_two_nums(*r1, *r3);
-    assert(c1->__str__() == "1/2 + i5/7");
-    assert(c2->__str__() == "1/2 - i5/7");
+
+    // Basic check for equality in Complex::from_two_nums and Complex::from_two_rats
+    assert(eq(c1, Complex::from_two_rats(static_cast<const Rational&>(*r1), static_cast<const Rational&>(*r2))));
+    assert(neq(c2, Complex::from_two_rats(static_cast<const Rational&>(*r1), static_cast<const Rational&>(*r2))));
+
+    // Checks for complex addition
+    // Final result is int
+    assert(eq(addnum(c1, c2), one));
+    // Final result is complex
+    r2 = Rational::from_two_ints(integer(1), integer(1));
+    r3 = Rational::from_two_ints(integer(10), integer(7));
+    c3 = Complex::from_two_nums(*r2, *r3);
+    assert(eq(addnum(c1, c1), c3));
+    // Final result is rational
+    r1 = Rational::from_two_ints(integer(1), integer(4));
+    r2 = Rational::from_two_ints(integer(5), integer(7));
+    r3 = Rational::from_two_ints(integer(-5), integer(7));
+    c1 = Complex::from_two_nums(*r1, *r2);
+    c2 = Complex::from_two_nums(*r1, *r3);
+    assert(eq(addnum(c1, c2), div(one, integer(2))));
+
 }
 
 
--- src/tests/printing/test_printing.cpp
+++ src/tests/printing/test_printing.cpp
@@ -5,6 +5,8 @@
 #include "mul.h"
 #include "pow.h"
 #include "symbol.h"
+#include "rational.h"
+#include "complex.h"
 
 using CSymPy::RCP;
 using CSymPy::Basic;
@@ -14,6 +16,9 @@ using CSymPy::mul;
 using CSymPy::integer;
 using CSymPy::print_stack_on_segfault;
 using CSymPy::symbol;
+using CSymPy::Complex;
+using CSymPy::Rational;
+using CSymPy::Number;
 
 void test_printing()
 {
@@ -31,6 +36,21 @@ void test_printing()
 
     r = mul(integer(2), pow(symbol("x"), integer(2)));
     assert(r->__str__() == "2x^2");
+
+    RCP<const Number> r1, r2, r3, c1, c2;
+    r1 = Rational::from_two_ints(integer(2), integer(4));
+    r2 = Rational::from_two_ints(integer(5), integer(7));
+    r3 = Rational::from_two_ints(integer(-5), integer(7));
+
+    c1 = Complex::from_two_rats(static_cast<const Rational&>(*r1), static_cast<const Rational&>(*r2));
+    c2 = Complex::from_two_rats(static_cast<const Rational&>(*r1), static_cast<const Rational&>(*r3));
+    assert(c1->__str__() == "1/2 + i5/7");
+    assert(c2->__str__() == "1/2 - i5/7");
+
+    c1 = Complex::from_two_nums(*r1, *r2);
+    c2 = Complex::from_two_nums(*r1, *r3);
+    assert(c1->__str__() == "1/2 + i5/7");
+    assert(c2->__str__() == "1/2 - i5/7");
 
 }
 
