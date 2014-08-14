43cfc54184d29678d3e2c7507c446ce5e3d82e94
Ondřej Čertík <ondrej.certik@gmail.com>
2014-08-14 16:24:55 -0600

    Move the rest of datastructures to constants.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -43,43 +43,11 @@ extern RCP<const Basic> mC4;
 extern RCP<const Basic> mC5;
 extern RCP<const Basic> mC6;
 
-// sin_table[n] represents the value of sin(2*pi*n/24) for n = 0..23
-static RCP<const Basic> sin_table[] = {
-        zero, C0, C1, C2, C3, C4, one, C4, C3, C2, C1, C0,
-        zero, mC0, mC1, mC2, mC3, mC4, minus_one, mC4, mC3, mC2, mC1, mC0
-    };
+extern RCP<const Basic> sin_table[];
 
-static umap_basic_basic inverse_cst = {
-    {C3, i3},
-    {mC3, im3},
-    {C2, mul(i2, i2)},
-    {mC2, mul(im2, i2)},
-    {C4, integer(12)},
-    {mC4, integer(-12)},
-    {C5, i5},
-    {mC5, im5},
-    {C6, integer(10)},
-    {mC6, integer(-10)},
-    {div(one, i2), integer(6)},
-    {div(minus_one, i2), integer(-6)},
-};
+extern umap_basic_basic inverse_cst;
 
-static umap_basic_basic inverse_tct = {
-    {div(one, sq3), mul(i2, i3)},
-    {div(minus_one, sq3), mul(im2, i3)},
-    {sq3, i3},
-    {mul(minus_one, sq3), im3},
-    {add(one, sq2), div(pow(i2, i3), i3)},
-    {mul(minus_one, add(one, sq2)), div(pow(i2, i3), im3)},
-    {sub(sq2, one), pow(i2, i3)},
-    {sub(one, sq2), pow(im2, i3)},
-    {sub(i2, sq3), mul(mul(i2,i2), i3)},
-    {sub(sq3, i2), mul(mul(im2,i2), i3)},
-    {sqrt(add(i5, mul(i2, sqrt(i5)))), div(i5, i2)},
-    {mul(minus_one, sqrt(add(i5, mul(i2, sqrt(i5))))), div(im5, i2)},
-    {one, pow(i2, i2)},
-    {minus_one, mul(minus_one, pow(i2, i2))},
-};
+extern umap_basic_basic inverse_tct;
 
 bool get_pi_shift(const RCP<const Basic> &arg,
         const Ptr<RCP<const Integer>> &n,
