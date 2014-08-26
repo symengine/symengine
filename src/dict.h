4f40011a71ab11663eb1855136dfab9a562b59ea
Ondřej Čertík <ondrej.certik@gmail.com>
2014-08-26 12:40:14 -0600

    Improve comment to VecBasic

--- src/dict.h
+++ src/dict.h
@@ -89,7 +89,10 @@ typedef struct
 typedef std::unordered_map<vec_int, mpz_class,
         vec_int_hash, vec_int_eq> umap_vec_mpz;
 
-// To be used in CSymPy wrappers
+// To be used in CSymPy wrappers only.
+// This is a simple wrapper around std::vector of Basic (vec_basic) in order to
+// allow it to be used inside Cython, as a workaround a bug in Cython:
+// https://groups.google.com/d/topic/cython-users/_wqZklAQcLA/discussion
 class VecBasic {
 private:
     vec_basic v_;
