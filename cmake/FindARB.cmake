4b0343a886603fc2d88a7e6245d9a3acee7e0322
Thilina Bandara Rathanyake <thilinarmtb@gmail.com>
2014-11-01 13:07:59 +0530

    Update the cmake find modules to follow FindGMP.cmake
    But not exactly, I avoided using "${MODULE_DIR}/include" and
    "${MODULE_DIR}/lib" in `find_path` and `find_library` respectively.

--- cmake/FindARB.cmake
+++ cmake/FindARB.cmake
@@ -1,9 +1,9 @@
-find_path(ARB_INCLUDE_DIR fmprb.h $ENV{PYTHONHPC}/include)
-find_library(ARB arb $ENV{PYTHONHPC}/lib)
-set(ARB_LIBRARY ${ARB})
+find_path(ARB_INCLUDE_DIR fmprb.h)
+find_library(ARB_LIBRARY arb)
+set(ARB_LIBRARIES ${ARB_LIBRARY})
 set(ARB_INCLUDE_DIRS ${ARB_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ARB DEFAULT_MSG ARB_LIBRARY ARB_INCLUDE_DIR)
+find_package_handle_standard_args(ARB DEFAULT_MSG ARB_LIBRARIES ARB_INCLUDE_DIR)
 
-mark_as_advanced(ARB_INCLUDE_DIR ARB)
+mark_as_advanced(ARB_INCLUDE_DIR ARB_LIBRARY)
--- cmake/FindMPFR.cmake
+++ cmake/FindMPFR.cmake
@@ -1,9 +1,9 @@
-find_path(MPFR_INCLUDE_DIR mpfr.h $ENV{PYTHONHPC}/include)
-find_library(MPFR mpfr $ENV{PYTHONHPC}/lib)
-set(MPFR_LIBRARY ${MPFR})
+find_path(MPFR_INCLUDE_DIR mpfr.h)
+find_library(MPFR_LIBRARY mpfr)
+set(MPFR_LIBRARIES ${MPFR_LIBRARY})
 set(MPFR_INCLUDE_DIRS ${MPFR_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MPFR DEFAULT_MSG MPFR_LIBRARY MPFR_INCLUDE_DIR)
+find_package_handle_standard_args(MPFR DEFAULT_MSG MPFR_LIBRARIES MPFR_INCLUDE_DIR)
 
-mark_as_advanced(MPFR_INCLUDE_DIR MPFR)
+mark_as_advanced(MPFR_INCLUDE_DIR MPFR_LIBRARY)
