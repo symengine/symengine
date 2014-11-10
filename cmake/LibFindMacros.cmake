763a2100ea7aa00ee0f7bfa881f2560251b773ee
Ondřej Čertík <ondrej.certik@gmail.com>
2014-11-10 10:41:08 -0700

    Add documentation to LibFindMacros.cmake

--- cmake/LibFindMacros.cmake
+++ cmake/LibFindMacros.cmake
@@ -1,3 +1,19 @@
+# Macros for finding external libraries
+#
+# Example usage:
+#
+#     include(LibFindMacros)
+#     libfind_include(gmpxx.h gmp)
+#     libfind_library(gmpxx gmp)
+#     libfind_library(gmp gmp)
+#     set(GMP_LIBRARIES ${GMPXX_LIBRARY} ${GMP_LIBRARY})
+#     set(GMP_INCLUDE_DIRS ${GMP_INCLUDE_DIR})
+#     include(FindPackageHandleStandardArgs)
+#     find_package_handle_standard_args(GMP DEFAULT_MSG GMP_LIBRARIES
+#         GMP_INCLUDE_DIRS)
+#     mark_as_advanced(GMP_INCLUDE_DIR GMPXX_LIBRARY GMP_LIBRARY)
+#
+
 macro (libfind_library libname pkg)
     string(TOUPPER ${pkg} PKG)
     string(TOUPPER ${libname} LIBNAME)
