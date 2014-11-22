a2fbf2113dbb04d587948c9ae62f5909b9d928bc
Ondřej Čertík <ondrej.certik@gmail.com>
2014-11-21 21:41:27 -0700

    Test that abs(x).diff(x) does not raise exception

--- csympy/tests/test_functions.py
+++ csympy/tests/test_functions.py
@@ -57,3 +57,8 @@ def test_abs():
     assert abs(-Integer(5)/3) == Integer(5)/3
     assert abs(Integer(5)/3+x) != Integer(5)/3
     assert abs(Integer(5)/3+x) == abs(Integer(5)/3+x)
+
+def test_abs_diff():
+    x = Symbol("x")
+    e = abs(x)
+    assert e.diff(x) != e
