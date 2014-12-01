a7a1d7d86267c3d19aec331a35e90bd2d920d355
Thilina Bandara Rathanyake <thilinarmtb@gmail.com>
2014-12-01 22:32:08 +0530

    Refactor `test_homogeneous_lde` to use `vec_dense_matrix_eq_perm`

--- src/tests/ntheory/test_diophantine.cpp
+++ src/tests/ntheory/test_diophantine.cpp
@@ -33,36 +33,32 @@ bool vec_dense_matrix_eq_perm(const std::vector<DenseMatrix> &a,
 
 void test_homogeneous_lde()
 {
+    std::vector<DenseMatrix> basis, true_basis;
+
     DenseMatrix A = DenseMatrix(2, 4, {
         integer(-1), integer(1), integer(2), integer(-3),
         integer(-1), integer(3), integer(-2), integer(-1)});
-
-    std::vector<DenseMatrix> basis;
     homogeneous_lde(basis, A);
+    true_basis = std::vector<DenseMatrix>{
+        DenseMatrix(1, 4, {integer(0), integer(1), integer(1), integer(1)}),
+        DenseMatrix(1, 4, {integer(4), integer(2), integer(1), integer(0)})
+    };
 
-    assert(basis.size() == 2);
-    assert(std::find(basis.begin(), basis.end(),
-        DenseMatrix(1, 4, {integer(0), integer(1), integer(1), integer(1)})) != basis.end());
-    assert(std::find(basis.begin(), basis.end(),
-        DenseMatrix(1, 4, {integer(4), integer(2), integer(1), integer(0)})) != basis.end());
+    assert(vec_dense_matrix_eq_perm(basis, true_basis));
 
     basis.clear();
     A = DenseMatrix(1, 4, {integer(-1), integer(1), integer(2), integer(-3)});
     homogeneous_lde(basis, A);
+    true_basis = std::vector<DenseMatrix>{
+        DenseMatrix(1, 4, {integer(0), integer(0), integer(3), integer(2)}),
+        DenseMatrix(1, 4, {integer(0), integer(1), integer(1), integer(1)}),
+        DenseMatrix(1, 4, {integer(0), integer(3), integer(0), integer(1)}),
+        DenseMatrix(1, 4, {integer(1), integer(0), integer(2), integer(1)}),
+        DenseMatrix(1, 4, {integer(2), integer(0), integer(1), integer(0)}),
+        DenseMatrix(1, 4, {integer(1), integer(1), integer(0), integer(0)})
+    };
 
-    assert(basis.size() == 6);
-    assert(std::find(basis.begin(), basis.end(),
-        DenseMatrix(1, 4, {integer(0), integer(0), integer(3), integer(2)})) != basis.end());
-    assert(std::find(basis.begin(), basis.end(),
-        DenseMatrix(1, 4, {integer(0), integer(1), integer(1), integer(1)})) != basis.end());
-    assert(std::find(basis.begin(), basis.end(),
-        DenseMatrix(1, 4, {integer(0), integer(3), integer(0), integer(1)})) != basis.end());
-    assert(std::find(basis.begin(), basis.end(),
-        DenseMatrix(1, 4, {integer(1), integer(0), integer(2), integer(1)})) != basis.end());
-    assert(std::find(basis.begin(), basis.end(),
-        DenseMatrix(1, 4, {integer(2), integer(0), integer(1), integer(0)})) != basis.end());
-    assert(std::find(basis.begin(), basis.end(),
-        DenseMatrix(1, 4, {integer(1), integer(1), integer(0), integer(0)})) != basis.end());
+    assert(vec_dense_matrix_eq_perm(basis, true_basis));
 }
 
 int main(int argc, char* argv[])
