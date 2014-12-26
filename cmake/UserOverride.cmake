2bf9ff7934a06608e6d47eda771cfde662974ba6
Isuru Fernando <isuruf@gmail.com>
2014-12-26 08:46:41 +0530

    Remove redundant AppleClang

--- cmake/UserOverride.cmake
+++ cmake/UserOverride.cmake
@@ -24,7 +24,7 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     set(CMAKE_CXX_FLAGS_DEBUG_INIT   "${common} -g")
 endif ()
 
-if (CMAKE_CXX_COMPILER_ID MATCHES Clang|AppleClang)
+if (CMAKE_CXX_COMPILER_ID MATCHES Clang)
     # clang
     set(common "-std=c++11 -Wall -Wextra -fPIC")
     set(CMAKE_CXX_FLAGS_RELEASE_INIT "${common} -O3 -march=native -ffast-math -funroll-loops")
