0a9724e67a408e5c2c5743760825096d53b4e8f6
Shivam Vats <shivamvats.iitkgp@gmail.com>
2015-03-03 23:31:00 +0530

    Working trivial prototype for Polynomial

--- src/polynomial.cpp
+++ src/polynomial.cpp
@@ -5,22 +5,20 @@ namespace CSymPy {
     //polynomial::polynomial (const RCP<const Basic> &var, umap_basic_num&& dict) :
    //degree{-1}, var_{var}, dict_{std::move(dict)}{}
    // void polynomial::add(const RCP<const poynomial> &b, const RCP<const polynomial> &c){
-    Polynomial::Polynomial(const std::string &var) : var_{var} {}
 
-    int Polynomial::ret_degree() {
-        if(degree!= -1)
-            return degree;
+    Polynomial::Polynomial(const std::string& var, map_integer_uint&& dict) :
+         var_{var}, dict_{std::move(dict)} {
+
         map_integer_uint::iterator it = dict_.begin();
-        int largest =(it->first)->as_int();
+        int largest = (it->first)->as_int();
         int cur;
         while(it!=dict_.end()){
-            it++;
             cur =(it->first)->as_int();
             if(cur > largest)
                 largest = cur;
+            it++;
         }
         degree = largest;
-        return degree;
     }
 
 }
--- src/polynomial.h
+++ src/polynomial.h
@@ -11,51 +11,52 @@ namespace CSymPy {
     class Polynomial : public Basic{
     public:
         int degree;
-        //const RCP<const Number> var_;
         std::string var_;
         //umap_vec_mpz dict;
         map_integer_uint dict_;
     public:
         IMPLEMENT_TYPEID(POLYNOMIAL)
         //polynomial(const RCP<const Symbol> &var, const umap_basic_num&& dict);
-    //    void add(const RCP<const polynomial> &b, const RCP<const polynomial> &c){
+        //void add(const RCP<const polynomial> &b, const RCP<const polynomial> &c){
         //polynomial(const RCP<const Number> &var) : var_{var} {}
         Polynomial() : degree{-1}, var_{-1} {}
-        Polynomial(const std::string& var, map_integer_uint&& dict) : degree{-1}, var_{var}, dict_{move(dict)} {}
-        Polynomial(const std::string &var);
+
+        Polynomial(const std::string& var, map_integer_uint&& dict);
+
+        //Polynomial(const std::string &var);
+
         int ret_degree();
 
+        std::size_t __hash__() const {return 0;}
+        //{
+            //std::hash<long long int> hash_fn;
+            //return hash_fn(this->var_);
+        //}
 
-std::size_t __hash__() const;
-//{
-    //std::hash<long long int> hash_fn;
-    //return hash_fn(this->var_);
-//}
+        bool __eq__(const Basic &o) const
+        {
+            const Polynomial &s = static_cast<const Polynomial&>(o);
+            return this->var_ == s.var_;
+        }
 
-bool __eq__(const Basic &o) const
-{
-    const Polynomial &s = static_cast<const Polynomial&>(o);
-    return this->var_ == s.var_;
-}
+        int compare(const Basic &o) const
+        {
+            const Polynomial &s = static_cast<const Polynomial&>(o);
+            if (this->var_ == s.var_) return 0;
+            return this->var_ < s.var_ ? -1 : 1;
+        }
 
-int compare(const Basic &o) const
-{
-    const Polynomial &s = static_cast<const Polynomial&>(o);
-    if (this->var_ == s.var_) return 0;
-    return this->var_ < s.var_ ? -1 : 1;
-}
+        virtual vec_basic get_args() const {return {};}
 
-virtual vec_basic get_args() const {return {};}
+        virtual void accept(Visitor &v) const;
 
-virtual void accept(Visitor &v) const;
+    }; //Polynomial
 
-};
-
-    inline RCP<const Polynomial> polynomial(std::string i)
-{
-    return rcp(new Polynomial(i));
-}
-}
+    inline RCP<const Polynomial> polynomial(std::string i, map_integer_uint&& dict)
+    {
+        return rcp(new Polynomial(i, std::move(dict)));
+    }
+}  //CSymPy
 
 #endif
 
