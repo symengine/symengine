ecfa0f435bf3841b77c61df948a6f4489a1e5d43
Isuru Fernando <isuruf@gmail.com>
2015-05-20 10:29:50 +0530

    Added eval_complex_double

--- src/tests/eval/test_eval_double.cpp
+++ src/tests/eval/test_eval_double.cpp
@@ -14,6 +14,8 @@
 #include "eval_double.h"
 
 using SymEngine::Basic;
+using SymEngine::Complex;
+using SymEngine::Rational;
 using SymEngine::symbol;
 using SymEngine::integer;
 using SymEngine::one;
@@ -63,10 +65,12 @@ void test_eval_double()
         { mul(r1, r2), 0.403422680111 },
         { pow(r1, r2), 0.920580670898 },
         { tan(pow(r1, r2)), 1.314847038576 },
-        { add(asin(r3), add(acos(r3), add(atan(r3), add(asec(r4), add(acsc(r4), acot(r4)))))), 3.536383773289 },
+        { add(sin(r3), add(cos(r4), add(tan(r3), add(sec(integer(6)), add(csc(r4), cot(r4)))))), 0.387875350057 },
+        { add(asin(r3), add(acos(r3), add(atan(r3), add(asec(integer(6)), add(acsc(r4), acot(r4)))))), 3.570293614860 },
         { add(sinh(one), add(cosh(one), add(tanh(one), coth(one)))), 4.792911269914 },
         { add(asinh(r4), add(acosh(r4), add(atanh(r3), acoth(r4)))), 5.010335118942 },
-        { SymEngine::abs(log(div(pi, mul(E, integer(2))))), 0.548417294710 }
+        { SymEngine::abs(log(div(pi, mul(E, integer(2))))), 0.548417294710 },
+        { SymEngine::atan2(r1, neg(r2)), 2.08867384922582 }
     };
 
     for (unsigned i = 0; i < vec.size(); i++) {
@@ -95,11 +99,47 @@ void test_eval_double()
     // ... we don't test the rest of functions that are not implemented.
 }
 
+void test_eval_complex_double()
+{
+    RCP<const Basic> r1, r2, r3, r4;
+    r1 = sin(pow(integer(-5), div(integer(1), integer(2))));
+    r2 = asin(Complex::from_two_nums(*integer(1), *integer(2)));
+    r3 = Complex::from_two_nums(*Rational::from_mpq(mpq_class(3, 5)), *Rational::from_mpq(mpq_class(4, 5)));
+    r4 = Complex::from_two_nums(*Rational::from_mpq(mpq_class(5, 13)), *Rational::from_mpq(mpq_class(12, 13)));
+
+    std::vector<std::pair<RCP<const Basic>, std::complex<double>>> vec = {
+        { r1, std::complex<double>(0.0, 4.624795545470) },
+        { r2, std::complex<double>(0.427078586392476, 1.52857091948100)},
+        { sub(add(pow(r1, r2), div(r1, r2)), r1), std::complex<double>(2.63366236495646, -3.81810521824194) },
+        { add(sin(r3), add(neg(cos(r4)), add(tan(r3), add(neg(cot(r4)), add(sec(r3), neg(csc(r4))))))),
+            std::complex<double>(-0.235180108309931, 4.27879468879223) },
+        { add(asin(r3), add(neg(acos(r4)), add(atan(r3), add(neg(acot(r4)), add(asec(r3), neg(acsc(r4))))))),
+                    std::complex<double>(0.0, 4.67018141674983) },
+        { add(sinh(r3), add(neg(cosh(r4)), add(tanh(r3), neg(coth(r4))))),
+                std::complex<double>(0.106911448328291, 1.70522247007143) },
+        { add(asinh(r3), add(neg(acosh(r4)), add(atanh(r3), neg(acoth(r4))))),
+                std::complex<double>(0.00319030587888230, 0.965754104505096) },
+        { log(div(pi, mul(E, r1))),
+                std::complex<double>(-1.38670227775307, -1.57079632679490) },
+        { abs(r1), std::complex<double>(4.62479554547038, 0.0) }
+    };
+
+    for (unsigned i = 0; i < vec.size(); i++) {
+        std::complex<double> val = eval_complex_double(*vec[i].first);
+        std::cout.precision(12);
+        std::cout << vec[i].first->__str__() << " ~ " << val << std::endl;
+        assert(std::abs(val.imag() - vec[i].second.imag()) < 1e-12);
+        assert(std::abs(val.real() - vec[i].second.real()) < 1e-12);
+    }
+}
+
 int main(int argc, char* argv[])
 {
     print_stack_on_segfault();
 
     test_eval_double();
+
+    test_eval_complex_double();
 
     return 0;
 }
