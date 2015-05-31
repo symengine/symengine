f6508b8bda4815d2774c5eaf6f4ecdfcf7ab86ec
Sumith <sumith1896@gmail.com>
2015-05-31 14:39:10 +0530

    Updated add_poly() and added test_add_poly()

--- src/polynomial.cpp
+++ src/polynomial.cpp
@@ -21,13 +21,9 @@ namespace SymEngine {
     RCP<const Polynomial> add_poly(const Polynomial &a, const Polynomial &b) {
         map_uint_mpz dict;
         for(auto &it : a.dict_)
-            dict[it.first] = 0;
+            dict[it.first] = it.second;
         for(auto &it : b.dict_)
-            dict[it.first] = 0;
-        for(auto &it : a.dict_)
-            dict[it.first] = dict[it.first] + it.second;
-        for(auto &it : b.dict_)
-            dict[it.first] = dict[it.first] + it.second;
+            dict[it.first] += it.second;
 
         RCP<const Polynomial> c = polynomial(a.var_, std::move(dict));
         return c;
--- src/tests/basic/test_polynomial.cpp
+++ src/tests/basic/test_polynomial.cpp
@@ -7,6 +7,8 @@ using SymEngine::Polynomial;
 using SymEngine::polynomial;
 using SymEngine::RCP;
 using SymEngine::print_stack_on_segfault;
+using SymEngine::map_uint_mpz;
+using SymEngine::Basic;
 
 void polynomial_constructor()
 {   
@@ -15,10 +17,25 @@ void polynomial_constructor()
     assert(P->degree == 2);
 }
 
+void test_add_poly()
+{
+	std::string x = "x";
+	map_uint_mpz adict_ = {{0, 1}, {1, 2}, {2, 1}};
+	map_uint_mpz bdict_ = {{0, 2}, {1, 3}, {2, 4}};
+	const Polynomial a(x, std::move(adict_));
+	const Polynomial b(x, std::move(bdict_));
+
+	RCP<const Basic> c = add_poly(a, b);
+	//std::cout<<c->__str__();
+	assert(c->__str__() == "5*x**2 + 5*x + 3");
+}
+
 int main(int argc, char* argv[])
 {
     print_stack_on_segfault();
 
     polynomial_constructor();
+
+    test_add_poly();
     return 0;
 }
