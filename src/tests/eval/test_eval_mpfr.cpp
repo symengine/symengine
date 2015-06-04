9bcd46638b090a92e2d5adac2528fa83aa00684c
Isuru Fernando <isuruf@gmail.com>
2015-06-04 11:18:20 +0530

    Improve eval_mpfr tests

--- src/tests/eval/test_eval_mpfr.cpp
+++ src/tests/eval/test_eval_mpfr.cpp
@@ -1,52 +1,46 @@
-#include <limits>
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
-
 #include "basic.h"
 #include "integer.h"
-#include "rational.h"
 #include "add.h"
 #include "mul.h"
-#include "pow.h"
-#include "functions.h"
 #include "eval_mpfr.h"
-#include "eval_arb.h"
 #include "constants.h"
-#include "real_double.h"
 
 using SymEngine::RCP;
 using SymEngine::Basic;
-using SymEngine::Rational;
 using SymEngine::integer;
 using SymEngine::pi;
 using SymEngine::mul;
-using SymEngine::div;
+using SymEngine::sub;
 using SymEngine::eval_mpfr;
 using SymEngine::print_stack_on_segfault;
 
 void test_precision()
 {
-    mpfr_t a, b;
-    mpz_t i;
+    mpfr_t a;
     mpfr_init2(a, 53);
-    mpfr_init2(b, 40);
-    RCP<const Basic> r = mul(pi, integer(1963319607));
-    mpz_init_set_ui(i, 6167950454);
+    RCP<const Basic> s = mul(pi, integer(1963319607));
+    RCP<const Basic> t = integer(mpz_class(6167950454));
+    RCP<const Basic> r = sub(s, t);
+    // value of `r` is approximately 0.000000000149734291
 
     eval_mpfr(a, *r, MPFR_RNDN);
-    mpfr_set_z(b, i, MPFR_RNDN);
-    // `a` and `b` are equal as `eval_mpfr` was done with a precision of 53 bits (precision of `a`)
-    assert(mpfr_cmp(a, b) == 0);
+    // `eval_mpfr` was done with a precision of 53 bits (precision of `a`) and rounding mode `MPFR_RNDN`
+    // With 53 bit precision, `s` and `t` have the same value.
+    // Hence value of `r` was  rounded down to `0.000000000000000`
+    assert(mpfr_cmp_si(a, 0) == 0);
 
     mpfr_set_prec(a, 100);
     eval_mpfr(a, *r, MPFR_RNDN);
-    // `a` and `b` are not equal as `eval_mpfr` was done with a precision of 100 bits
-    assert(mpfr_cmp(a, b) == 1);
+    // `eval_mpfr` was done with a precision of 100 bits (precision of `a`) and rounding mode `MPFR_RNDN`
+    // With 100 bit precision, `s` and `t` are not equal in value.
+    // Value of `r` is a positive quantity with value 0.000000000149734291.....
+    assert(mpfr_cmp_si(a, 0) == 1);
 
-    mpz_clear(i);
+    // Check that value of `r` (`a`) starts with 0.000000000149734291
+    assert(mpfr_cmp_d(a, 0.000000000149734291) == 1);
+    assert(mpfr_cmp_d(a, 0.000000000149734292) == -1);
+
     mpfr_clear(a);
-    mpfr_clear(b);
 }
 
 int main(int argc, char* argv[])
@@ -54,6 +48,8 @@ int main(int argc, char* argv[])
     print_stack_on_segfault();
 
     test_precision();
+
+    mpfr_free_cache();
 
     return 0;
 }
