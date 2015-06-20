7e53331f5fbb44c67ac28f87f118018ba098c9c5
Isuru Fernando <isuruf@gmail.com>
2015-06-20 14:40:46 +0530

    Build gmp from source

--- bin/install_travis.sh
+++ bin/install_travis.sh
@@ -1,21 +1,32 @@
 #!/usr/bin/env bash
 
-if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
+if [[ "${CC}" == "" ]]; then
+    export CC=gcc
+    export CXX=g++
+fi
+if [[ "${TRAVIS_OS_NAME}" == "osx" ]] && [[ "${CC}" == "gcc" ]]; then
+    export CC=gcc-4.8
+    export CXX=g++-4.8
+fi
+
+if [[ "${TRAVIS_OS_NAME}" != "osx" ]]; then
     sudo apt-get update
     sudo apt-get install cmake libgmp-dev
 else
-    brew update
-    brew install cmake gmp
+    brew install cmake
+    brew uninstall gmp --force
+    brew install --build-from-source gmp
 fi
+
 if [[ "${WITH_BFD}" == "yes" ]]; then
-    if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
+    if [[ "${TRAVIS_OS_NAME}" != "osx" ]]; then
         sudo apt-get install binutils-dev;
     else
         brew install binutils;
     fi
 fi
 if [[ "${WITH_ECM}" == "yes" ]]; then
-    if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
+    if [[ "${TRAVIS_OS_NAME}" != "osx" ]]; then
         sudo apt-get install libecm-dev;
     else
         wget http://gforge.inria.fr/frs/download.php/file/32159/ecm-6.4.4.tar.gz;
@@ -29,7 +40,7 @@ if [[ "${WITH_PRIMESIEVE}" == "yes" ]]; then
     cd primesieve-5.2 && ./configure && make -j8 && sudo make install && cd ..;
 fi
 if [[ "${WITH_MPFR}" == "yes" ]] || [[ "${WITH_MPC}" == "yes" ]] || [[ "${WITH_ARB}" == "yes" ]]; then
-    if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
+    if [[ "${TRAVIS_OS_NAME}" != "osx" ]]; then
         sudo apt-get install libmpfr-dev;
     else
         brew install mpfr;
@@ -44,7 +55,7 @@ if [[ "${WITH_ARB}" == "yes" ]]; then
     cd arb-2.2.0 && ./configure && make -j8 ARB_GMP_LIB_DIR=/usr/lib/x86_64-linux-gnu ARB_MPFR_LIB_DIR=/usr/lib/x86_64-linux-gnu && sudo make ARB_GMP_LIB_DIR=/usr/lib/x86_64-linux-gnu ARB_MPFR_LIB_DIR=/usr/lib/x86_64-linux-gnu install && cd ..;
 fi
 if [[ "${WITH_MPC}" == "yes" ]]; then
-    if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
+    if [[ "${TRAVIS_OS_NAME}" != "osx" ]]; then
         sudo apt-get install libmpc-dev;
     else
         brew install libmpc;
@@ -52,7 +63,7 @@ if [[ "${WITH_MPC}" == "yes" ]]; then
 fi
 # Install python using Miniconda.
 if [[ "${WITH_PYTHON}" == "yes" ]] || [[ "${PYTHON_INSTALL}" == "yes" ]]; then
-    if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
+    if [[ "${TRAVIS_OS_NAME}" != "osx" ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
     else
         wget https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh -O miniconda.sh;
@@ -66,12 +77,4 @@ if [[ "${WITH_PYTHON}" == "yes" ]] || [[ "${PYTHON_INSTALL}" == "yes" ]]; then
 
     conda create -q -n test-environment python="${PYTHON_VERSION}" pip cython sympy nose pytest;
     source activate test-environment;
-fi
-if [[ "${CC}" == "" ]]; then
-    export CC=gcc
-    export CXX=g++
-fi
-if [[ "$TRAVIS_OS_NAME" == "osx" ]] && [[ "$CC" == "gcc" ]]; then
-    export CC=gcc-4.8
-    export CXX=g++-4.8
 fi
