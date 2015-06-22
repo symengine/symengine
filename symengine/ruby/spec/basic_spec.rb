143f27c37920d5a915e7c3e9f01a2aa9710c3707
Abinash Meher <abinashdakshana999@gmail.com>
2015-06-23 01:20:40 +0530

    Added the tests for symbol from basic
    Signed-off-by: Abinash Meher <abinashdakshana999@gmail.com>

--- symengine/ruby/spec/basic_spec.rb
+++ symengine/ruby/spec/basic_spec.rb
@@ -16,56 +16,5 @@ describe SymEngine do
                 end
             end
         end
-
-        describe 'binary operations' do
-            before :each do
-                @a = SymEngine::Basic.new
-                @b = SymEngine::Basic.new
-            end
-            describe '#+' do
-                context 'with another Basic object as argument' do
-                    xit 'returns a Basic object that is result of
-                        self added to the argument' do
-                    end
-                end
-            end
-            describe '#-' do
-                context 'with another Basic object as argument' do
-                    xit 'returns a Basic object that is result of
-                        argument subtracted from self' do
-                    end
-                end
-            end
-            describe '#*' do
-                context 'with another Basic object as argument' do
-                    xit 'returns a Basic object that is result of
-                        self multiplied by the argument' do
-                    end
-                end
-            end
-            describe '#/' do
-                context 'with another Basic object as argument' do
-                    xit 'returns a Basic object that is result of
-                        self divided by the argument' do
-                    end
-                end
-            end
-            describe '#**' do
-                context 'with another Basic object as argument' do
-                    xit 'returns a Basic object that is result of
-                        self raised to the power of argument' do
-                    end
-                end
-            end
-        end
-
-        describe 'unary operations' do
-            describe '#-@' do
-                context "doesn't take any argument" do
-                    xit 'returns the negation of self' do
-                    end
-                end
-            end
-        end
     end
 end
