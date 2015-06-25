283be8bff2605bdf7290cdb094577a7926173575
Abinash Meher <abinashdakshana999@gmail.com>
2015-06-25 20:43:07 +0530

    Moved the symbol related files to symengine/ruby

--- symengine/ruby/ext/symengine/ruby_symbol.h
+++ symengine/ruby/ext/symengine/ruby_symbol.h
@@ -1,9 +1,14 @@
+#ifndef RUBY_SYMBOL_H_
+#define RUBY_SYMBOL_H_
+
 #include "ruby.h"
 #include "ruby_basic.h"
-#include "cwrapper.h"
+#include "symengine/cwrapper.h"
 
 void csymbol_free(void *ptr);
 
 VALUE csymbol_alloc(VALUE klass);
 
 VALUE csymbol_init(VALUE self, VALUE name);
+
+#endif //RUBY_SYMBOL_H_
