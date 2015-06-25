5db823cf4683f3d70969d3cdf2467a74eee5013a
Ondřej Čertík <ondrej.certik@gmail.com>
2015-06-25 09:10:31 -0600

    Move the get_rcp* function bodies into basic-inl.h

--- symengine/basic-inl.h
+++ symengine/basic-inl.h
@@ -9,11 +9,26 @@ inline std::size_t Basic::hash() const
         hash_ = __hash__();
     return hash_;
 }
+
 //! \return true if not equal    
 inline bool Basic::__neq__(const Basic &o) const
 {
     return !(this->__eq__(o));
 }
+
+inline RCP<const Basic> Basic::get_rcp() const {
+    return get_rcp_cast<const Basic>();
+}
+
+template <class T>
+inline RCP<T> Basic::get_rcp_cast() const {
+#if defined(WITH_SYMENGINE_RCP)
+    return rcp(static_cast<T*>(this));
+#else
+    return rcp_static_cast<T>(weak_self_ptr_.create_strong());
+#endif
+}
+
 //! \return true if  `a` equal `b`
 inline bool eq(const RCP<const Basic> &a, const RCP<const Basic> &b)
 {
