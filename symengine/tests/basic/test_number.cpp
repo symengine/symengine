d2a957fb5b8291bb9d31bf340a71a3d6292da35c
Isuru Fernando <isuruf@gmail.com>
2015-06-27 21:06:03 +0530

    Tests for ComplexMPC operations

--- symengine/tests/basic/test_number.cpp
+++ symengine/tests/basic/test_number.cpp
@@ -30,37 +30,71 @@ TEST_CASE("RealMPFR: arithmetic", "[number]")
     mpfr_set_ui(b.get_mpfr_t(), 20, MPFR_RNDN);
     mpfr_set_ui(c.get_mpfr_t(), 100, MPFR_RNDN);
     mpfr_set_ui(d.get_mpfr_t(), 1024, MPFR_RNDN);
-    RCP<const Number> r1 = real_mpfr(a);
-    RCP<const Number> r2 = real_mpfr(b);
-    RCP<const Number> r3 = real_mpfr(c);
-    RCP<const Number> r4 = real_mpfr(d);
+    RCP<const Number> r1 = real_mpfr(std::move(a));
+    RCP<const Number> r2 = real_mpfr(std::move(b));
+    RCP<const Number> r3 = real_mpfr(std::move(c));
+    RCP<const Number> r4 = real_mpfr(std::move(d));
     RCP<const Number> r5 = subnum(integer(0), r1);
     RCP<const Number> i1 = integer(1);
     RCP<const Number> i2 = integer(2);
     RCP<const Number> half = integer(1)->div(*integer(2));
     RCP<const Number> c1 = Complex::from_two_nums(*i1, *i1);
 
-    assert(eq(r2, addnum(r1, r1)));
-    assert(eq(r2, mulnum(r1, i2)));
-    assert(eq(r2, mulnum(i2, r1)));
-    assert(eq(r1, divnum(r2, i2)));
-    assert(eq(divnum(i1, r1), divnum(i2, r2)));
-    assert(eq(r1, subnum(r2, r1)));
-    assert(eq(r1, subnum(integer(20), r1)));
-    assert(eq(r1, mulnum(r2, half)));
-    assert(eq(r3, pownum(r1, i2)));
-    assert(eq(r4, pownum(i2, r1)));
-    assert(eq(r1, pownum(r3, half)));
-    assert(eq(divnum(i1, r4), half->pow(*r1)));
+    REQUIRE(eq(r2, addnum(r1, r1)));
+    REQUIRE(eq(r2, mulnum(r1, i2)));
+    REQUIRE(eq(r2, mulnum(i2, r1)));
+    REQUIRE(eq(r1, divnum(r2, i2)));
+    REQUIRE(eq(divnum(i1, r1), divnum(i2, r2)));
+    REQUIRE(eq(r1, subnum(r2, r1)));
+    REQUIRE(eq(r1, subnum(integer(20), r1)));
+    REQUIRE(eq(r1, mulnum(r2, half)));
+    REQUIRE(eq(r3, pownum(r1, i2)));
+    REQUIRE(eq(r4, pownum(i2, r1)));
+    REQUIRE(eq(r1, pownum(r3, half)));
+    REQUIRE(eq(divnum(i1, r4), half->pow(*r1)));
 #ifdef HAVE_SYMENGINE_MPC
-    assert(is_a<ComplexMPC>(*addnum(r1, c1)));
-    assert(is_a<ComplexMPC>(*mulnum(c1, r2)));
-    assert(is_a<ComplexMPC>(*pownum(r5, half)));
-    assert(is_a<ComplexMPC>(*pownum(integer(-2), r5)));
+    REQUIRE(is_a<ComplexMPC>(*addnum(r1, c1)));
+    REQUIRE(is_a<ComplexMPC>(*mulnum(c1, r2)));
+    REQUIRE(is_a<ComplexMPC>(*pownum(r5, half)));
+    REQUIRE(is_a<ComplexMPC>(*pownum(integer(-2), r5)));
 #else
     SYMENGINE_CHECK_THROW(addnum(r1, c1), std::runtime_error);
     SYMENGINE_CHECK_THROW(pownum(r5, half), std::runtime_error);
     SYMENGINE_CHECK_THROW(pownum(integer(-2), r1), std::runtime_error);
 #endif //HAVE_SYMENGINE_MPC
 #endif //HAVE_SYMENGINE_MPFR
+}
+
+TEST_CASE("ComplexMPC: arithmetic", "[number]")
+{
+#ifdef HAVE_SYMENGINE_MPC
+    mpc_class a(100), b(100), d(100), c(100);
+    mpc_set_ui_ui(a.get_mpc_t(), 10, 7, MPFR_RNDN);
+    mpc_set_ui_ui(b.get_mpc_t(), 20, 14, MPFR_RNDN);
+    mpc_pow_ui(c.get_mpc_t(), a.get_mpc_t(), 2, MPFR_RNDN);
+    mpc_set_ui(d.get_mpc_t(), 2, MPFR_RNDN);
+    mpc_pow_ui(d.get_mpc_t(), d.get_mpc_t(), 10, MPFR_RNDN);
+
+    RCP<const Number> r1 = complex_mpc(std::move(a));
+    RCP<const Number> r2 = complex_mpc(std::move(b));
+    RCP<const Number> r3 = complex_mpc(std::move(c));
+    RCP<const Number> r4 = complex_mpc(std::move(d));
+    RCP<const Number> i1 = integer(1);
+    RCP<const Number> i2 = integer(2);
+    RCP<const Number> half = integer(1)->div(*integer(2));
+    RCP<const Number> c1 = Complex::from_two_nums(*integer(10), *integer(7));
+
+    REQUIRE(eq(r2, addnum(c1, r1)));
+    REQUIRE(eq(r2, mulnum(r1, i2)));
+    REQUIRE(eq(r2, mulnum(i2, r1)));
+    REQUIRE(eq(r1, divnum(r2, i2)));
+    REQUIRE(eq(divnum(i1, r1), divnum(i2, r2)));
+    REQUIRE(eq(r1, subnum(r2, r1)));
+    REQUIRE(eq(r1, subnum(mulnum(c1, i2), r1)));
+    REQUIRE(eq(r1, mulnum(r2, half)));
+    REQUIRE(eq(r3, pownum(r1, i2)));
+    REQUIRE(eq(r4, pownum(i2, r1)));
+    REQUIRE(eq(r1, pownum(r3, half)));
+    REQUIRE(eq(divnum(i1, r4), half->pow(*r1)));
+#endif //HAVE_SYMENGINE_MPC
 }
