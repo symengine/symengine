0fd5febb372a00f3b7300fe328506de12f5d9f66
Sumith <sumith1896@gmail.com>
2015-07-01 18:10:00 +0530

    Asserting updated to == instead of = (assignment)

--- symengine/dense_matrix.cpp
+++ symengine/dense_matrix.cpp
@@ -620,7 +620,7 @@ void fraction_free_gauss_jordan_solve(const DenseMatrix &A, const DenseMatrix &b
 {
     SYMENGINE_ASSERT(A.row_ == A.col_);
     SYMENGINE_ASSERT(b.row_ == A.row_ && x.row_ == A.row_);
-    SYMENGINE_ASSERT(x.col_ = b.col_);
+    SYMENGINE_ASSERT(x.col_ == b.col_);
 
     unsigned i, j, k, col = A.col_, bcol = b.col_;
     RCP<const Basic> d;
