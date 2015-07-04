eb0fa62d56672d29146ba1846744cfd8083fff52
Ondřej Čertík <ondrej.certik@gmail.com>
2015-07-03 23:00:53 -0600

    Refactor the assert macros

--- symengine/symengine_assert.h
+++ symengine/symengine_assert.h
@@ -1,21 +1,21 @@
 #ifndef SYMENGINE_ASSERT_H
 #define SYMENGINE_ASSERT_H
 
-// SYMENGINE_ASSERT uses internal functions to perform as assert
-// so that there is no effect with NDEBUG
-#if !defined(SYMENGINE_ASSERT)
-#if defined(WITH_SYMENGINE_ASSERT)
-#define stringize(s) #s
-#define XSTR(s) stringize(s)
-#define SYMENGINE_ASSERT(cond) \
+#define SYMENGINE_ASSERT_DO(cond) \
 { \
 if (0 == (cond)) { \
 std::cerr << "SYMENGINE_ASSERT failed: " << __FILE__ << \
 	"\nfunction " << __func__ <<  "(), line number " << __LINE__ << \
-	" at \n" << XSTR(cond) << "\n"; \
+	" at \n" << #cond << "\n"; \
 abort(); \
 } \
 }
+
+// SYMENGINE_ASSERT uses internal functions to perform as assert
+// so that there is no effect with NDEBUG
+#if !defined(SYMENGINE_ASSERT)
+#if defined(WITH_SYMENGINE_ASSERT)
+#define SYMENGINE_ASSERT(cond) SYMENGINE_ASSERT_DO(cond)
 #else
 #define SYMENGINE_ASSERT(cond)
 #endif
