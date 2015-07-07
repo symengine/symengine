46b821f2b5fba5f23b544532283f92a0aab32555
Sumith <sumith1896@gmail.com>
2015-07-07 19:23:47 +0530

    Add comments and fix syntax

--- symengine/expression.h
+++ symengine/expression.h
@@ -24,101 +24,103 @@ namespace SymEngine
 class Expression
 {
 private:
-	RCP<const Basic> m_basic;
+    RCP<const Basic> m_basic;
 
 public:
-	Expression():m_basic(new Integer(0)) {}
-
-	template <typename T, typename = typename std::enable_if<std::is_constructible<RCP<const Basic>,T &&>::value>::type>
-	Expression(T &&o):m_basic(std::forward<T>(o)) {}
-
-	Expression(int n):m_basic(new Integer(n)) {}
-
-	Expression(const Expression &) = default;
-
-	Expression(Expression &&other) noexcept : m_basic(std::move(other.m_basic)) {}
-
-	Expression &operator=(const Expression &) = default;
-	Expression &operator=(Expression &&other) noexcept
-	{
-		if (this != &other) {
-			*this = std::move(other);
-		}
-		return *this;
-	}
-
-	~Expression() noexcept {}
-
-	friend std::ostream &operator<<(std::ostream &os, const Expression &expr)
-	{
-		os << (*expr.m_basic.get());
-		return os;
-	}
-
-	friend Expression operator+(const Expression &a, const Expression &b)
-	{
-		return Expression(add(a.m_basic,b.m_basic));
-	}
-
-	Expression &operator+=(const Expression &other)
-	{
-		m_basic = add(m_basic,other.m_basic);
-		return *this;
-	}
-
-	friend Expression operator-(const Expression &a, const Expression &b)
-	{
-		return Expression(sub(a.m_basic,b.m_basic));
-	}
-
-	Expression operator-() const
-	{
-		Expression retval(*this);
-		retval *= -1;
-		return retval;
-	}
-
-	Expression &operator-=(const Expression &other)
-	{
-		m_basic = sub(m_basic,other.m_basic);
-		return *this;
-	}
-
-	friend Expression operator*(const Expression &a, const Expression &b)
-	{
-		return Expression(mul(a.m_basic,b.m_basic));
-	}
-
-	Expression &operator*=(const Expression &other)
-	{
-		m_basic = mul(m_basic,other.m_basic);
-		return *this;
-	}
-
-	bool operator==(const Expression &other) const
-	{
-		return eq(m_basic,other.m_basic);
-	}
-
-	bool operator!=(const Expression &other) const
-	{
-		return !(*this == other);
-	}
-
-	const RCP<const Basic> &get_basic() const
-	{
-		return m_basic;
-	}
+    //! Plain constructor of Expression
+    Expression() : m_basic(integer(0)) {}
+    //! Construct Expression from `int`
+    Expression(int n) : m_basic(integer(n)) {}
+    //! Construct Expression from Basic
+    template <typename T, typename = typename std::enable_if<std::is_constructible<RCP<const Basic>, T &&>::value>::type>
+    Expression(T &&o) : m_basic(std::forward<T>(o)) {}
+    //! Construct Expression from Expression
+    Expression(const Expression &) = default;
+    //! Construct Expression from reference to Expression
+    Expression(Expression &&other) noexcept : m_basic(std::move(other.m_basic)) {}
+    //! Overload assignment operator
+    Expression &operator=(const Expression &) = default;
+    //! Overload assignment operator for reference
+    Expression &operator=(Expression &&other) noexcept
+    {
+        if (this != &other) {
+            *this = std::move(other);
+        }
+        return *this;
+    }
+    //! Destructor of Expression
+    ~Expression() noexcept {}
+    //! Overload stream operator
+    friend std::ostream &operator<<(std::ostream &os, const Expression &expr)
+    {
+        os << (*expr.m_basic.get());
+        return os;
+    }
+    //! Overload addition
+    friend Expression operator+(const Expression &a, const Expression &b)
+    {
+        return Expression(add(a.m_basic, b.m_basic));
+    }
+    //! Overload addition and assignment(+=)
+    Expression &operator+=(const Expression &other)
+    {
+        m_basic = add(m_basic, other.m_basic);
+        return *this;
+    }
+    //! Overload subtraction
+    friend Expression operator-(const Expression &a, const Expression &b)
+    {
+        return Expression(sub(a.m_basic, b.m_basic));
+    }
+    //! Overload unary negative
+    Expression operator-() const
+    {
+        Expression retval(*this);
+        retval *= -1;
+        return retval;
+    }
+    //! Overload subtraction and assignment(-=)	
+    Expression &operator-=(const Expression &other)
+    {
+        m_basic = sub(m_basic, other.m_basic);
+        return *this;
+    }
+    //! Overload multiplication
+    friend Expression operator*(const Expression &a, const Expression &b)
+    {
+        return Expression(mul(a.m_basic, b.m_basic));
+    }
+    //! Overload multiplication and assignment (*=)
+    Expression &operator*=(const Expression &other)
+    {
+        m_basic = mul(m_basic, other.m_basic);
+        return *this;
+    }
+    //! Overload check equality (==)
+    bool operator==(const Expression &other) const
+    {
+        return eq(m_basic, other.m_basic);
+    }
+    //! Overload check not equal (!=)
+    bool operator!=(const Expression &other) const
+    {
+        return !(*this == other);
+    }
+    //! Method to get Basic from Expression
+    const RCP<const Basic> &get_basic() const
+    {
+        return m_basic;
+    }
 };
 
 inline Expression pow(const Expression &base, const Expression &exp)
 {
-	return pow(base.get_basic(),exp.get_basic());
+    return pow(base.get_basic(), exp.get_basic());
 }
 
 inline Expression expand(const Expression &arg)
 {
-	return expand(arg.get_basic());
+    return expand(arg.get_basic());
 }
 
 } // SymEngine
--- symengine/tests/expression/test_expression.cpp
+++ symengine/tests/expression/test_expression.cpp
@@ -1,40 +1,39 @@
 #include "catch.hpp"
-#include <chrono>
 #include <iostream>
+#include <chrono>
 
-#include <symengine/symengine_rcp.h>
 #include <symengine/expression.h>
-#include <symengine/pow.h>
-#include <symengine/symbol.h>
 
-using namespace SymEngine;
+using SymEngine::Expression;
+using SymEngine::symbol;
 
 TEST_CASE("Constructors of Expression", "[Expression]")
 {
-	Expression e0 = symbol("x");
+    Expression e0 = symbol("x");
 }
 
 TEST_CASE("Printing of Expression", "[Expression]")
 {
-	Expression e0 = symbol("x");
-	std::cout << e0 << '\n';
+    Expression e0 = symbol("x");
+    std::cout << e0 << std::endl;
 }
 
 TEST_CASE("Arithmetic of Expression", "[Expression]")
 {
-	Expression x = symbol("x"), y = symbol("y");
-	auto z = x + y;
-	std::cout << z << '\n';
-	z += y;
-	std::cout << z << '\n';
-	REQUIRE(z == x + y + y);
-	REQUIRE(z == x + 2 * y);
-	std::cout << pow(z,z) << '\n';
-	std::cout << pow(z,45) << '\n';
-	auto t1 = std::chrono::high_resolution_clock::now();
-	auto res = expand(pow(z,45) * pow(z,45));
-	std::cout
-		<< std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now()-t1).count()
-		<< "ns" << std::endl;
-	std::cout << res << '\n';
+    Expression x = symbol("x"), y = symbol("y");
+    auto z = x + y;
+    std::cout << z << std::endl;
+    z += y;
+    std::cout << z << std::endl;
+    REQUIRE(z == x + y + y);
+    REQUIRE(z == x + 2 * y);
+    std::cout << pow(z, z) << std::endl;
+    std::cout << pow(z, 45) << std::endl;
+    auto t1 = std::chrono::high_resolution_clock::now();
+    auto res = expand(pow(z, 45) * pow(z, 45));
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::cout
+        << std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count()
+        << "ns" << std::endl;
+    std::cout << res << std::endl;
 }
