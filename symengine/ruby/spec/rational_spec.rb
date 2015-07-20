2d5811bed364cd559eaaea8e789d672f8e6ed2a6
Abinash Meher <abinashdakshana999@gmail.com>
2015-07-21 02:22:37 +0530

    Added tests for instantiation of Rational

--- symengine/ruby/spec/rational_spec.rb
+++ symengine/ruby/spec/rational_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe SymEngine do
-    before :each do
+  describe SymEngine::Rational do
+    describe '.new' do
+      context 'with a Ruby Rational object as input' do
+        it 'returns an instance of SymEngine::Rational class' do
+          a = Rational('2/3')
+          a = SymEngine::Rational.new(a)
+          expect(a).to be_an_instance_of SymEngine::Rational
+          expect(a.to_s).to eq('2/3')
+        end
+      end
     end
+  end
 end
