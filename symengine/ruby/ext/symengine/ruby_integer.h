ea5e463da3f5d031d27a922e0776b61976210f49
Abinash Meher <abinashdakshana999@gmail.com>
2015-07-21 02:20:16 +0530

    Made a macro to get Integer from T_INTEGER VALUE

--- symengine/ruby/ext/symengine/ruby_integer.h
+++ symengine/ruby/ext/symengine/ruby_integer.h
@@ -3,6 +3,19 @@
 
 #include "ruby_basic.h"
 
+#define GET_SYMINTFROMVAL(num_value, this) \
+    switch(TYPE(num_value)) { \
+        case T_FIXNUM: \
+            integer_set_si(this, FIX2LONG(num_value)); \
+            break; \
+        case T_BIGNUM: \
+            integer_set_ui(this, NUM2ULONG(num_value)); \
+            break; \
+        default: \
+            rb_raise(rb_eTypeError, "Invalid Type: Fixnum or Bignum required"); \
+            break; \
+    } \
+
 VALUE cinteger_init(VALUE self, VALUE num_value);
 
 #endif //RUBY_INTEGER_H_
