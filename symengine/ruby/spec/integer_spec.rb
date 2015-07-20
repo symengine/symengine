af08997f9b365ecaf1935f7c86b758db4f25e650
Abinash Meher <abinashdakshana999@gmail.com>
2015-07-21 00:51:01 +0530

    Added tests for `#new` in Integer

--- symengine/ruby/spec/integer_spec.rb
+++ symengine/ruby/spec/integer_spec.rb
@@ -1,6 +1,23 @@
 require 'spec_helper'
 
 describe SymEngine do
-    before :each do
+  describe SymEngine::Integer do
+    describe '#new' do
+      context 'with Integer as argument' do
+        it 'gives a new SymEngine::Integer instance' do
+          a = SymEngine::Integer.new(123)
+          b = SymEngine::Integer.new(-123)
+          expect(a).to be_an_instance_of SymEngine::Integer
+          expect(b).to be_an_instance_of SymEngine::Integer
+          expect(a.to_s).to eq('123')
+          expect(b.to_s).to eq('-123')
+          c = SymEngine::Integer.new(12_345_678_912_345_678_912)
+          expect(c.to_s).to eq('12345678912345678912')
+          expect do
+            SymEngine::Integer.new(-12_345_678_912_345_678_912)
+          end.to raise_error(RangeError)
+        end
+      end
     end
+  end
 end
