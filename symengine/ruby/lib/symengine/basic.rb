04fcb179927eccede0bb45ef6d6c5c98e8060ea9
Abinash Meher <abinashdakshana999@gmail.com>
2015-07-28 23:53:47 +0530

    Implemented class coercion Integer and Rational can now be coerced into SymEngine classes

--- symengine/ruby/lib/symengine/basic.rb
+++ symengine/ruby/lib/symengine/basic.rb
@@ -7,3 +7,13 @@ module SymEngine
     end
   end
 end
+
+class SymEngine::Basic
+  def coerce(other)
+    if other.is_a? Integer
+      [SymEngine::Integer.new(other), self]
+    elsif other.is_a? Rational
+      [SymEngine::Rational.new(other), self]
+    end
+  end
+end
