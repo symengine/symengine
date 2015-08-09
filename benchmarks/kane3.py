9acde00e2ad8ae6a9dc9e362c905ba84ae6c19fc
Isuru Fernando <isuruf@gmail.com>
2015-08-09 21:07:37 +0530

    Added sage conversions for kane3.py

--- benchmarks/kane3.py
+++ benchmarks/kane3.py
@@ -4,6 +4,13 @@ from timeit import default_timer as clock
 from symengine import var, sympify, function_symbol, Symbol, symbols
 import sympy
 from sympy.physics.mechanics import msubs
+
+try:
+    import sage
+    with_sage=True
+except ImportError:
+    with_sage=False
+
 consts = symbols("forkcg1, mwf, mwr, mframe, WRrad, t, htangle, g, framelength, forkcg3, WFrad, forkoffset, mfork, forklength, framecg1, framecg3")
 syms = symbols("s0 s1 s2 s3 s4 s5")
 
@@ -13,6 +20,13 @@ e = sympy.sympify(s)
 print "Converting to SymEngine..."
 ce = sympify(e)
 print "    Done."
+
+if with_sage:
+    print "Converting to Sage..."
+    sconsts = [i._sage_() for i in consts]
+    ssyms = [i._sage_() for i in syms]
+    se = ce._sage_()
+    print "    Done."
 
 print "Subs"
 cf = ce.subs(dict(zip(consts, range(1, len(consts)+1))))
@@ -29,20 +43,31 @@ g = e.diff(syms[5])
 t2 = clock()
 tsympy = t2-t1
 print "Total time:", t2-t1, "s"
+
 print "SymEngine diff:"
 t1 = clock()
 cg = ce.diff(syms[5])
 t2 = clock()
 tsymengine = t2-t1
 print "Total time:", t2-t1, "s"
-print
-print "Speedup: %.2fx" % (tsympy/tsymengine)
+
+if with_sage:
+    print "Sage diff:"
+    t1 = clock()
+    sg = se.diff(ssyms[5])
+    t2 = clock()
+    tsage = t2-t1
+    print "Total time:", t2-t1, "s"
+    print
+    print "Speedup compared to Sage : %.2fx" % (tsage/tsymengine)
+
+print "Speedup compared to SymPy: %.2fx" % (tsympy/tsymengine)
 
 print
 print "Subs"
 cg = cg.subs(dict(zip(consts, range(1, len(consts)+1))))
 cg = cg.subs(dict(zip(syms, range(1, len(syms)+1))))
-print "Eval derivative"
+print "SymEngine Eval derivative"
 print cg.n(real=True)
 print cg.n(200)
 # This number was checked against sympy:
@@ -54,7 +79,16 @@ cg = sympify(g)
 print "Subs"
 cg = cg.subs(dict(zip(consts, range(1, len(consts)+1))))
 cg = cg.subs(dict(zip(syms, range(1, len(syms)+1))))
-print "Eval derivative"
+print "SymPy/SymEngine Eval derivative"
 print cg.n(real=True)
 print cg.n(200)
 assert abs(cg.n(real=True) - (-31789.936485)) < 1e-6
+
+if with_sage:
+    print "Subs"
+    sg = sg.subs(dict(zip(sconsts, range(1, len(sconsts)+1))))
+    sg = sg.subs(dict(zip(ssyms, range(1, len(ssyms)+1))))
+    print "Sage Eval derivative"
+    print sg.n()
+    print sg.n(200)
+    assert abs(sg.n() - (-31789.936485)) < 1e-6
