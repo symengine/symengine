b704b8baf55d32578d225669b6acff503f9751d2
Abinash Meher <abinashdakshana999@gmail.com>
2015-08-22 01:57:02 +0530

    basic on stack wherever possible in ext/*.c

--- symengine/ruby/ext/symengine/symengine_macros.c
+++ symengine/ruby/ext/symengine/symengine_macros.c
@@ -2,7 +2,7 @@
 #include "symengine.h"
 
 void sympify(VALUE operand2, basic_struct *cbasic_operand2) {
-    basic_struct *num_basic, *den_basic, *temp;
+    basic_struct *temp;
     VALUE new_operand2, num, den;
 
     switch(TYPE(operand2)) {
@@ -15,16 +15,17 @@ void sympify(VALUE operand2, basic_struct *cbasic_operand2) {
             num = rb_funcall(operand2, rb_intern("numerator"), 0, NULL);
             den = rb_funcall(operand2, rb_intern("denominator"), 0, NULL);
 
-            num_basic = basic_new_heap();
-            den_basic = basic_new_heap();
+            basic num_basic, den_basic;
+            basic_new_stack(num_basic);
+            basic_new_stack(den_basic);
 
             GET_SYMINTFROMVAL(num, num_basic);
             GET_SYMINTFROMVAL(den, den_basic);
 
             rational_set(cbasic_operand2, num_basic, den_basic);
 
-            basic_free_heap(num_basic);
-            basic_free_heap(den_basic);
+            basic_free_stack(num_basic);
+            basic_free_stack(den_basic);
             break;
 
         case T_DATA:
