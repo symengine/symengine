ef714ad4c474967eccc9455a26bf822746f1fcbd
Abhinav Agarwal <abhinavagarwal07@users.noreply.github.com>
2016-01-05 09:27:25 +0530

    whitespace fix

--- symengine/tests/basic/test_functions.cpp
+++ symengine/tests/basic/test_functions.cpp
@@ -1824,7 +1824,7 @@ TEST_CASE("Polygamma: functions", "[functions]")
     r2 = add(neg(div(pi, i3)), sub(mul(im3, log(i2)), EulerGamma));
     REQUIRE(eq(*r1, *r2));
     
-    r1 = SymEngine::rcp_dynamic_cast<const PolyGamma>(polygamma(i2, x))->rewrite_as_zeta(); 
+    r1 = SymEngine::rcp_dynamic_cast<const PolyGamma>(polygamma(i2, x))->rewrite_as_zeta();
     r2 = neg(mul(i2, zeta(i3, x)));
     REQUIRE(eq(*r1, *r2));
 }
