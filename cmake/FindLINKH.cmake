33adc3b6843808f8716c41fd6b3a7472a4a408dc
Ondřej Čertík <ondrej.certik@gmail.com>
2016-01-21 12:37:29 -0700

    Port BFD and LINKH

--- cmake/FindLINKH.cmake
+++ cmake/FindLINKH.cmake
@@ -1,10 +1,4 @@
-include(LibFindMacros)
-
-libfind_include(link.h linkh)
-
-set(LINKH_INCLUDE_DIRS ${LINKH_INCLUDE_DIR})
+find_path(LINKH_INCLUDE_DIR linkh.h)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LINKH DEFAULT_MSG LINKH_INCLUDE_DIRS)
-
-mark_as_advanced(LINKH_INCLUDE_DIR)
+find_package_handle_standard_args(LINKH DEFAULT_MSG LINKH_INCLUDE_DIR)
