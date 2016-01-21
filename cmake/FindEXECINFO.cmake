985f6cd12369dd7aead1f39df9332a41d5aa9835
Ondřej Čertík <ondrej.certik@gmail.com>
2016-01-21 12:30:58 -0700

    Port execinfo

--- cmake/FindEXECINFO.cmake
+++ cmake/FindEXECINFO.cmake
@@ -1,18 +1,12 @@
-include(LibFindMacros)
+find_path(EXECINFO_INCLUDE_DIR execinfo.h)
 
-libfind_include(execinfo.h execinfo)
-set(EXECINFO_INCLUDE_DIRS ${EXECINFO_INCLUDE_DIR})
 include(FindPackageHandleStandardArgs)
-
+find_package_handle_standard_args(ARB DEFAULT_MSG ARB_INCLUDE_DIR ARB_LIBRARY)
 if (CMAKE_SYSTEM_NAME MATCHES "BSD")
-    libfind_library(execinfo execinfo)
-    set(EXECINFO_LIBRARIES ${EXECINFO_LIBRARY})
-    set(EXECINFO_TARGETS execinfo)
+    find_library(EXECINFO_LIBRARY execinfo)
     find_package_handle_standard_args(EXECINFO DEFAULT_MSG
-        EXECINFO_LIBRARIES EXECINFO_INCLUDE_DIRS)
+        EXECINFO_LIBRARY EXECINFO_INCLUDE_DIR)
 else ()
     find_package_handle_standard_args(EXECINFO DEFAULT_MSG
-        EXECINFO_INCLUDE_DIRS)
+        EXECINFO_INCLUDE_DIR)
 endif ()
-
-mark_as_advanced(EXECINFO_INCLUDE_DIR EXECINFO_LIBRARY)
