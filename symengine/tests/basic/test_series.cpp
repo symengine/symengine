529905c9ccc830d01b7e8466716a7f18da02bb11
Ralf Stephan <ralf@ark.in-berlin.de>
2016-01-28 16:51:38 +0100

    test_series should always compile

--- symengine/tests/basic/test_series.cpp
+++ symengine/tests/basic/test_series.cpp
@@ -25,7 +25,9 @@ using SymEngine::sin;
 using SymEngine::cos;
 using SymEngine::series;
 
-
+//! TODO: remove this ifdef when we have a generic implementation of series
+//#if HAVE_SYMENGINE_PIRANHA || HAVE_SYMENGINE_FLINT
+#ifdef HAVE_SYMENGINE_PIRANHA
 TEST_CASE("Expression series expansion interface", "[Expansion interface]")
 {
     RCP<const Symbol> x = symbol("x"), y = symbol("y");
@@ -37,4 +39,11 @@ TEST_CASE("Expression series expansion interface", "[Expansion interface]")
     REQUIRE(rcp_static_cast<const Number>(ser->as_dict()[8])->is_one());
     REQUIRE(ser->as_basic()->__str__()=="1 - x + x**2 - x**3 + x**4 - x**5 + x**6 - x**7 + x**8 - x**9");
 }
-
+#else
+TEST_CASE("Check error when expansion called without Piranha ", "[Expansion without Piranha]")
+{
+    RCP<const Symbol> x = symbol("x");
+    auto ex1 = lambertw(x);
+    REQUIRE_THROWS_AS(UPSeriesPiranha::series(ex1, "x", 10), std::runtime_error);
+}
+#endif
