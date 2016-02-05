f871099099d13707cd43ce971e2e4c311cd83103
malayaleecoder <malayaleecoder@gmail.com>
2016-02-05 13:36:54 +0530

    Mertens function

--- symengine/ntheory.cpp
+++ symengine/ntheory.cpp
@@ -1498,4 +1498,15 @@ int mobius(const Integer &a)
     }
 }
 
+int mertens(const int long a)
+{
+    if (a <= 0) {
+        throw std::runtime_error("mertens : Mertens function is only defined for positive integers!");
+    }
+    unsigned mertens = 0;
+    for (unsigned i = 1; i <= a; i++) {
+        mertens += mobius(*integer(i));
+    }
+    return mertens;
+}
 } // SymEngine
