d482dcc71d3301c2aaa0a76166f8a31bbd434157
Matthew Luszczak <myluszczak@ucdavis.edu>
2016-02-20 17:20:39 -0800

    added get_args, from_dict to multivariate polynomial

--- symengine/polynomial.h
+++ symengine/polynomial.h
@@ -132,36 +132,61 @@ inline RCP<const UnivariatePolynomial> univariate_polynomial(RCP<const Symbol> i
 
 class sym_hash{
 public:
-  size_t operator()(const Symbol &s) const{
-    return s.__hash__();
+  size_t operator()(const RCP<const Symbol> s) const{
+    return s->__hash__();
   }
 };
 
 class sym_compare{
   public:
-  size_t operator()(const Symbol &a, const Symbol &b){
-    return a.compare(b);
+  size_t operator()(const RCP<const Symbol> a, const RCP<const Symbol> b){
+    return a->compare(*b);
   }
 };
 
 class sym_eq{
  public:
-  bool operator()(const Symbol &a, const Symbol &b){
-    return a.__eq__(b);
+  bool operator()(const RCP<const Symbol> a, const RCP<const Symbol> &b){
+    return a->__eq__(*b);
   }
 };
 
-int umap_vec_mpz_compare(umap_vec_mpz &a, umap_vec_mpz &b){
+int umap_vec_mpz_compare(const umap_vec_mpz &a, const umap_vec_mpz &b){
   if(a.size() < b.size())
     return (a.size() < b.size()) ? -1 : 1;
   return 0;
 };
 
+unsigned int mpz_hash(mpz_class z){
+  return z.get_ui();
+ }
+
+ typedef std::vector<unsigned int> vec_uint;
  
- typedef std::set<Symbol, sym_compare> set_sym;
- typedef std::unordered_map<Symbol, unsigned int, sym_hash, sym_eq> umap_sym_uint;
  
+ class vec_uint_hash{
+   std::size_t operator() (const vec_uint &v){
+     std::size_t h = 0;
+     for(unsigned int i : v){
+       h ^= h + 0x9e3779b + h <<6 + h >> 2;
+     }
+     return h;
+   }
+ }
  
+ typedef std::set<RCP<const Symbol>, sym_compare> set_sym;
+ typedef std::unordered_map<RCP<const Symbol>, unsigned int, sym_hash, sym_eq> umap_sym_uint;
+ typedef std::unordered_map<vec_uint, mpz_class, vec_uint_hash>;
+ 
+template<class T>
+bool set_eq(const std::set<T> &a, const std::set<T> &b){
+  return a == b;
+}
+
+template<class T>
+int set_compare(std::set<T> &a, std::set<T> &b){
+  return 0;
+}
  
 class MultivariatePolynomial : public Basic{
 public:
@@ -173,13 +198,16 @@ public:
     umap_sym_uint degrees_;
     umap_vec_mpz dict_;
 public:
+    IMPLEMENT_TYPEID(MULTIVARIATEPOLYNOMIAL)
     //constructor from components
-    MultivariatePolynomial(set_sym &var, umap_sym_uint degrees, umap_vec_mpz &dict);
-    /*bool is_canonical(set_sym &vars, uamp_sym_uint &degrees, umap_vec_mpz &dict);
-    std::size_t __hash__();
-    bool __eq__(const Basic &o);
-    int compare(const Basic &o);
-    mpz_class eval(std::map<Symbol, mpz_class> &vals);*/   
+    MultivariatePolynomial(set_sym &var, umap_sym_uint &degrees, umap_vec_mpz &dict);
+    RCP<const Basic> from_dict(set_sym &s, umap_vec_mpz &&d);
+    vec_basic get_args() const;
+    bool is_canonical(set_sym &vars, umap_sym_uint &degrees, umap_vec_mpz &dict);
+    std::size_t __hash__() const;
+    bool __eq__(const Basic &o) const;
+    int compare(const Basic &o) const;
+    mpz_class eval(std::map<RCP<const Symbol>, mpz_class> &vals);   
 };
 /*
 RCP<const MultivariatePolynomial> add_mult_poly(const MultivariatePolynomial &a, const MultivariatePolynomial &b);
