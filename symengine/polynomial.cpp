a3d7fe8fa0c25cbcb1ab4886339df4111bf5b2b8
Matthew Luszczak <myluszczak@ucdavis.edu>
2016-03-05 12:31:51 -0800

    fixes suggested by isuruf, now compiles and runs on my computer

--- symengine/polynomial.cpp
+++ symengine/polynomial.cpp
@@ -86,23 +86,11 @@ RCP<const UnivariateIntPolynomial> UnivariateIntPolynomial::from_dict(const RCP<
     return make_rcp<const UnivariateIntPolynomial>(var, degree, std::move(d));
 }
 
-RCP<const UnivariateIntPolynomial> UnivariateIntPolynomial::from_vec(const RCP<const Symbol> &var, const std::vector<integer_class> &v){
-    map_uint_mpz dict;
-    unsigned int degree = 0;
-    for (unsigned int i = 0; i < v.size(); i++) {
-        if (0 != v[i]) {
-  	    dict.insert(std::pair<unsigned int, integer_class>(i, v[i]));
-	    degree = i;
-        }
-    }
-    return make_rcp<const UnivariatePolynomial>(var, degree, std::move(dict));
-}
-
 RCP<const UnivariateIntPolynomial> from_vec(const RCP<const Symbol> &var, const std::vector<integer_class> &v){
     map_uint_mpz dict;
     unsigned int degree = 0;
     for(unsigned int i = 0; i < v.size(); i++){
-        if(0 != v[i]){
+        if (0 != v[i]){
   	    dict.insert(std::pair<unsigned int, integer_class>(i, v[i]));
 	    degree = i;
         }
