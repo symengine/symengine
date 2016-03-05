4edb3013b4974af8f3e34e7eec62c73008851bfa
Matthew Luszczak <myluszczak@ucdavis.edu>
2016-03-04 23:37:11 -0800

    added from_vec; does not link test_arrit

--- symengine/expand.cpp
+++ symengine/expand.cpp
@@ -243,7 +243,7 @@ public:
         if (is_a<Integer>(*self.get_exp()) && is_a<UnivariateIntPolynomial>(*_base)) {
             int q = rcp_static_cast<const Integer>(self.get_exp())->as_int();
             RCP<const UnivariateIntPolynomial> p = rcp_static_cast<const UnivariateIntPolynomial>(_base);
-            RCP<const UnivariateIntPolynomial> r = univariate_int_polynomial(p->var_, 0, {{0, integer_class(1)}});
+            RCP<const UnivariateIntPolynomial> r = univariate_int_polynomial(p->var_, {{0, integer_class(1)}});
             while (q != 0) {
                 if (q % 2 == 1) {
                     r = mul_poly(r, p);
