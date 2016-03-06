22caad6d9cbb83d6fe0eac2f6043f39a04e295b9
Matthew Luszczak <myluszczak@ucdavis.edu>
2016-03-06 01:49:49 -0800

    formatting

--- symengine/polynomial.cpp
+++ symengine/polynomial.cpp
@@ -18,14 +18,14 @@ bool UnivariateIntPolynomial::is_canonical(const unsigned int &degree_, const ma
         unsigned int prev_degree = (--dict.end())->first;
         if (prev_degree != degree_)
             return false;
-    } else if(0 != degree_)
+    } else if (0 != degree_) {
         return false;
+    }
     //Check if dictionary contains terms with coeffienct 0
     for (auto itter = dict.begin(); itter != dict.end(); itter++) {
         if (0 == itter->second)
-	    return false;
+            return false;
     }
-    
     return true;
 }
 
@@ -35,8 +35,7 @@ std::size_t UnivariateIntPolynomial::__hash__() const
     std::size_t seed = UNIVARIATEINTPOLYNOMIAL;
 
     seed += hash_string(this->var_->get_name());
-    for (const auto &it : this->dict_)
-    {
+    for (const auto &it : this->dict_) {
         std::size_t temp = UNIVARIATEINTPOLYNOMIAL;
         hash_combine<unsigned int>(temp, it.first);
         hash_combine<long long int>(temp, mp_get_si(it.second));
@@ -88,7 +87,7 @@ RCP<const UnivariateIntPolynomial> UnivariateIntPolynomial::from_dict(const RCP<
 RCP<const UnivariateIntPolynomial> from_vec(const RCP<const Symbol> &var, const std::vector<integer_class> &v){
     map_uint_mpz dict;
     unsigned int degree = 0;
-    for(unsigned int i = 0; i < v.size(); i++){
+    for(unsigned int i = 0; i < v.size(); i++) {
         if (0 != v[i]) {
   	    dict.insert(std::pair<unsigned int, integer_class>(i, v[i]));
 	    degree = i;
@@ -109,8 +108,9 @@ vec_basic UnivariateIntPolynomial::get_args() const {
     map_uint_mpz d;
     for (const auto &p: dict_) {
         d = {{p.first, p.second}};
-        if (d.begin()->first == 0)
+        if (d.begin()->first == 0) {
             args.push_back( integer(d.begin()->second));
+        }
         else if (d.begin()->first == 1) {
 	    if (d.begin()->second == 1) {
                 args.push_back(var_);
@@ -119,13 +119,13 @@ vec_basic UnivariateIntPolynomial::get_args() const {
 	    }
         } else {
             if (d.begin()->second == 1) {
-	        args.push_back( pow(var_, integer(d.begin()->first)));
+                args.push_back( pow(var_, integer(d.begin()->first)));
             } else {
-	        args.push_back( Mul::from_dict(integer(d.begin()->second),{{var_, integer(d.begin()->first)}}));
+                args.push_back( Mul::from_dict(integer(d.begin()->second),{{var_, integer(d.begin()->first)}}));
             }
         }
     }
-    if(dict_.empty())
+    if (dict_.empty())
         args.push_back(zero);
     return args;
 }
@@ -167,14 +167,14 @@ bool UnivariatePolynomial::is_zero() const {
 
 bool UnivariateIntPolynomial::is_one() const {
     if (dict_.size() == 1 and dict_.begin()->second == 1 and
-	    dict_.begin()->first == 0)
+            dict_.begin()->first == 0)
         return true;
     return false;
 }
 
 bool UnivariateIntPolynomial::is_minus_one() const {
     if (dict_.size() == 1 and dict_.begin()->second == -1 and
-	    dict_.begin()->first == 0)
+            dict_.begin()->first == 0)
         return true;
     return false;
 }
@@ -189,21 +189,21 @@ bool UnivariateIntPolynomial::is_integer() const {
 
 bool UnivariateIntPolynomial::is_symbol() const {
     if (dict_.size() == 1 and dict_.begin()->first == 1 and
-	    dict_.begin()->second == 1)
+            dict_.begin()->second == 1)
         return true;
     return false;
 }
 
 bool UnivariateIntPolynomial::is_mul() const {
     if (dict_.size() == 1 and dict_.begin()->first != 0 and
-	    dict_.begin()->second != 1 and dict_.begin()->second != 0)
+            dict_.begin()->second != 1 and dict_.begin()->second != 0)
         return true;
     return false;
 }
 
 bool UnivariateIntPolynomial::is_pow() const {
     if (dict_.size() == 1 and dict_.begin()->second == 1 and
-	    dict_.begin()->first != 1 and dict_.begin()->first != 0)
+            dict_.begin()->first != 1 and dict_.begin()->first != 0)
         return true;
     return false;
 }
