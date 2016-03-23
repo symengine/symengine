9e1d4c7a3d355d1bb7f807ed8481c3f5dca1c433
Srajan Garg <srajan.garg@gmail.com>
2016-03-23 14:54:13 +0530

    made changes for travis for clang-format

---
+++ bin/travis_clang.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+RETURN=0
+CLANG="clang-format-3.7"
+
+if [ ! -f ".clang-format" ]; then
+    echo ".clang-format file not found!"
+    exit 1
+fi
+
+FILES=`git diff master --name-only | grep -E "\.(cpp|h)$"`
+
+for FILE in $FILES; do
+
+	$CLANG $FILE | cmp  $FILE >/dev/null
+
+	if [ $? -ne 0 ]; then
+		echo "[!] INCORRECT FORMATTING! $FILE" >&2
+		RETURN=1
+	fi
+
+done
+
+exit $RETURN
