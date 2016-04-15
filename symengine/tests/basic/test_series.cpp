c1ddffe85c536e02bc884bd1a93e6638d9fa5225
Charles Chen <cyuchen@ucdavis.edu>
2016-04-15 10:40:32 -0700

    Remove ifdef guard now that we have a generic implementation

--- symengine/tests/basic/test_series.cpp
+++ symengine/tests/basic/test_series.cpp
@@ -30,17 +30,12 @@ TEST_CASE("Expression series expansion interface", "[Expansion interface]")
     RCP<const Symbol> x = symbol("x"), y = symbol("y");
     auto ex = div(integer(1), add(integer(1), x));
 
-//! TODO: remove this ifdef when we have a generic implementation of series
-#if defined(HAVE_SYMENGINE_PIRANHA) || defined(HAVE_SYMENGINE_FLINT)
     auto ser = series(ex, x, 10);
 
     REQUIRE(rcp_static_cast<const Number>(ser->get_coeff(7))->is_minus_one());
     REQUIRE(rcp_static_cast<const Number>(ser->as_dict()[8])->is_one());
     REQUIRE(ser->as_basic()->__str__()
             == "1 - x + x**2 - x**3 + x**4 - x**5 + x**6 - x**7 + x**8 - x**9");
-#else
-    REQUIRE_THROWS_AS(series(ex, x, 10), std::runtime_error);
-#endif
 
 #ifndef HAVE_SYMENGINE_PIRANHA
     auto ex1 = sin(add(integer(1), x));
