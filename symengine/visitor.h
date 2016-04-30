dab70237bd9f04f987f172208a0038ff2918125d
Matthew Luszczak <myluszczak@ucdavis.edu>
2016-04-30 14:02:47 -0700

    Added the test of MultivariatePolynomial constructor

--- symengine/visitor.h
+++ symengine/visitor.h
@@ -11,6 +11,7 @@
 #include <symengine/mul.h>
 #include <symengine/pow.h>
 #include <symengine/polynomial.h>
+#include <symengine/polynomial_multivariate.h>
 #include <symengine/functions.h>
 #include <symengine/symbol.h>
 #include <symengine/integer.h>
