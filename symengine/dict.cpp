33b1c42c3c5c2630a50ff414584a8a83dd14c500
Srajan Garg <srajan.garg@gmail.com>
2016-05-10 18:44:31 +0530

    moved parts of dict.cpp/.h, small restrucutre

--- symengine/dict.cpp
+++ symengine/dict.cpp
@@ -92,17 +92,58 @@ std::ostream &operator<<(std::ostream &out, const SymEngine::set_basic &d)
     return SymEngine::print_vec_rcp(out, d);
 }
 
+//! derivatives of base functions
 bool vec_basic_eq(const vec_basic &a, const vec_basic &b)
 {
-    // Can't be equal if # of entries differ:
-    if (a.size() != b.size())
-        return false;
-    // Loop over elements in "a" and "b":
-    for (size_t i = 0; i < a.size(); i++) {
-        if (neq(*a[i], *b[i]))
-            return false; // values not equal
+    return vec_eq<vec_basic>(a, b);
+}
+
+int vec_basic_compare(const vec_basic &a, const vec_basic &b)
+{
+    return vec_compare<vec_basic>(a, b);
+}
+
+bool multiset_basic_eq(const multiset_basic &a, const multiset_basic &b)
+{
+    return set_eq<multiset_basic>(a, b);
+}
+
+int multiset_basic_compare(const multiset_basic &a, const multiset_basic &b)
+{
+    return set_compare<multiset_basic>(a, b);
+}
+
+//! non-derivable functions
+int map_uint_mpz_compare(const map_uint_mpz &A, const map_uint_mpz &B)
+{
+    if (A.size() != B.size())
+        return (A.size() < B.size()) ? -1 : 1;
+    auto a = A.begin();
+    auto b = B.begin();
+    for (; a != A.end(); ++a, ++b) {
+        if (a->first != b->first)
+            return (a->first < b->first) ? -1 : 1;
+        if (a->second != b->second)
+            return (a->second < b->second) ? -1 : 1;
     }
-    return true;
+    return 0;
+    // return map_compare<map_uint_mpz>(a, b);
+}
+
+int map_int_Expr_compare(const map_int_Expr &A, const map_int_Expr &B)
+{
+    if (A.size() != B.size())
+        return (A.size() < B.size()) ? -1 : 1;
+    auto a = A.begin();
+    auto b = B.begin();
+    for (; a != A.end(); ++a, ++b) {
+        if (a->first != b->first)
+            return (a->first < b->first) ? -1 : 1;
+        if (a->second != b->second)
+            return (a->second.get_basic()->__cmp__(*b->second.get_basic())) ? -1
+                                                                            : 1;
+    }
+    return 0;
 }
 
 bool vec_basic_eq_perm(const vec_basic &a, const vec_basic &b)
@@ -126,93 +167,5 @@ bool vec_basic_eq_perm(const vec_basic &a, const vec_basic &b)
     }
     // If all elements were found, then a == b
     return true;
-}
-
-int vec_basic_compare(const vec_basic &A, const vec_basic &B)
-{
-    if (A.size() != B.size())
-        return (A.size() < B.size()) ? -1 : 1;
-    auto a = A.begin();
-    auto b = B.begin();
-    int cmp;
-    for (; a != A.end(); ++a, ++b) {
-        cmp = (*a)->__cmp__(**b);
-        if (cmp != 0)
-            return cmp;
-    }
-    return 0;
-}
-
-int map_uint_mpz_compare(const map_uint_mpz &A, const map_uint_mpz &B)
-{
-    if (A.size() != B.size())
-        return (A.size() < B.size()) ? -1 : 1;
-    auto a = A.begin();
-    auto b = B.begin();
-    for (; a != A.end(); ++a, ++b) {
-        if (a->first != b->first)
-            return (a->first < b->first) ? -1 : 1;
-        if (a->second != b->second)
-            return (a->second < b->second) ? -1 : 1;
-    }
-    return 0;
-}
-
-int map_int_Expr_compare(const map_int_Expr &A, const map_int_Expr &B)
-{
-    if (A.size() != B.size())
-        return (A.size() < B.size()) ? -1 : 1;
-    auto a = A.begin();
-    auto b = B.begin();
-    for (; a != A.end(); ++a, ++b) {
-        if (a->first != b->first)
-            return (a->first < b->first) ? -1 : 1;
-        if (a->second != b->second)
-            return (a->second.get_basic()->__cmp__(*b->second.get_basic())) ? -1
-                                                                            : 1;
-    }
-    return 0;
-}
-
-template <class T>
-bool set_eq(const T &A, const T &B)
-{
-    // Can't be equal if # of entries differ:
-    if (A.size() != B.size())
-        return false;
-    // Loop over elements in "a" and "b":
-    auto a = A.begin();
-    auto b = B.begin();
-    for (; a != A.end(); ++a, ++b) {
-        if (neq(**a, **b))
-            return false; // values not equal
-    }
-    return true;
-}
-
-template <class T>
-int set_compare(const T &A, const T &B)
-{
-    if (A.size() != B.size())
-        return (A.size() < B.size()) ? -1 : 1;
-    auto a = A.begin();
-    auto b = B.begin();
-    int cmp;
-    for (; a != A.end(); ++a, ++b) {
-        cmp = (*a)->__cmp__(**b);
-        if (cmp != 0)
-            return cmp;
-    }
-    return 0;
-}
-
-bool multiset_basic_eq(const multiset_basic &a, const multiset_basic &b)
-{
-    return set_eq<multiset_basic>(a, b);
-}
-
-int multiset_basic_compare(const multiset_basic &a, const multiset_basic &b)
-{
-    return set_compare<multiset_basic>(a, b);
 }
 }
