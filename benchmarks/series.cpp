6bd7a987962478ad962149cba3afa11f602a43f8
Iris Lui <iwlui@ucdavis.edu>
2016-05-15 13:40:52 -0700

    Remove unnecessary indent space in series.cpp

--- benchmarks/series.cpp
+++ benchmarks/series.cpp
@@ -40,6 +40,6 @@ int main(int argc, char *argv[])
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1)
                      .count()
               << "ms" << std::endl;
-    
+
     return 0;
 }
