09c8e543c59ede63ccda10fbc7ba049d6a155d2d
Matthew Luszczak <myluszczak@ucdavis.edu>
2016-05-22 15:39:14 -0700

    formatting corrections

--- symengine/printer.h
+++ symengine/printer.h
@@ -66,6 +66,62 @@ public:
         bvisit_upoly(x);
     }
 
+    void bvisit(const MultivariateIntPolynomial &x)
+    {
+        if (0 == x.dict_.size()) {
+            precedence = PrecedenceEnum::Atom;
+        } else if (1 == x.dict_.size()) {
+            auto iter = x.dict_.begin();
+            precedence = PrecedenceEnum::Atom;
+            bool first = true; // true if there are no nonzero exponents, false
+                               // otherwise
+            for (unsigned int exp : iter->first) {
+                if (exp > 0) {
+                    if (first && exp > 1)
+                        precedence = PrecedenceEnum::Pow;
+                    if (!first)
+                        precedence = PrecedenceEnum::Mul;
+                    first = false;
+                }
+            }
+            if (!first) {
+                if (iter->second != 1)
+                    precedence = PrecedenceEnum::Mul;
+            }
+        } else {
+            precedence = PrecedenceEnum::Add;
+        }
+    }
+
+    void bvisit(const MultivariatePolynomial &x)
+    {
+        if (0 == x.dict_.size()) {
+            precedence = PrecedenceEnum::Atom;
+        } else if (1 == x.dict_.size()) {
+            auto iter = x.dict_.begin();
+            precedence = PrecedenceEnum::Atom;
+            bool first = true; // true if there are no nonzero exponents, false
+                               // otherwise
+            for (unsigned int exp : iter->first) {
+                if (exp > 0) {
+                    if (first && exp > 1)
+                        precedence = PrecedenceEnum::Pow;
+                    if (!first)
+                        precedence = PrecedenceEnum::Mul;
+                    first = false;
+                }
+            }
+            if (first) {
+                iter->second.get_basic()->accept(*this);
+            } else {
+                if (iter->second != 1)
+                    precedence = PrecedenceEnum::Mul;
+            }
+        } else {
+            precedence = PrecedenceEnum::Add;
+        }
+    }
+
     void bvisit(const Rational &x)
     {
         precedence = PrecedenceEnum::Add;
