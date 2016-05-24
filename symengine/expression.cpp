5b4e2595360f19f5724e317d89254ca64ed422e5
Matthew Luszczak <myluszczak@ucdavis.edu>
2016-05-23 23:14:48 -0700

    revert expression.cpp to c6e6b and test_printing.cpp to master

--- symengine/expression.cpp
+++ symengine/expression.cpp
@@ -9,7 +9,7 @@ namespace detail
 std::string poly_print(const Expression &x)
 {
     Precedence prec;
-    if (prec.getPrecedence(x.get_basic()) != PrecedenceEnum::Atom) {
+    if (prec.getPrecedence(x.get_basic()) == PrecedenceEnum::Add) {
         return "(" + x.get_basic()->__str__() + ")";
     }
     return x.get_basic()->__str__();
