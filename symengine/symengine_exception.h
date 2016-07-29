cd9414ca81c9883acee468884c6bf45963f3cbee
Rajith Vidanaarachchi <rajith.vidanaarachchi@gmail.com>
2016-07-29 08:00:32 +0530

    Added more common exceptions to symengine_exception.h

--- symengine/symengine_exception.h
+++ symengine/symengine_exception.h
@@ -11,5 +11,30 @@ class DivisionByZero : public std::exception
         return "Division By Zero";
     }
 };
+
+class NotImplemented : public std::exception
+{
+    virtual const char *what() const throw()
+    {
+        return "Not Implemented";
+    }
+};
+
+class ResultIsComplex : public std::exception
+{
+    virtual const char *what() const throw()
+    {
+        return "Result is Complex. Recompile with MPC support";
+    }
+};
+
+class Undefined : public std::exception
+{
+    virtual const char *what() const throw()
+    {
+        return "Undefined";
+    }
+};
+
 }
 #endif // SYMENGINE_EXCEPTION_H
