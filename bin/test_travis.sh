e7f56ad5922e5601baa4521059a6e69a7e70d916
Isuru Fernando <isuruf@gmail.com>
2016-10-17 22:39:03 +0530

    Travis testing

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -75,6 +75,9 @@ fi
 if [[ "${WITH_COVERAGE}" != "" ]]; then
     cmake_line="$cmake_line -DWITH_COVERAGE=${WITH_COVERAGE}"
 fi
+if [[ "${WITH_LLVM}" != "" ]]; then
+    cmake_line="$cmake_line -DWITH_LLVM=${WITH_LLVM}"
+fi
 
 if [[ "${CC}" == "clang"* ]] && [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
     export CXXFLAGS=""
