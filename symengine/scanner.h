eecb0a0341ca60c54aaee08d78f4aa48d5c99129
Srajan Garg <srajan.garg@gmail.com>
2017-03-02 20:07:16 +0530

    add cmake tests for flexc++/bisonc++, formatting of generated files

--- symengine/scanner.h
+++ symengine/scanner.h
@@ -9,61 +9,60 @@ namespace SymEngine
 {
 
 // $insert classHead
-class Scanner: public ScannerBase
+class Scanner : public ScannerBase
 {
-    public:
-        explicit Scanner(std::istream &in = std::cin,
-                                std::ostream &out = std::cout);
+public:
+    explicit Scanner(std::istream &in = std::cin,
+                     std::ostream &out = std::cout);
 
-        Scanner(std::string const &infile, std::string const &outfile);
-        
-        int lex();
+    Scanner(std::string const &infile, std::string const &outfile);
 
-        ParserBase::STYPE__ * dval;
+    int lex();
 
-    private:
-        int lex__();
-        int executeAction__(size_t ruleNr);
+    ParserBase::STYPE__ *dval;
 
-        void print();
-        void preCode();     // re-implement this function for code that must 
-                            // be exec'ed before the patternmatching starts
+private:
+    int lex__();
+    int executeAction__(size_t ruleNr);
 
-        void postCode(PostEnum__ type);    
-                            // re-implement this function for code that must 
-                            // be exec'ed after the rules's actions.
+    void print();
+    void preCode(); // re-implement this function for code that must
+                    // be exec'ed before the patternmatching starts
+
+    void postCode(PostEnum__ type);
+    // re-implement this function for code that must
+    // be exec'ed after the rules's actions.
 };
 
 inline Scanner::Scanner(std::istream &in, std::ostream &out)
-:
-    ScannerBase(in, out)
-{}
+    : ScannerBase(in, out)
+{
+}
 
 inline Scanner::Scanner(std::string const &infile, std::string const &outfile)
-:
-    ScannerBase(infile, outfile)
-{}
+    : ScannerBase(infile, outfile)
+{
+}
 
 inline int Scanner::lex()
 {
     return lex__();
 }
 
-inline void Scanner::preCode() 
+inline void Scanner::preCode()
 {
     // optionally replace by your own code
 }
 
-inline void Scanner::postCode(PostEnum__ type) 
+inline void Scanner::postCode(PostEnum__ type)
 {
     // optionally replace by your own code
 }
 
-inline void Scanner::print() 
+inline void Scanner::print()
 {
     print__();
 }
-
 }
 
 #endif
