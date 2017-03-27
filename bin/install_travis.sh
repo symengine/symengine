91969d4a9e64601a97fc78ea16d2bf5eaeceb12a
Isuru Fernando <isuruf@gmail.com>
2017-03-27 17:46:20 +0530

    Use miniconda3
    Installing some packages like sage needs miniconda3 as miniconda2
    doesn't support unicode in filenames.

--- bin/install_travis.sh
+++ bin/install_travis.sh
@@ -48,9 +48,9 @@ export our_install_dir="$HOME/our_usr"
 
 
 if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-    wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
 else
-    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 fi
 bash miniconda.sh -b -p $HOME/conda_root
 export PATH="$HOME/conda_root/bin:$PATH"
