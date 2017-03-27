148149ffbdbca8000b6b973f72fbfd00536fc66f
Isuru Fernando <isuruf@gmail.com>
2017-03-27 19:25:53 +0530

    Python 3 support for test_make_install.py

--- symengine/utilities/tests/test_make_install.py
+++ symengine/utilities/tests/test_make_install.py
@@ -10,21 +10,21 @@ install_dir = sys.argv[1]
 symengine_dir = sys.argv[2]
 
 installed_files = [basename(x) for x in glob(join(install_dir, '*.h'))]
-print 'Total number of installed files : %d' % len(installed_files)
+print('Total number of installed files : %d' % len(installed_files))
 
 all_files = [basename(x) for x in glob(join(symengine_dir, '*.h'))]
-print 'Number of all files to be installed : %d' % len(all_files)
+print('Number of all files to be installed : %d' % len(all_files))
 
 # res = True if all are installed
 res = True
 
 for fl in all_files:
     if not fl in installed_files:
-        print '%s is not installed!' % fl
+        print('%s is not installed!' % fl)
         res = False
 
 if res:
-    print 'All files are installed!'
+    print('All files are installed!')
     exit(0)
 else:
     exit(1)
