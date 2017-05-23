468407ba8f56fe6374254582e30c9d5f016fa574
ranjithkumar007 <ranjith.dakshana2015@gmail.com>
2017-05-23 16:50:12 +0530

    [skip appveyor]check that WITH_ARB is not yes

--- bin/install_travis.sh
+++ bin/install_travis.sh
@@ -104,7 +104,7 @@ fi
 conda create -q -p $our_install_dir ${conda_pkgs};
 source activate $our_install_dir;
 
-if [[ "${WITH_FLINT_DEV}" == "yes" ]]; then
+if [[ "${WITH_FLINT_DEV}" == "yes" ]] && [[ "${WITH_ARB}" != "yes" ]]; then
     git clone https://github.com/wbhart/flint2;
     cd flint2 && git checkout 06defcbc52efe41a8c73496ffde9fc66941e3f0d && ./configure --prefix=$our_install_dir --with-gmp=$our_install_dir --with-mpfr=$our_install_dir && make -j8 install && cd ..;
 fi
