3e28e3667c42a2135f2e4ff36518e3e7775eaea0
Ranjith Kumar <ranjith.dakshana2015@gmail.com>
2017-07-13 16:24:43 +0530

    improvements and adds real_imag for trig functions

--- symengine/as_real_imag.cpp
+++ symengine/as_real_imag.cpp
@@ -24,78 +24,73 @@ public:
     void bvisit(const Mul &x)
     {
         RCP<const Basic> rest = one;
-        RCP<const Basic> re_, im_;
+        RCP<const Basic> fre_ = one, fim_ = zero;
         RCP<const Basic> coef = one;
         bool is_imag = false;
 
         for (const auto &arg : x.get_args()) {
-            as_real_imag(arg, outArg(re_), outArg(im_));
-            if (eq(*im_, *zero)) {
-                coef = mul(coef, re_);
-            } else if (eq(*re_, *zero)) {
-                is_imag = !is_imag;
-                coef = mul(coef, im_);
-            } else {
-                rest = mul(rest, arg);
-            }
+            apply(*arg);
+            std::tie(fre_, fim_)
+                = std::make_tuple(sub(mul(fre_, *real_), mul(fim_, *imag_)),
+                                  add(mul(fre_, *imag_), mul(fim_, *real_)));
         }
-
-        if (eq(*rest, *one)) {
-            if (is_imag) {
-                *real_ = zero;
-                *imag_ = coef;
-            } else {
-                *real_ = coef;
-                *imag_ = zero;
-            }
-        } else {
-            if (eq(*rest, x)) {
-                throw SymEngineException(
-                    "Not Implemented classes for real and imaginary parts");
-            }
-            as_real_imag(rest, outArg(re_), outArg(im_));
-            if (is_imag) {
-                *real_ = neg(mul(coef, im_));
-                *imag_ = mul(coef, re_);
-            } else {
-                *real_ = mul(coef, re_);
-                *imag_ = mul(coef, im_);
-            }
-        }
+        *real_ = fre_;
+        *imag_ = fim_;
     }
 
     void bvisit(const Add &x)
     {
-        RCP<const Basic> re = zero, re_, im = zero, im_;
+        RCP<const Basic> t;
+        umap_basic_num dr, dim;
+        RCP<const Number> coefr = zero, coefim = zero, coef;
 
         for (const auto &arg : x.get_args()) {
-            as_real_imag(arg, outArg(re_), outArg(im_));
-            re = add(re, re_);
-            im = add(im, im_);
+            apply(*arg);
+            if (is_a_Number(**real_)) {
+                iaddnum(outArg(coefr), rcp_static_cast<const Number>(*real_));
+            } else {
+                Add::as_coef_term(*real_, outArg(coef), outArg(t));
+                Add::dict_add_term(dr, coef, t);
+            }
+            if (is_a_Number(**imag_)) {
+                iaddnum(outArg(coefim), rcp_static_cast<const Number>(*imag_));
+            } else {
+                Add::as_coef_term(*imag_, outArg(coef), outArg(t));
+                Add::dict_add_term(dim, coef, t);
+            }
         }
 
-        *real_ = re;
-        *imag_ = im;
+        *real_ = Add::from_dict(coefr, std::move(dr));
+        *imag_ = Add::from_dict(coefim, std::move(dim));
     }
 
     void bvisit(const Pow &x)
     {
-        RCP<const Basic> base_, exp_, re, im;
-        base_ = x.get_base();
+        RCP<const Basic> exp_;
         exp_ = x.get_exp();
-        as_real_imag(base_, outArg(re), outArg(im));
+        apply(*x.get_base());
 
+        if (eq(**imag_, *zero)) {
+            *real_ = x.rcp_from_this();
+            *imag_ = zero;
+            return;
+        }
         if (is_a<Integer>(*exp_)) {
-            if (eq(*im, *zero)) {
-                *real_ = x.rcp_from_this();
-                *imag_ = zero;
-                return;
+            RCP<const Basic> expx;
+            if (eq(*Gt(exp_, zero), *boolTrue)) {
+                expx = expand(x.rcp_from_this());
+            } else {
+                auto magn = add(mul(*real_, *real_), mul(*imag_, *imag_));
+                *real_ = div(*real_, magn);
+                *imag_ = div(neg(*imag_), magn);
+                expx = expand(pow(add(*real_, mul(*imag_, I)), neg(exp_)));
             }
-            auto expx = expand(x.rcp_from_this());
-            as_real_imag(expx, real_, imag_);
+            if (eq(*expx, x))
+                throw SymEngineException("Not Implemented");
+            apply(*expx);
         } else if (is_a<Rational>(*exp_)) {
-            auto magn = sqrt(add(mul(re, re), mul(im, im)));
-            auto ang = atan2(im, re);
+            auto magn = sqrt(add(mul(*real_, *real_), mul(*imag_, *imag_)));
+            auto ang = atan2(*imag_, *real_);
             magn = pow(magn, exp_);
             ang = mul(ang, exp_);
             *real_ = mul(magn, cos(ang));
@@ -105,15 +100,139 @@ public:
         }
     }
 
-    void bvisit(const Complex &x)
+    void bvisit(const ComplexBase &x)
     {
         *real_ = x.real_part();
         *imag_ = x.imaginary_part();
     }
 
+    void bvisit(const Infty &x)
+    {
+        if (eq(x, *ComplexInf)) {
+            *real_ = Nan;
+            *imag_ = Nan;
+        } else {
+            *real_ = x.rcp_from_this();
+            *imag_ = zero;
+        }
+    }
+
+    void bvisit(const Sin &x)
+    {
+        apply(*x.get_arg());
+        std::tie(*real_, *imag_) = std::make_tuple(
+            mul(sin(*real_), cosh(*imag_)), mul(sinh(*imag_), cos(*real_)));
+    }
+
+    void bvisit(const Cos &x)
+    {
+        apply(*x.get_arg());
+        std::tie(*real_, *imag_)
+            = std::make_tuple(mul(cos(*real_), cosh(*imag_)),
+                              neg(mul(sinh(*imag_), sin(*real_))));
+    }
+
+    void bvisit(const Tan &x)
+    {
+        apply(*x.get_arg());
+        if (eq(**imag_, *zero)) {
+            *real_ = x.rcp_from_this();
+            *imag_ = zero;
+            return;
+        }
+        auto i2 = integer(2);
+        auto twice_real_ = mul(i2, *real_), twice_imag_ = mul(i2, *imag_);
+        auto den = add(cos(twice_real_), cosh(twice_imag_));
+        *real_ = div(sin(twice_real_), den);
+        *imag_ = div(sinh(twice_imag_), den);
+    }
+
+    void bvisit(const Csc &x)
+    {
+        apply(*div(one, sin(x.get_arg())));
+    }
+
+    void bvisit(const Sec &x)
+    {
+        apply(*div(one, cos(x.get_arg())));
+    }
+
+    void bvisit(const Cot &x)
+    {
+        apply(*x.get_arg());
+        if (eq(**imag_, *zero)) {
+            *real_ = x.rcp_from_this();
+            return;
+        }
+        auto i2 = integer(2);
+        auto twice_real_ = mul(i2, *real_), twice_imag_ = mul(i2, *imag_);
+        auto den = sub(cos(twice_real_), cosh(twice_imag_));
+        *real_ = neg(div(sin(twice_real_), den));
+        *imag_ = neg(div(sinh(twice_imag_), den));
+    }
+
+    void bvisit(const Sinh &x)
+    {
+        apply(*x.get_arg());
+        std::tie(*real_, *imag_) = std::make_tuple(
+            mul(sinh(*real_), cos(*imag_)), mul(sin(*imag_), cosh(*real_)));
+    }
+
+    void bvisit(const Cosh &x)
+    {
+        apply(*x.get_arg());
+        std::tie(*real_, *imag_) = std::make_tuple(
+            mul(cosh(*real_), cos(*imag_)), mul(sin(*imag_), sinh(*real_)));
+    }
+
+    void bvisit(const Tanh &x)
+    {
+        apply(*x.get_arg());
+        if (eq(**imag_, *zero)) {
+            *real_ = x.rcp_from_this();
+            return;
+        }
+        auto i2 = integer(2);
+        auto sinh_re = sinh(*real_), cos_im = cos(*imag_);
+        auto den = add(pow(sinh_re, i2), pow(cos_im, i2));
+        *real_ = div(mul(sinh_re, cosh(*real_)), den);
+        *imag_ = div(mul(sin(*imag_), cos_im), den);
+    }
+
+    void bvisit(const Csch &x)
+    {
+        apply(*div(one, sinh(x.get_arg())));
+    }
+
+    void bvisit(const Sech &x)
+    {
+        apply(*div(one, cosh(x.get_arg())));
+    }
+
+    void bvisit(const Coth &x)
+    {
+        apply(*x.get_arg());
+        if (eq(**imag_, *zero)) {
+            *real_ = x.rcp_from_this();
+            return;
+        }
+        auto i2 = integer(2);
+        auto sinh_re = sinh(*real_), sin_im = sin(*imag_);
+        auto den = add(pow(sinh_re, i2), pow(sin_im, i2));
+        *real_ = div(mul(sinh_re, cosh(*real_)), den);
+        *imag_ = neg(div(mul(sin_im, cos(*imag_)), den));
+    }
+
+    void bvisit(const Function &x)
+    {
+        throw SymEngineException(
+            "Not Implemented classes for real and imaginary parts");
+    }
+
     void bvisit(const Symbol &x)
     {
-        throw SymEngineException("Not Implemented");
+        throw SymEngineException(
+            "Not Implemented classes for real and imaginary parts");
     }
 
     void bvisit(const Basic &x)
--- symengine/tests/basic/test_as_real_imag.cpp
+++ symengine/tests/basic/test_as_real_imag.cpp
@@ -1,6 +1,6 @@
 #include "catch.hpp"
 
-#include <symengine/real_imag.cpp>
+#include <symengine/as_real_imag.cpp>
 #include <symengine/symengine_casts.h>
 
 using SymEngine::Basic;
@@ -19,6 +19,24 @@ using SymEngine::add;
 using SymEngine::Rational;
 using SymEngine::symbol;
 using SymEngine::SymEngineException;
+using SymEngine::Nan;
+using SymEngine::Inf;
+using SymEngine::ComplexInf;
+using SymEngine::minus_one;
+using SymEngine::sin;
+using SymEngine::cos;
+using SymEngine::tan;
+using SymEngine::cot;
+using SymEngine::csc;
+using SymEngine::sec;
+using SymEngine::asin;
+using SymEngine::sinh;
+using SymEngine::cosh;
+using SymEngine::tanh;
+using SymEngine::coth;
+using SymEngine::csch;
+using SymEngine::sech;
+using SymEngine::asinh;
 
 TEST_CASE("RealImag: Number and Symbol", "[as_real_imag]")
 {
@@ -39,6 +57,14 @@ TEST_CASE("RealImag: Number and Symbol", "[as_real_imag]")
     REQUIRE(eq(*re, *neg(i2)));
     REQUIRE(eq(*im, *zero));
 
+    as_real_imag(Inf, outArg(re), outArg(im));
+    REQUIRE(eq(*re, *Inf));
+    REQUIRE(eq(*im, *zero));
+
+    as_real_imag(ComplexInf, outArg(re), outArg(im));
+    REQUIRE(eq(*re, *Nan));
+    REQUIRE(eq(*im, *Nan));
+
     // Symbol
     CHECK_THROWS_AS(
         as_real_imag(mul(add(i2, I), symbol("x")), outArg(re), outArg(im)),
@@ -48,13 +74,22 @@ TEST_CASE("RealImag: Number and Symbol", "[as_real_imag]")
 TEST_CASE("RealImag: Mul", "[as_real_imag]")
 {
     RCP<const Basic> re, im;
-    auto i2 = integer(2), i3 = integer(-3);
+    auto i2 = integer(2), i3 = integer(3);
 
     as_real_imag(neg(add(i2, I)), outArg(re), outArg(im));
     REQUIRE(eq(*re, *neg(i2)));
     REQUIRE(eq(*im, *neg(one)));
 
-    CHECK_THROWS_AS(as_real_imag(mul(add(i2, I), pow(I, rational(2, 3))),
+    as_real_imag(mul(i2, pow(I, rational(2, 3))), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *one));
+    REQUIRE(eq(*im, *sqrt(i3)));
+
+    as_real_imag(mul(add(i2, I), pow(I, rational(2, 3))), outArg(re),
+                 outArg(im));
+    REQUIRE(eq(*re, *sub(one, div(sqrt(i3), i2))));
+    REQUIRE(eq(*im, *add(sqrt(i3), div(one, i2))));
+
+    CHECK_THROWS_AS(as_real_imag(mul(add(i2, I), add(i2, mul(symbol("x"), I))),
                                  outArg(re), outArg(im)),
                     SymEngineException);
 }
@@ -80,6 +115,10 @@ TEST_CASE("RealImag: Pow", "[as_real_imag]")
     RCP<const Basic> re, im;
     auto i2 = integer(2);
 
+    as_real_imag(pow(I, rational(2, 3)), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *div(one, i2)));
+    REQUIRE(eq(*im, *div(sqrt(integer(3)), i2)));
+
     as_real_imag(sqrt(neg(I)), outArg(re), outArg(im));
     REQUIRE(eq(*re, *div(sqrt(i2), i2)));
     REQUIRE(eq(*im, *neg(div(sqrt(i2), i2))));
@@ -89,5 +128,134 @@ TEST_CASE("RealImag: Pow", "[as_real_imag]")
     REQUIRE(eq(*im, *div(sqrt(i2), i2)));
 
     CHECK_THROWS_AS(as_real_imag(pow(I, symbol("x")), outArg(re), outArg(im)),
+                    SymEngineException);
+}
+
+TEST_CASE("RealImag: Trigonometric functions", "[as_real_imag]")
+{
+    auto sq = sqrt(neg(I));
+    RCP<const Basic> re, im;
+    auto i2 = integer(2);
+
+    as_real_imag(sin(i2), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *sin(i2)));
+    REQUIRE(eq(*im, *zero));
+
+    as_real_imag(sin(add(i2, I)), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *mul(sin(i2), cosh(one))));
+    REQUIRE(eq(*im, *mul(cos(i2), sinh(one))));
+
+    as_real_imag(cos(i2), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *cos(i2)));
+    REQUIRE(eq(*im, *zero));
+
+    as_real_imag(cos(add(i2, I)), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *mul(cos(i2), cosh(one))));
+    REQUIRE(eq(*im, *mul({minus_one, sin(i2), sinh(one)})));
+
+    as_real_imag(tan(i2), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *tan(i2)));
+    REQUIRE(eq(*im, *zero));
+
+    as_real_imag(tan(add(i2, I)), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *div(sin(integer(4)), add(cosh(i2), cos(integer(4))))));
+    REQUIRE(eq(*im, *div(sinh(i2), add(cosh(i2), cos(integer(4))))));
+
+    as_real_imag(csc(i2), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *div(one, sin(i2))));
+    REQUIRE(eq(*im, *zero));
+
+    as_real_imag(csc(add(i2, I)), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *div(mul(sin(i2), cosh(one)),
+                         add(mul(pow(cos(i2), i2), pow(sinh(one), i2)),
+                             mul(pow(sin(i2), i2), pow(cosh(one), i2))))));
+    REQUIRE(eq(*im, *div(mul({minus_one, cos(i2), sinh(one)}),
+                         add(mul(pow(cos(i2), i2), pow(sinh(one), i2)),
+                             mul(pow(sin(i2), i2), pow(cosh(one), i2))))));
+
+    as_real_imag(sec(i2), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *div(one, cos(i2))));
+    REQUIRE(eq(*im, *zero));
+
+    as_real_imag(sec(add(i2, I)), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *div(mul(cos(i2), cosh(one)),
+                         add(mul(pow(cos(i2), i2), pow(cosh(one), i2)),
+                             mul(pow(sin(i2), i2), pow(sinh(one), i2))))));
+    REQUIRE(eq(*im, *div(mul(sin(i2), sinh(one)),
+                         add(mul(pow(cos(i2), i2), pow(cosh(one), i2)),
+                             mul(pow(sin(i2), i2), pow(sinh(one), i2))))));
+
+    as_real_imag(cot(i2), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *cot(i2)));
+    REQUIRE(eq(*im, *zero));
+
+    as_real_imag(cot(add(i2, I)), outArg(re), outArg(im));
+    REQUIRE(
+        eq(*re, *div(neg(sin(integer(4))), sub(cos(integer(4)), cosh(i2)))));
+    REQUIRE(eq(*im, *div(neg(sinh(i2)), sub(cos(integer(4)), cosh(i2)))));
+
+    as_real_imag(sinh(i2), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *sinh(i2)));
+    REQUIRE(eq(*im, *zero));
+
+    as_real_imag(sinh(add(i2, I)), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *mul(sinh(i2), cos(one))));
+    REQUIRE(eq(*im, *mul(cosh(i2), sin(one))));
+
+    as_real_imag(cosh(i2), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *cosh(i2)));
+    REQUIRE(eq(*im, *zero));
+
+    as_real_imag(cosh(add(i2, I)), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *mul(cosh(i2), cos(one))));
+    REQUIRE(eq(*im, *mul(sinh(i2), sin(one))));
+
+    as_real_imag(tanh(i2), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *tanh(i2)));
+    REQUIRE(eq(*im, *zero));
+
+    as_real_imag(tanh(add(i2, I)), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *div(mul(sinh(i2), cosh(i2)),
+                         add(pow(cos(one), i2), pow(sinh(i2), i2)))));
+    REQUIRE(eq(*im, *div(mul(sin(one), cos(one)),
+                         add(pow(cos(one), i2), pow(sinh(i2), i2)))));
+
+    as_real_imag(csch(i2), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *div(one, sinh(i2))));
+    REQUIRE(eq(*im, *zero));
+
+    as_real_imag(csch(add(i2, I)), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *div(mul(sinh(i2), cos(one)),
+                         add(mul(pow(cosh(i2), i2), pow(sin(one), i2)),
+                             mul(pow(sinh(i2), i2), pow(cos(one), i2))))));
+    REQUIRE(eq(*im, *div(mul({minus_one, cosh(i2), sin(one)}),
+                         add(mul(pow(cosh(i2), i2), pow(sin(one), i2)),
+                             mul(pow(sinh(i2), i2), pow(cos(one), i2))))));
+
+    as_real_imag(sech(i2), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *div(one, cosh(i2))));
+    REQUIRE(eq(*im, *zero));
+
+    as_real_imag(sech(add(i2, I)), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *div(mul(cosh(i2), cos(one)),
+                         add(mul(pow(cosh(i2), i2), pow(cos(one), i2)),
+                             mul(pow(sinh(i2), i2), pow(sin(one), i2))))));
+    REQUIRE(eq(*im, *div(mul({minus_one, sin(one), sinh(i2)}),
+                         add(mul(pow(cosh(i2), i2), pow(cos(one), i2)),
+                             mul(pow(sinh(i2), i2), pow(sin(one), i2))))));
+
+    as_real_imag(coth(i2), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *coth(i2)));
+    REQUIRE(eq(*im, *zero));
+
+    as_real_imag(coth(add(i2, I)), outArg(re), outArg(im));
+    REQUIRE(eq(*re, *div(mul(sinh(i2), cosh(i2)),
+                         add(pow(sin(one), i2), pow(sinh(i2), i2)))));
+    REQUIRE(eq(*im, *div(mul({minus_one, sin(one), cos(one)}),
+                         add(pow(sin(one), i2), pow(sinh(i2), i2)))));
+
+    CHECK_THROWS_AS(as_real_imag(asin(i2), outArg(re), outArg(im)),
+                    SymEngineException);
+    CHECK_THROWS_AS(as_real_imag(asinh(i2), outArg(re), outArg(im)),
                     SymEngineException);
 }
