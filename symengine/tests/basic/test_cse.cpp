4861623ac4d9e3512fcc16bc8f73c52c2eea5e00
Isuru Fernando <isuruf@gmail.com>
2017-09-26 10:36:01 -0500

    More tests

--- symengine/tests/basic/test_cse.cpp
+++ symengine/tests/basic/test_cse.cpp
@@ -20,6 +20,7 @@ using SymEngine::vec_pair;
 using SymEngine::sqrt;
 using SymEngine::sin;
 using SymEngine::cse;
+using SymEngine::one;
 using SymEngine::unified_eq;
 using SymEngine::integer;
 
@@ -29,9 +30,13 @@ TEST_CASE("CSE: simple", "[cse]")
     RCP<const Basic> x0 = symbol("x0");
     RCP<const Basic> x1 = symbol("x1");
     RCP<const Basic> x2 = symbol("x2");
+    RCP<const Basic> x3 = symbol("x3");
+    RCP<const Basic> x4 = symbol("x4");
     RCP<const Basic> y = symbol("y");
     RCP<const Basic> z = symbol("z");
     RCP<const Basic> w = symbol("w");
+    RCP<const Basic> im1 = integer(-1);
+    RCP<const Basic> im2 = integer(-2);
     RCP<const Basic> i2 = integer(2);
     RCP<const Basic> i3 = integer(3);
     RCP<const Basic> i4 = integer(4);
@@ -171,5 +176,121 @@ TEST_CASE("CSE: simple", "[cse]")
         cse(substs, reduced, {e});
         REQUIRE(unified_eq(substs, {{x0, pow(x, x)}}));
         REQUIRE(unified_eq(reduced, {div(sin(x0), x0)}));
+    }
+    {
+        auto e = add(pow(x, im2), pow(x, i2));
+        vec_pair substs;
+        vec_basic reduced;
+        cse(substs, reduced, {e});
+        REQUIRE(unified_eq(substs, {{x0, mul(x, x)}}));
+        REQUIRE(unified_eq(reduced, {add(pow(x0, im1), x0)}));
+    }
+    {
+        auto e = add(div(add(one, pow(x, im2)), pow(x, i2)), pow(x, i2));
+        vec_pair substs;
+        vec_basic reduced;
+        cse(substs, reduced, {e});
+        REQUIRE(unified_eq(substs, {{x0, mul(x, x)}, {x1, pow(x0, im1)}}));
+        REQUIRE(unified_eq(reduced, {add(mul(x1, add(one, x1)), x0)}));
+    }
+    {
+        auto e = add(mul(add(one, pow(x, im2)), pow(x, i2)), pow(x, im2));
+        vec_pair substs;
+        vec_basic reduced;
+        cse(substs, reduced, {e});
+        REQUIRE(unified_eq(substs, {{x0, mul(x, x)}, {x1, pow(x0, im1)}}));
+        REQUIRE(unified_eq(reduced, {add(mul(x0, add(one, x1)), x1)}));
+    }
+    {
+        auto e = add(cos(pow(x, im2)), sin(pow(x, im2)));
+        vec_pair substs;
+        vec_basic reduced;
+        cse(substs, reduced, {e});
+        REQUIRE(unified_eq(substs, {{x0, pow(x, im2)}}));
+        REQUIRE(unified_eq(reduced, {add(sin(x0), cos(x0))}));
+    }
+    {
+        auto e = add(cos(pow(x, i2)), sin(pow(x, i2)));
+        vec_pair substs;
+        vec_basic reduced;
+        cse(substs, reduced, {e});
+        REQUIRE(unified_eq(substs, {{x0, pow(x, i2)}}));
+        REQUIRE(unified_eq(reduced, {add(sin(x0), cos(x0))}));
+    }
+    {
+        auto e = add(div(y, add(i2, pow(x, i2))), div(div(z, pow(x, i2)), y));
+        vec_pair substs;
+        vec_basic reduced;
+        cse(substs, reduced, {e});
+        REQUIRE(unified_eq(substs, {{x0, pow(x, i2)}}));
+        REQUIRE(unified_eq(reduced,
+                           {add(div(y, add(x0, i2)), div(z, mul(x0, y)))}));
+    }
+    {
+        auto e = add(exp(pow(x, i2)), mul(pow(x, i2), cos(pow(x, im2))));
+        vec_pair substs;
+        vec_basic reduced;
+        cse(substs, reduced, {e});
+        REQUIRE(unified_eq(substs, {{x0, pow(x, i2)}}));
+        REQUIRE(
+            unified_eq(reduced, {add(exp(x0), mul(x0, cos(pow(x0, im1))))}));
+    }
+    {
+        auto e = div(add(one, pow(x, im2)), pow(x, i2));
+        vec_pair substs;
+        vec_basic reduced;
+        cse(substs, reduced, {e});
+        REQUIRE(unified_eq(substs, {{x0, pow(x, im2)}}));
+        REQUIRE(unified_eq(reduced, {mul(x0, add(x0, one))}));
+    }
+    {
+        auto e = add(pow(x, mul(i2, y)), pow(x, mul(im2, y)));
+        vec_pair substs;
+        vec_basic reduced;
+        cse(substs, reduced, {e});
+        REQUIRE(unified_eq(substs, {{x0, pow(x, mul(i2, y))}}));
+        REQUIRE(unified_eq(reduced, {add(x0, div(one, x0))}));
+    }
+    {
+        auto z1 = add(x0, y);
+        auto z2 = add(x2, x3);
+        auto e1 = add(cos(z1), z1);
+        auto e2 = add(cos(z2), z2);
+        auto e3 = add(x0, x2);
+        vec_pair substs;
+        vec_basic reduced;
+        cse(substs, reduced, {e1, e2, e3});
+        REQUIRE(unified_eq(substs, {{x1, add(x0, y)}, {x4, add(x2, x3)}}));
+        REQUIRE(unified_eq(reduced,
+                           {add(x1, cos(x1)), add(x4, cos(x4)), add(x0, x2)}));
+    }
+    {
+        auto e1 = add(x, i3);
+        auto e2 = add(x, i4);
+        vec_pair substs;
+        vec_basic reduced;
+        cse(substs, reduced, {e1, e2});
+        REQUIRE(unified_eq(substs, {}));
+        REQUIRE(unified_eq(reduced, {e1, e2}));
+    }
+    {
+        auto e = div(x, sub(pow(y, i2), mul(i4, pow(x, i2))));
+        vec_pair substs;
+        vec_basic reduced;
+        cse(substs, reduced, {e});
+        REQUIRE(unified_eq(substs, {}));
+        REQUIRE(unified_eq(reduced, {e}));
+    }
+    {
+        auto e1 = add(x, y);
+        auto e2 = add(add(x, y), i2);
+        auto e3 = add(add(x, y), z);
+        auto e4 = add(add(add(x, y), z), i3);
+        // x + y, 2 + x + y, x + y + z, 3 + x + y + z
+        vec_pair substs;
+        vec_basic reduced;
+        cse(substs, reduced, {e1, e2, e3, e4});
+        REQUIRE(unified_eq(substs, {{x0, add(x, y)}, {x1, add(x0, z)}}));
+        REQUIRE(unified_eq(reduced, {x0, add(i2, x0), x1, add(i3, x1)}));
     }
 }
