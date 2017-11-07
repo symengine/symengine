0a53cc20755fbbbcd594b3451208cb5fca884652
Gerrit Ansmann <bla@example.com>
2017-11-07 11:30:39 +0100

    Test code printing for rationals.

--- symengine/tests/printing/test_ccode.cpp
+++ symengine/tests/printing/test_ccode.cpp
@@ -24,6 +24,7 @@ using SymEngine::NegInf;
 using SymEngine::boolTrue;
 using SymEngine::sin;
 using SymEngine::sqrt;
+using SymEngine::rational;
 
 TEST_CASE("Arithmetic", "[ccode]")
 {
@@ -32,6 +33,15 @@ TEST_CASE("Arithmetic", "[ccode]")
     auto p = add(add(add(add(x, mul(x, y)), pow(x, y)), mul(x, x)),
                  sqrt(integer(2)));
     REQUIRE(ccode(*p) == "x + x*y + sqrt(2) + pow(x, 2) + pow(x, y)");
+}
+
+TEST_CASE("Rational", "[ccode]")
+{
+    auto p = rational(1,3);
+    REQUIRE(ccode(*p) == "1.0/3.0");
+
+    p = rational(1,-3);
+    REQUIRE(ccode(*p) == "-1.0/3.0");
 }
 
 TEST_CASE("Functions", "[ccode]")
