67ab59c854b995116c868701da87cb6658251064
Björn Dahlgren <bjodah@gmail.com>
2018-03-14 19:50:16 +0100

    Build using _GLIBCXX_DEBUG for 1 Travis job

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -95,7 +95,12 @@ if [[ "${CC}" == *"clang"* ]] && [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
         export CXXFLAGS=""
     fi
 else
-    export CXXFLAGS="-Werror"
+    if [[ "${CC}" == *"gcc"* ]] && [[ "${BUILD_TYPE}" == "Debug" ]] && [[ "${BUILD_SHARED_LIBS}" == "yes" ]] && [[ "${WITH_GCC_6}" == "yes" ]] && [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+        rm -r /home/travis/.ccache  # remove this line before merge
+        export CXXFLAGS="-Werror -D_GLIBCXX_DEBUG"
+    else
+        export CXXFLAGS="-Werror"
+    fi
 fi
 cmake $cmake_line ${SOURCE_DIR}
 
