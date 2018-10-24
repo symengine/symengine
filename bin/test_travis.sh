f33ee5e5c006a4b5a7c5edfc44f88622acac8e5d
Björn Dahlgren <bjodah@gmail.com>
2018-10-24 13:36:38 +0200

    see if setting LD_LIBRARY_PATH can aid finding the right lib*san.so

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -100,7 +100,8 @@ if [[ "${USE_GLIBCXX_DEBUG}" == "yes" ]]; then
 fi
 if [[ "${WITH_SANITIZE}" != "" ]]; then
 	export CXXFLAGS="$CXXFLAGS -fsanitize=${WITH_SANITIZE}"
-	if [[ "{WITH_SANITIZE}" == "address" ]]; then
+	export LD_LIBRARY_PATH="/usr/lib/llvm-7/lib"
+	if [[ "${WITH_SANITIZE}" == "address" ]]; then
 	    export ASAN_OPTIONS=symbolize=1,detect_leaks=0,external_symbolizer_path=/usr/lib/llvm-7/bin/llvm-symbolizer
 	elif [[ "${WITH_SANITIZE}" == "undefined" ]]; then
 	    export UBSAN_OPTIONS=print_stacktrace=1,halt_on_error=1,external_symbolizer_path=/usr/lib/llvm-7/bin/llvm-symbolizer
