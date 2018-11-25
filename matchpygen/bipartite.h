e7bfe607d4b1235533c57ef50588e993ce4ef5a7
Francesco Bonazzi <franz.bonazzi@gmail.com>
2018-11-25 22:12:57 +0100

    correcting formatting

--- matchpygen/bipartite.h
+++ matchpygen/bipartite.h
@@ -16,26 +16,26 @@ typedef tuple<int, int> TItem; // either TLeft or TRight, use <variant> instead?
 typedef TItem TRight;
 typedef TItem TLeft;
 
-
 const int LEFT = 0;
 const int RIGHT = 1;
 
-typedef map<tuple<int, int>, tuple<int, int> > Matching;
+typedef map<tuple<int, int>, tuple<int, int>> Matching;
 
 //(Generic[TLeft, TRight, TEdgeValue], MutableMapping[Tuple[TLeft, TRight], TEdgeValue])
 /*
  * A bipartite graph representation.
  */
 template<typename TEdgeValue>
-class BipartiteGraph {
+class BipartiteGraph
+{
 public:
 
-	//Node = Tuple[int, Union[TLeft, TRight]]
-	// typedef tuple<int, variant<TLeft, TRight>> Node; ???
-	typedef tuple<int, TItem> Node;
-	typedef vector<Node> NodeList;
-	typedef set<Node> NodeSet;
-	typedef tuple<TLeft, TRight> Edge;
+    //Node = Tuple[int, Union[TLeft, TRight]]
+    // typedef tuple<int, variant<TLeft, TRight>> Node; ???
+    typedef tuple<int, TItem> Node;
+    typedef vector<Node> NodeList;
+    typedef set<Node> NodeSet;
+    typedef tuple<TLeft, TRight> Edge;
 
     map<Edge, TEdgeValue> _edges;
     map<int, int> _matching;
@@ -45,11 +45,14 @@ public:
     set<TRight> _right;
     map<Node, set<Node>> _graph;
 
-    BipartiteGraph() {}
+    BipartiteGraph()
+    {
+    }
 
-    void __setitem__(Edge key, TEdgeValue value) {
-//        if not isinstance(key, tuple) or len(key) != 2:
-//            raise TypeError("The edge must be a 2-tuple")
+    void __setitem__(Edge key, TEdgeValue value)
+    {
+        //if not isinstance(key, tuple) or len(key) != 2:
+        //    raise TypeError("The edge must be a 2-tuple")
         _edges[key] = value;
         _left.insert(get<0>(key));
         _right.insert(get<1>(key));
@@ -69,11 +72,13 @@ public:
         _graph[k2].insert(make_tuple(LEFT, get<0>(key)));
     }
 
-    TEdgeValue &__getitem__(Edge key) {
+    TEdgeValue &__getitem__(Edge key)
+    {
         return _edges[key];
     }
 
-    void __delitem__(Edge key) {
+    void __delitem__(Edge key)
+    {
         _edges.erase(key);
         //if all(l != key[0] for (l, _) in self._edges):
         //    self._left.remove(key[0])
@@ -94,19 +99,22 @@ public:
             }
         }
         //self._graph[(LEFT, key[0])].remove((RIGHT, key[1]))
-        _graph[make_tuple(LEFT, get<0>(key))].erase(make_tuple(RIGHT, get<1>(key)));
+        _graph[make_tuple(LEFT, get<0>(key))].erase(
+                make_tuple(RIGHT, get<1>(key)));
         //self._graph[(RIGHT, key[1])].remove((LEFT, key[0]))
-        _graph[make_tuple(RIGHT, get<1>(key))].erase(make_tuple(LEFT, get<0>(key)));
+        _graph[make_tuple(RIGHT, get<1>(key))].erase(
+                make_tuple(LEFT, get<0>(key)));
     }
 
-//    def edges_with_labels(self):
-//        """Returns a view on the edges with labels."""
-//        return self._edges.items()
+    //def edges_with_labels(self):
+    //    """Returns a view on the edges with labels."""
+    //    return self._edges.items()
 
-//    def edges(self):
-//        return self._edges.keys()
+    //def edges(self):
+    //    return self._edges.keys()
 
-    void clear() {
+    void clear()
+    {
         _edges.clear();
         _left.clear();
         _right.clear();
@@ -115,128 +123,139 @@ public:
 
 };
 
-
 template<typename TEdgeType>
-class HopcroftKarp {
+class HopcroftKarp
+{
 public:
 
-	HopcroftKarp(BipartiteGraph<TEdgeType> &bipartite) : bipartite(bipartite) {
-		reference_distance = -1;
-	}
+    HopcroftKarp(BipartiteGraph<TEdgeType> &bipartite) :
+            bipartite(bipartite)
+    {
+        reference_distance = -1;
+    }
 
-	int hopcroft_karp() {
-		pair_left.clear();
-		pair_right.clear();
-		dist_left.clear();
-		vertex_queue.clear();
-		int matching = 0;
-		while (true) {
-			if (!_bfs_hopcroft_karp())
-				break;
-			for (const TLeft &left : bipartite._left) {
-				if ((pair_left.find(left) == pair_left.end()) && _dfs_hopcroft_karp(left)) {
-					matching++;
-				}
-			}
-		}
-		return matching;
-	}
+    int hopcroft_karp()
+    {
+        pair_left.clear();
+        pair_right.clear();
+        dist_left.clear();
+        vertex_queue.clear();
+        int matching = 0;
+        while (true) {
+            if (!_bfs_hopcroft_karp())
+                break;
+            for (const TLeft &left : bipartite._left) {
+                if ((pair_left.find(left) == pair_left.end())
+                        && _dfs_hopcroft_karp(left)) {
+                    matching++;
+                }
+            }
+        }
+        return matching;
+    }
 
-	void dump_state() {
-		cout << endl;
-		for (const pair<TLeft, TRight> &p : pair_left) {
-			int l = get<1>(p.first);
-			cout << l << " " << get<1>(p.second) << endl;
-		}
-		cout << endl;
-		cout << "Distances:\n";
-		for (const pair<TLeft, int> &p : dist_left) {
-			cout << "Vertex: " << get<1>(p.first) << " " << p.second << endl;
-		}
-		return;
-	}
+    void dump_state()
+    {
+        cout << endl;
+        for (const pair<TLeft, TRight> &p : pair_left) {
+            int l = get<1>(p.first);
+            cout << l << " " << get<1>(p.second) << endl;
+        }
+        cout << endl;
+        cout << "Distances:\n";
+        for (const pair<TLeft, int> &p : dist_left) {
+            cout << "Vertex: " << get<1>(p.first) << " " << p.second << endl;
+        }
+        return;
+    }
 
 private:
-	map<TLeft, TRight> pair_left;
-	map<TRight, TLeft> pair_right;
-	BipartiteGraph<TEdgeType> &bipartite;
-	deque<TLeft> vertex_queue;
-	map<TLeft, int> dist_left;
-	int reference_distance;
+    map<TLeft, TRight> pair_left;
+    map<TRight, TLeft> pair_right;
+    BipartiteGraph<TEdgeType> &bipartite;
+    deque<TLeft> vertex_queue;
+    map<TLeft, int> dist_left;
+    int reference_distance;
 
-	bool _bfs_hopcroft_karp() {
-		for (const TLeft &left_vert : bipartite._left) {
-			if (pair_left.find(left_vert) == pair_left.end()) {
-				vertex_queue.push_back(left_vert);
-				dist_left[left_vert] = 0;
-			} else {
-				dist_left.erase(left_vert);
-			}
-		}
-		reference_distance = -1;
-		while (true) {
-			if (vertex_queue.size() == 0)
-				break;
-			TLeft &left_vertex = vertex_queue.front();
-			vertex_queue.pop_front();
-			if (dist_left.find(left_vertex) == dist_left.end())
-				continue;
-			for (const tuple<int, TRight> &p : bipartite._graph[make_tuple(LEFT, left_vertex)]) {
-				if (get<0>(p) == LEFT) {
-					// not a left vertex, continue
-					throw("inconsistent graph");
-				}
-				const TRight &right_vertex = get<1>(p);
-				if (pair_right.find(right_vertex) == pair_right.end()) {
-					if (reference_distance == -1) {
-						reference_distance = dist_left[left_vertex] + 1;
-					}
-				} else {
-					TLeft &other_left = pair_right.at(right_vertex);
-					if (dist_left.find(other_left) == dist_left.end()) {
-						dist_left[other_left] = dist_left[left_vertex] + 1;
-						vertex_queue.push_back(other_left);
-					}
-				}
+    bool _bfs_hopcroft_karp()
+    {
+        for (const TLeft &left_vert : bipartite._left) {
+            if (pair_left.find(left_vert) == pair_left.end()) {
+                vertex_queue.push_back(left_vert);
+                dist_left[left_vert] = 0;
+            } else {
+                dist_left.erase(left_vert);
+            }
+        }
+        reference_distance = -1;
+        while (true) {
+            if (vertex_queue.size() == 0)
+                break;
+            TLeft &left_vertex = vertex_queue.front();
+            vertex_queue.pop_front();
+            if (dist_left.find(left_vertex) == dist_left.end())
+                continue;
+            for (const tuple<int, TRight> &p : bipartite._graph[make_tuple(LEFT,
+                    left_vertex)]) {
+                if (get<0>(p) == LEFT) {
+                    // not a left vertex, continue
+                    throw("inconsistent graph");
+                }
+                const TRight &right_vertex = get<1>(p);
+                if (pair_right.find(right_vertex) == pair_right.end()) {
+                    if (reference_distance == -1) {
+                        reference_distance = dist_left[left_vertex] + 1;
+                    }
+                } else {
+                    TLeft &other_left = pair_right.at(right_vertex);
+                    if (dist_left.find(other_left) == dist_left.end()) {
+                        dist_left[other_left] = dist_left[left_vertex] + 1;
+                        vertex_queue.push_back(other_left);
+                    }
+                }
 
-			}
-		}
-		return reference_distance != -1;
-	}
+            }
+        }
+        return reference_distance != -1;
+    }
 
-	bool _dfs_hopcroft_karp(const TLeft &left) {
-		for (const tuple<int, TRight> &p : bipartite._graph[make_tuple(LEFT, left)]) {
-			const TRight &right = get<1>(p);
-			int distance;
+    bool _dfs_hopcroft_karp(const TLeft &left)
+    {
+        for (const tuple<int, TRight> &p : bipartite._graph[make_tuple(LEFT,
+                left)]) {
+            const TRight &right = get<1>(p);
+            int distance;
 
-			if (pair_right.find(right) == pair_right.end()) {
-				distance = reference_distance;
-			} else {
-				TLeft &other_left = pair_right.at(right);
-				if (dist_left.find(other_left) == dist_left.end()) {
-					distance = reference_distance;
-				} else {
-					distance = dist_left.at(other_left);
-				}
-			}
-			if (distance == dist_left.at(left) + 1) {
-				pair_left[left] = right;
-				pair_right[right] = left;
-				return true;
-			}
+            if (pair_right.find(right) == pair_right.end()) {
+                distance = reference_distance;
+            } else {
+                TLeft &other_left = pair_right.at(right);
+                if (dist_left.find(other_left) == dist_left.end()) {
+                    distance = reference_distance;
+                } else {
+                    distance = dist_left.at(other_left);
+                }
+            }
+            if (distance == dist_left.at(left) + 1) {
+                pair_left[left] = right;
+                pair_right[right] = left;
+                return true;
+            }
 
-		}
-		dist_left.erase(left);
-		return false;
-	}
+        }
+        dist_left.erase(left);
+        return false;
+    }
 
 };
 
-int get0(tuple<int, int> a) {
-	return get<0>(a);
+int get0(tuple<int, int> a)
+{
+    return get<0>(a);
 }
-int get1(tuple<int, int> a) {
-	return get<1>(a);
+int get1(tuple<int, int> a)
+{
+    return get<1>(a);
 }
 
 #endif
--- matchpygen/test_bipartite.cpp
+++ matchpygen/test_bipartite.cpp
@@ -1,55 +1,83 @@
 #include "bipartite.h"
 
-void test_hopcroft_karp_bipartite1() {
-	BipartiteGraph<string> bipartite;
+void test_hopcroft_karp_bipartite1()
+{
+    BipartiteGraph<string> bipartite;
 
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 0), make_tuple(RIGHT, 0)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 0), make_tuple(RIGHT, 1)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 1), make_tuple(RIGHT, 0)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 1), make_tuple(RIGHT, 4)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 2), make_tuple(RIGHT, 2)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 2), make_tuple(RIGHT, 3)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 3), make_tuple(RIGHT, 0)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 3), make_tuple(RIGHT, 4)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 4), make_tuple(RIGHT, 1)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 4), make_tuple(RIGHT, 3)), "X");
+    bipartite.__setitem__(make_tuple(make_tuple(LEFT, 0), make_tuple(RIGHT, 0)),
+            "X");
+    bipartite.__setitem__(make_tuple(make_tuple(LEFT, 0), make_tuple(RIGHT, 1)),
+            "X");
+    bipartite.__setitem__(make_tuple(make_tuple(LEFT, 1), make_tuple(RIGHT, 0)),
+            "X");
+    bipartite.__setitem__(make_tuple(make_tuple(LEFT, 1), make_tuple(RIGHT, 4)),
+            "X");
+    bipartite.__setitem__(make_tuple(make_tuple(LEFT, 2), make_tuple(RIGHT, 2)),
+            "X");
+    bipartite.__setitem__(make_tuple(make_tuple(LEFT, 2), make_tuple(RIGHT, 3)),
+            "X");
+    bipartite.__setitem__(make_tuple(make_tuple(LEFT, 3), make_tuple(RIGHT, 0)),
+            "X");
+    bipartite.__setitem__(make_tuple(make_tuple(LEFT, 3), make_tuple(RIGHT, 4)),
+            "X");
+    bipartite.__setitem__(make_tuple(make_tuple(LEFT, 4), make_tuple(RIGHT, 1)),
+            "X");
+    bipartite.__setitem__(make_tuple(make_tuple(LEFT, 4), make_tuple(RIGHT, 3)),
+            "X");
 
-	HopcroftKarp<string> hk(bipartite);
-	cout << "Maximum matchings: " << hk.hopcroft_karp() << endl;
-	hk.dump_state();
+    HopcroftKarp<string> hk(bipartite);
+    cout << "Maximum matchings: " << hk.hopcroft_karp() << endl;
+    hk.dump_state();
 }
 
-void test_hopcroft_karp_bipartite2() {
-	BipartiteGraph<string> bipartite;
+void test_hopcroft_karp_bipartite2()
+{
+    BipartiteGraph<string> bipartite;
 
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'a'-'a'), make_tuple(RIGHT, 1)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'b'-'a'), make_tuple(RIGHT, 1)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'b'-'a'), make_tuple(RIGHT, 2)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'c'-'a'), make_tuple(RIGHT, 1)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'c'-'a'), make_tuple(RIGHT, 2)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'd'-'a'), make_tuple(RIGHT, 2)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'd'-'a'), make_tuple(RIGHT, 3)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'd'-'a'), make_tuple(RIGHT, 4)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'e'-'a'), make_tuple(RIGHT, 3)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'e'-'a'), make_tuple(RIGHT, 4)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'f'-'a'), make_tuple(RIGHT, 4)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'f'-'a'), make_tuple(RIGHT, 5)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'f'-'a'), make_tuple(RIGHT, 6)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'g'-'a'), make_tuple(RIGHT, 5)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'g'-'a'), make_tuple(RIGHT, 6)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'g'-'a'), make_tuple(RIGHT, 7)), "X");
-	bipartite.__setitem__(make_tuple(make_tuple(LEFT, 'h'-'a'), make_tuple(RIGHT, 8)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'a' - 'a'), make_tuple(RIGHT, 1)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'b' - 'a'), make_tuple(RIGHT, 1)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'b' - 'a'), make_tuple(RIGHT, 2)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'c' - 'a'), make_tuple(RIGHT, 1)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'c' - 'a'), make_tuple(RIGHT, 2)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'd' - 'a'), make_tuple(RIGHT, 2)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'd' - 'a'), make_tuple(RIGHT, 3)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'd' - 'a'), make_tuple(RIGHT, 4)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'e' - 'a'), make_tuple(RIGHT, 3)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'e' - 'a'), make_tuple(RIGHT, 4)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'f' - 'a'), make_tuple(RIGHT, 4)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'f' - 'a'), make_tuple(RIGHT, 5)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'f' - 'a'), make_tuple(RIGHT, 6)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'g' - 'a'), make_tuple(RIGHT, 5)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'g' - 'a'), make_tuple(RIGHT, 6)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'g' - 'a'), make_tuple(RIGHT, 7)), "X");
+    bipartite.__setitem__(
+            make_tuple(make_tuple(LEFT, 'h' - 'a'), make_tuple(RIGHT, 8)), "X");
 
-	HopcroftKarp<string> hk(bipartite);
-	cout << "Maximum matchings: " << hk.hopcroft_karp() << endl;
-	hk.dump_state();
+    HopcroftKarp<string> hk(bipartite);
+    cout << "Maximum matchings: " << hk.hopcroft_karp() << endl;
+    hk.dump_state();
 }
 
-int main(int argc, char **argv) {
-	cout << "hello\n";
+int main(int argc, char **argv)
+{
+    cout << "Test BipartiteGraph\n";
 
-	test_hopcroft_karp_bipartite1();
-	test_hopcroft_karp_bipartite2();
-
+    test_hopcroft_karp_bipartite1();
+    test_hopcroft_karp_bipartite2();
 }
-
