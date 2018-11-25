565d6c6a33741c8ded16b9325313255c8defe737
Francesco Bonazzi <franz.bonazzi@gmail.com>
2018-11-25 15:23:34 +0100

    BipartiteGraph and Hopcroft-Karp algorithm in C++

---
+++ matchpygen/common.h
@@ -0,0 +1,10 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <symengine/basic.h>
+#include <map>
+#include <string>
+
+typedef std::map<std::string, SymEngine::RCP<const SymEngine::Basic> > Substitution2;
+
+#endif
