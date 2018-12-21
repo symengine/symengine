0be23b33edfd4177f07d516dc61c01e803fdbf84
Isuru Fernando <isuruf@gmail.com>
2018-12-20 22:12:20 -0600

    Add test

--- symengine/tests/basic/test_basic.cpp
+++ symengine/tests/basic/test_basic.cpp
@@ -556,6 +556,10 @@ TEST_CASE("Diff: Basic", "[basic]")
     r1 = sdiff(add(pow(x, i2), x), x);
     r2 = diff(add(pow(x, i2), x), x);
     REQUIRE(eq(*r1, *r2));
+
+    // Test that this doesn't segfault
+    r1 = pow(sqrt(div(x, i3)), real_double(2.0));
+    r1 = diff(r1, x);
 }
 
 TEST_CASE("compare: Basic", "[basic]")
