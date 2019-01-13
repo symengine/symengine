70925a9a18b65dbbd9823925a05cd252235baddd
Francesco Bonazzi <franz.bonazzi@gmail.com>
2019-01-13 19:54:17 +0100

    added example with setjmp

--- matchpygen/example_coroutine2.cpp
+++ matchpygen/example_coroutine2.cpp
@@ -43,25 +43,25 @@ template<typename T>
 class Yielder
 {
 public:
-	Yielder() {}
-	virtual ~Yielder() {
-		coroutine::destroy(routine);
-	}
+    Yielder() {}
+    virtual ~Yielder() {
+        coroutine::destroy(routine);
+    }
 
-	T next()
-	{
-		coroutine::resume(routine);
-		return value;
-	}
+    T next()
+    {
+        coroutine::resume(routine);
+        return value;
+    }
 protected:
-	void yield(T val)
-	{
-		value = val;
-		//channel.push(val);
-		coroutine::yield();
-	}
-	coroutine::routine_t routine;
-	T value;
+    void yield(T val)
+    {
+        value = val;
+        //channel.push(val);
+        coroutine::yield();
+    }
+    coroutine::routine_t routine;
+    T value;
 };
 
 //Yielder<tuple<int, Substitution2>> match_root;
@@ -70,60 +70,60 @@ protected:
 class match_root : public Yielder<tuple<int, Substitution2>>
 {
 public:
-	RCP<const Basic> subject;
+    RCP<const Basic> subject;
 
-	match_root(RCP<const Basic> subject) : subject(subject)
-	{
-		routine = coroutine::create(std::bind(&match_root::run, this));
-	}
-	void run() {
-		Deque subjects;
-		subjects.push_back(subject);
-		Substitution2 subst0;
-		// state 2200;
-		if (subjects.size() >= 1 && is_a<Pow>(*subjects[0])) {
-			RCP<const Basic> tmp1 = subjects.front();
-			subjects.pop_front();
-			Deque subjects2 = get_deque(tmp1);
-			// state 2201;
-			if (subjects2.size() >= 1 && subjects2[0]->__eq__(*symbol("x"))) {
-				RCP<const Basic> tmp3 = subjects2.front();
-				subjects2.pop_front();
-				// state 2202;
-				if (subjects2.size() >= 1) {
-					auto tmp4 = subjects2.front();
-					subjects2.pop_front();
-					Substitution2 subst1(subst0);
-					if (!try_add_variable(subst1, "i2", tmp4)) {
-						// state 2203;
-						if (subjects2.size() == 0) {
-							// state 2204;
-							if (subjects.size() == 0) {
-								Substitution2 tmp_subst;
-								tmp_subst["w"] = subst1["i2"];
-								// 0: x**w;
-								yield(make_tuple(0, tmp_subst));
-							}
-						}
-					}
-					subjects2.push_front(tmp4);
-				}
-				subjects2.push_front(tmp3);
-			}
-			subjects.push_front(tmp1);
-		}
-		if (subjects.size() >= 1 && subjects[0]->__eq__(*symbol("y"))) {
-			auto &tmp6 = subjects.front();
-			subjects.pop_front();
-			// state 2205;
-			if (subjects.size() == 0) {
-				// 1: y;
-				yield(make_tuple(1, subst0));
-			}
-			subjects.push_front(tmp6);
-		}
-		yield(make_tuple(-1, subst0));
-	}
+    match_root(RCP<const Basic> subject) : subject(subject)
+    {
+        routine = coroutine::create(std::bind(&match_root::run, this));
+    }
+    void run() {
+        Deque subjects;
+        subjects.push_back(subject);
+        Substitution2 subst0;
+        // state 2200;
+        if (subjects.size() >= 1 && is_a<Pow>(*subjects[0])) {
+            RCP<const Basic> tmp1 = subjects.front();
+            subjects.pop_front();
+            Deque subjects2 = get_deque(tmp1);
+            // state 2201;
+            if (subjects2.size() >= 1 && subjects2[0]->__eq__(*symbol("x"))) {
+                RCP<const Basic> tmp3 = subjects2.front();
+                subjects2.pop_front();
+                // state 2202;
+                if (subjects2.size() >= 1) {
+                    auto tmp4 = subjects2.front();
+                    subjects2.pop_front();
+                    Substitution2 subst1(subst0);
+                    if (!try_add_variable(subst1, "i2", tmp4)) {
+                        // state 2203;
+                        if (subjects2.size() == 0) {
+                            // state 2204;
+                            if (subjects.size() == 0) {
+                                Substitution2 tmp_subst;
+                                tmp_subst["w"] = subst1["i2"];
+                                // 0: x**w;
+                                yield(make_tuple(0, tmp_subst));
+                            }
+                        }
+                    }
+                    subjects2.push_front(tmp4);
+                }
+                subjects2.push_front(tmp3);
+            }
+            subjects.push_front(tmp1);
+        }
+        if (subjects.size() >= 1 && subjects[0]->__eq__(*symbol("y"))) {
+            auto &tmp6 = subjects.front();
+            subjects.pop_front();
+            // state 2205;
+            if (subjects.size() == 0) {
+                // 1: y;
+                yield(make_tuple(1, subst0));
+            }
+            subjects.push_front(tmp6);
+        }
+        yield(make_tuple(-1, subst0));
+    }
 
 };
 
