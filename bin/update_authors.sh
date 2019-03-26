bc712e2661a893d430eb2b287e90c8b8f192c0a2
Ondřej Čertík <ondrej@certik.us>
2019-03-26 12:52:44 -0600

    Use --topo-order

--- bin/update_authors.sh
+++ bin/update_authors.sh
@@ -5,5 +5,5 @@ set -ex
 tmpfile=$(mktemp)
 
 head -n 5 AUTHORS > $tmpfile
-git log --reverse --format="%aN <%aE>" | awk ' !x[$0]++' >> $tmpfile
+git log --reverse --topo-order --format="%aN <%aE>" | awk ' !x[$0]++' >> $tmpfile
 mv $tmpfile AUTHORS
