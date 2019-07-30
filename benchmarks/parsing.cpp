ef477db6028566fa81bfe793e0c3bf776998f71f
Ondřej Čertík <ondrej@certik.us>
2019-07-30 12:10:15 -0600

    Better benchmark

--- benchmarks/parsing.cpp
+++ benchmarks/parsing.cpp
@@ -15,24 +15,34 @@ int main(int argc, char *argv[])
     RCP<const Basic> a;
     int N;
 
-    N = 3000;
+    std::string text;
+    std::string t0 = "(x + y - sin(x)/(z**2-4) - x**(y**z))";
+    text = t0;
+    for (int i = 0; i < 1000; i++) {
+        text = text + " * " + t0;
+    }
+    //std::cout << text << std::endl;
+
+    N = 1;
     auto t1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < N; i++) {
-        a = parse("x + y - sin(x)/(z**2-4) - x**(y**z)");
+        a = parse(text);
     }
     auto t2 = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1)
                      .count()
               << "ms" << std::endl;
+    std::cout << *a << std::endl;
 
     t1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < N; i++) {
-        a = parse_old("x + y - sin(x)/(z**2-4) - x**(y**z)");
+        a = parse_old(text);
     }
     t2 = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1)
                      .count()
               << "ms" << std::endl;
+    std::cout << *a << std::endl;
 
     return 0;
 }
