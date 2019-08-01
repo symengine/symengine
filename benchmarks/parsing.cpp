a321afe5dbdeaaf4654da7182f3d564bb5c640ae
Ondřej Čertík <ondrej@certik.us>
2019-08-01 11:26:03 -0600

    X

--- benchmarks/parsing.cpp
+++ benchmarks/parsing.cpp
@@ -15,34 +15,24 @@ int main(int argc, char *argv[])
     RCP<const Basic> a;
     int N;
 
-    std::string text;
-    std::string t0 = "(x + y - sin(x)/(z**2-4) - x**(y**z))";
-    text = t0;
-    for (int i = 0; i < 5000; i++) {
-        text = text + " * " + t0;
-    }
-    //std::cout << text << std::endl;
-
-    N = 1;
+    N = 3000;
     auto t1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < N; i++) {
-        a = parse(text);
+        a = parse("x + y - sin(x)/(z**2-4) - x**(y**z)");
     }
     auto t2 = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1)
                      .count()
               << "ms" << std::endl;
-    std::cout << *a << std::endl;
 
     t1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < N; i++) {
-        a = parse_old(text);
+        a = parse_old("x + y - sin(x)/(z**2-4) - x**(y**z)");
     }
     t2 = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1)
                      .count()
               << "ms" << std::endl;
-    std::cout << *a << std::endl;
 
     return 0;
 }
