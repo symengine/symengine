ed27e20120576e6b4c1c0f8e1afeffec257f1f8c
Ondřej Čertík <ondrej@certik.us>
2019-08-04 19:42:49 -0600

    Add explicit constructors and assignments

--- symengine/parser/parser_new.h
+++ symengine/parser/parser_new.h
@@ -13,6 +13,16 @@ struct YYSTYPE {
     SymEngine::RCP<const SymEngine::Basic> basic;
     SymEngine::vec_basic basic_vec;
     std::string string;
+    // Constructor
+    YYSTYPE() = default;
+    // Destructor
+    ~YYSTYPE() = default;
+    // Copy constructor and assignment
+    YYSTYPE(const YYSTYPE &) = default;
+    YYSTYPE &operator=(const YYSTYPE &) = default;
+    // Move constructor and assignment
+    YYSTYPE(YYSTYPE &&) = default;
+    YYSTYPE &operator=(YYSTYPE &&) = default;
 };
 
 namespace SymEngine
