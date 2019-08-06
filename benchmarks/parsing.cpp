a066e9ce261e0f67456bd878fe496ee876719901
Ondřej Čertík <ondrej@certik.us>
2019-08-06 14:20:45 -0600

    Simplify benchmark

--- benchmarks/parsing.cpp
+++ benchmarks/parsing.cpp
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 
     N = 5000;
     std::string text;
-    std::string t0 = "(x + y - sin(x)/(z**2-4) - x**(y**z))";
+    std::string t0 = "(x + y - x/(z**2-4) - x**(y**z))";
     text = t0;
     for (int i = 0; i < N; i++) {
         text = text + " * " + t0;
@@ -39,35 +39,6 @@ int main(int argc, char *argv[])
                      .count()
               << "ms" << std::endl;
     std::cout << *a << std::endl;
-
-    t1 = std::chrono::high_resolution_clock::now();
-    a = parse_old(text);
-    t2 = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1)
-                     .count()
-              << "ms" << std::endl;
-    std::cout << *a << std::endl;
-
-    /* ------------------------------------------------- */
-
-    N = 3000;
-    t1 = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < N; i++) {
-        a = parse(t0);
-    }
-    t2 = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1)
-                     .count()
-              << "ms" << std::endl;
-
-    t1 = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < N; i++) {
-        a = parse_old(t0);
-    }
-    t2 = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1)
-                     .count()
-              << "ms" << std::endl;
 
     return 0;
 }
