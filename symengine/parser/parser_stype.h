a15c65e38d534c068eb71b6af174efee8dc3642c
Ondřej Čertík <ondrej@certik.us>
2019-08-06 15:26:12 -0600

    Improve

--- symengine/parser/parser_stype.h
+++ symengine/parser/parser_stype.h
@@ -83,31 +83,55 @@ typedef struct Node *PNode;
 struct Node {
     NodeType type;
     union {
-        struct { PNode left; PNode right; } Add;
-        struct { PNode left; PNode right; } Sub;
-        struct { PNode left; PNode right; } Mul;
-        struct { PNode left; PNode right; } Div;
-        struct { PNode base; PNode exp; } Pow;
-        //struct { std::string name; } Symbol;
-        //struct { std::string i; } Integer;
+        struct { PNode left; PNode right; } binop;
+        struct { PNode base; PNode exp; } pow;
+        struct { char *name; } symbol;
+        struct { char *i; } integer;
     } d;
 };
 
-static struct Node* make_node(NodeType type) {
-    struct Node *n;
+static struct Node* make_binop(NodeType type, PNode x, PNode y) {
+    PNode n;
     n = new Node;
     n->type = type;
+    n->d.binop.left = x;
+    n->d.binop.right = y;
+    return n;
+}
+
+static struct Node* make_pow(PNode x, PNode y) {
+    PNode n;
+    n = new Node;
+    n->type = NodeType::Pow;
+    n->d.pow.base = x;
+    n->d.pow.exp = y;
+    return n;
+}
+
+static struct Node* make_symbol(std::string s) {
+    PNode n;
+    n = new Node;
+    n->type = NodeType::Symbol;
+    n->d.symbol.name = &s[0];
+    return n;
+}
+
+static struct Node* make_integer(std::string s) {
+    PNode n;
+    n = new Node;
+    n->type = NodeType::Integer;
+    n->d.integer.i = &s[0];
     return n;
 }
 
 #define TYPE PNode
-#define ADD(x, y) make_node(NodeType::Add)
-#define SUB(x, y) make_node(NodeType::Sub)
-#define MUL(x, y) make_node(NodeType::Mul)
-#define DIV(x, y) make_node(NodeType::Div)
-#define POW(x, y) make_node(NodeType::Pow)
-#define SYMBOL(x) make_node(NodeType::Symbol)
-#define INTEGER(x) make_node(NodeType::Integer)
+#define ADD(x, y) make_binop(NodeType::Add, x, y)
+#define SUB(x, y) make_binop(NodeType::Sub, x, y)
+#define MUL(x, y) make_binop(NodeType::Mul, x, y)
+#define DIV(x, y) make_binop(NodeType::Div, x, y)
+#define POW(x, y) make_pow(x, y)
+#define SYMBOL(x) make_symbol(x)
+#define INTEGER(x) make_integer(x)
 #define PRINT(x) std::cout << x->type << std::endl
 
 
