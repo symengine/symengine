3d99bec626c9bb5d9db7c7ed23dc83d414180d88
Ondřej Čertík <ondrej@certik.us>
2019-08-06 14:58:34 -0600

    Works

--- symengine/parser/parser_stype.h
+++ symengine/parser/parser_stype.h
@@ -3,13 +3,44 @@
 
 #include <string>
 #include "symengine/basic.h"
+#include "symengine/add.h"
+#include "symengine/pow.h"
+
+/*
+using SymEngine::add;
+using SymEngine::sub;
+using SymEngine::mul;
+using SymEngine::div;
+using SymEngine::pow;
+using SymEngine::symbol;
+using SymEngine::integer;
+
+#define TYPE SymEngine::RCP<const SymEngine::Basic>
+#define ADD(x, y) add(x, y)
+#define SUB(x, y) sub(x, y)
+#define MUL(x, y) mul(x, y)
+#define DIV(x, y) div(x, y)
+#define POW(x, y) pow(x, y)
+#define SYMBOL(x) symbol(x)
+#define INTEGER(x) integer(x)
+#define PRINT(x) std::cout << *x << std::endl
+*/
+
+#define TYPE int
+#define ADD(x, y) x+y
+#define SUB(x, y) x-y
+#define MUL(x, y) x*y
+#define DIV(x, y) x/y
+#define POW(x, y) std::pow(x,y)
+#define SYMBOL(x) 1
+#define INTEGER(x) std::stoi(x)
+#define PRINT(x) std::cout << x << std::endl
 
 namespace SymEngine
 {
 
 struct YYSTYPE {
-    //SymEngine::RCP<const SymEngine::Basic> basic;
-    int basic;
+    TYPE basic;
     std::string string;
     // Constructor
     YYSTYPE() = default;
@@ -25,34 +56,6 @@ struct YYSTYPE {
 
 } // namespace SymEngine
 
-/*
-#include "symengine/add.h"
-#include "symengine/pow.h"
 
-using SymEngine::add;
-using SymEngine::sub;
-using SymEngine::mul;
-using SymEngine::div;
-using SymEngine::pow;
-using SymEngine::symbol;
-using SymEngine::integer;
-
-#define ADD(x, y) add(x, y)
-#define SUB(x, y) sub(x, y)
-#define MUL(x, y) mul(x, y)
-#define DIV(x, y) div(x, y)
-#define POW(x, y) pow(x, y)
-#define SYMBOL(x) symbol(x)
-#define INTEGER(x) integer(x)
-*/
-
-#define ADD(x, y) x+y
-#define SUB(x, y) x-y
-#define MUL(x, y) x*y
-#define DIV(x, y) x/y
-#define POW(x, y) std::pow(x,y)
-#define SYMBOL(x) 1
-#define INTEGER(x) std::stoi(x)
-#define PRINT(x) std::cout << x << std::endl
 
 #endif
