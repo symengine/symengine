85669e45d07c5c6fa0afacf3050d8239d4f91465
jmig5776 <jmig5776@gmail.com>
2019-09-28 02:06:39 +0530

    Formated apart

--- symengine/polys/apart.cpp
+++ symengine/polys/apart.cpp
@@ -5,13 +5,12 @@ using SymEngine::Basic;
 
 namespace SymEngine
 {
-  RCP<const Basic> apart(const RCP<const Basic> &f)
-  {
-    if (is_a_Atom(*f))
-    {
-      return f;
+RCP<const Basic> apart(const RCP<const Basic> &f)
+{
+    if (is_a_Atom(*f)) {
+        return f;
     }
-    //work pending here
+    // work pending here
     return f;
-  }
+}
 }
