439cf947399969fdd8778c7f71baeba345c31aac
jmig5776 <jmig5776@gmail.com>
2019-10-02 02:06:37 +0530

    Formatted

--- symengine/polys/apart.cpp
+++ symengine/polys/apart.cpp
@@ -5,20 +5,18 @@ using SymEngine::RCP;
 
 namespace SymEngine
 {
-RCP<const Basic> apart(const RCP<const Basic> &self, bool full=false)
+RCP<const Basic> apart(const RCP<const Basic> &self, bool full = false)
 {
     umap_basic_num gens = _find_gens_poly(self);
 
-
-    if (gens.size() != 1)
-    {
-      //only considering univariate here
-      return self;
+    if (gens.size() != 1) {
+        // only considering univariate here
+        return self;
     }
     RCP<const Basic> var = gens.begin()->first;
 
     RCP<const UExprPoly> poly = from_basic<UExprPoly>(self, var);
-    std::cout<<poly->get_poly();
+    std::cout << poly->get_poly();
 
     return self;
 }
