903c2d015846c25391d07999547982c3fdb5b691
jmig5776 <jmig5776@gmail.com>
2019-10-02 12:07:53 +0530

    Extracting the function as numerator and denominator

--- symengine/polys/apart.cpp
+++ symengine/polys/apart.cpp
@@ -16,7 +16,14 @@ RCP<const Basic> apart(const RCP<const Basic> &self, bool full = false)
     RCP<const Basic> var = gens.begin()->first;
 
     RCP<const UExprPoly> poly = from_basic<UExprPoly>(self, var);
-    std::cout << poly->get_poly();
+    std::cout << poly->get_poly() << "\n";
+
+    //main algorithm
+    if(is_a_Atom(*self)) return self;
+
+    RCP<const Basic> self_num, self_denom;
+    as_numer_denom(self, outArg(self_num), outArg(self_denom));
+    std::cout << self_num->__str__() << "/" << self_denom->__str__();
 
     return self;
 }
