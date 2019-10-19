7d0a40eb13775751144404ec431910515e097b03
jmig5776 <jmig5776@gmail.com>
2019-10-19 18:36:19 +0530

    Trying to convert to UIntPolyFlint

--- symengine/polys/apart.cpp
+++ symengine/polys/apart.cpp
@@ -16,14 +16,28 @@ RCP<const Basic> apart(const RCP<const Basic> &self, bool full = false)
     RCP<const Basic> var = gens.begin()->first;
 
     RCP<const UExprPoly> poly = from_basic<UExprPoly>(self, var);
-    std::cout << poly->get_poly() << "\n";
+    std::cout << poly->__str__() << "\n";
 
     //main algorithm
     if(is_a_Atom(*self)) return self;
 
     RCP<const Basic> self_num, self_denom;
-    as_numer_denom(self, outArg(self_num), outArg(self_denom));
+    as_numer_denom(poly, outArg(self_num), outArg(self_denom));
     std::cout << self_num->__str__() << "/" << self_denom->__str__();
+
+    try{
+      RCP<const UIntPolyFlint> a = from_basic<UIntPolyFlint>(self_num, var);
+      RCP<const UIntPolyFlint> b = from_basic<UIntPolyFlint>(self_denom, var);
+      std::cout << a->__str__() << "/" << b->__str__();
+    }
+    catch(SymEngineException &e)
+    {
+      std::cout<<"No";
+    }
+
+
+    // UIntPolyFlint::container_type quotient, remainder;
+    // a->get_poly().divrem(quotient, remainder, b->get_poly());
 
     return self;
 }
