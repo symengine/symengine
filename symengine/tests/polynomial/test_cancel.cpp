bf4561362a4c776eaa0cba46b6e34327aa11f139
jmig5776 <jmig5776@gmail.com>
2019-10-28 15:06:35 +0530

    Created file test_cancel

---
+++ symengine/tests/polynomial/test_cancel.cpp
@@ -0,0 +1,27 @@
+#include "catch.hpp"
+#include <chrono>
+
+#include <symengine/mul.h>
+#include <symengine/add.h>
+#include <symengine/pow.h>
+#include <symengine/polys/cancel.cpp>
+#include <symengine/polys/uintpoly_flint.h>
+
+using SymEngine::cancel;
+using SymEngine::mul;
+using SymEngine::UIntPolyFlint;
+using SymEngine::RCP;
+
+using namespace SymEngine::literals;
+
+TEST_CASE("cancel", "[Basic]")
+{
+  RCP<const Basic> x = symbol("x");
+  RCP<const Basic> y = symbol("y");
+  RCP<const UIntPolyFlint> result_self, result_other;
+
+  cancel(mul(x, integer(2)), x, outArg(result_self), outArg(result_other));
+  REQUIRE(result_self->__str__() == "2");
+  REQUIRE(result_other->__str__() == "1");
+
+}
