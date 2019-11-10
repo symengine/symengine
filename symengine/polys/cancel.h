f0df895f59ed90b78633270e218642d6b234d4de
jmig5776 <jmig5776@gmail.com>
2019-11-10 18:15:49 +0530

    Added #ifdef for uintpolyflint

--- symengine/polys/cancel.h
+++ symengine/polys/cancel.h
@@ -1,6 +1,8 @@
+#ifdef SYMENGINE_UINTPOLY_FLINT_H
+
+#include <symengine/polys/uintpoly_flint.h>
 #include <symengine/basic.h>
 #include <symengine/polys/basic_conversions.h>
-#include <symengine/polys/uintpoly_flint.h>
 
 using SymEngine::RCP;
 using SymEngine::UIntPolyFlint;
@@ -14,3 +16,5 @@ void cancel(const RCP<const Basic> &numer, const RCP<const Basic> &denom,
             const Ptr<RCP<const UIntPolyFlint>> &result_denom,
             const Ptr<RCP<const UIntPolyFlint>> &common);
 }
+
+#endif // SYMENGINE_UINTPOLY_FLINT_H
