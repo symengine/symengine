0a725d349e192a425e68efaabd61e2e266c695b0
jmig5776 <jmig5776@gmail.com>
2019-11-11 17:54:06 +0530

    Fixed formatting

--- symengine/polys/cancel.h
+++ symengine/polys/cancel.h
@@ -1,4 +1,3 @@
-
 #ifndef SYMENGINE_CANCEL_H
 #define SYMENGINE_CANCEL_H
 
@@ -16,34 +15,31 @@ namespace SymEngine
 // Declaration of cancel function
 template <typename Poly>
 inline void cancel(const RCP<const Basic> &numer, const RCP<const Basic> &denom,
-            const Ptr<RCP<const Poly>> &result_numer,
-            const Ptr<RCP<const Poly>> &result_denom,
-            const Ptr<RCP<const Poly>> &common)
+                   const Ptr<RCP<const Poly>> &result_numer,
+                   const Ptr<RCP<const Poly>> &result_denom,
+                   const Ptr<RCP<const Poly>> &common)
 {
-  // Converting basic to Poly
-  umap_basic_num numer_gens = _find_gens_poly(numer);
-  umap_basic_num denom_gens = _find_gens_poly(denom);
+    // Converting basic to Poly
+    umap_basic_num numer_gens = _find_gens_poly(numer);
+    umap_basic_num denom_gens = _find_gens_poly(denom);
 
-  if (numer_gens.size() != 1 && denom_gens.size() != 1) {
-      // only considering univariate here
-      return;
-  }
-  RCP<const Basic> numer_var = numer_gens.begin()->first;
-  RCP<const Basic> denom_var = denom_gens.begin()->first;
+    if (numer_gens.size() != 1 && denom_gens.size() != 1) {
+        // only considering univariate here
+        return;
+    }
+    RCP<const Basic> numer_var = numer_gens.begin()->first;
+    RCP<const Basic> denom_var = denom_gens.begin()->first;
 
-  RCP<const Poly> numer_poly
-      = from_basic<Poly>(numer, numer_var);
-  RCP<const Poly> denom_poly
-      = from_basic<Poly>(denom, denom_var);
+    RCP<const Poly> numer_poly = from_basic<Poly>(numer, numer_var);
+    RCP<const Poly> denom_poly = from_basic<Poly>(denom, denom_var);
 
-  // Finding common factors of numer_poly and denom_poly
-  RCP<const Poly> gcd_poly = gcd_upoly(*numer_poly, *denom_poly);
+    // Finding common factors of numer_poly and denom_poly
+    RCP<const Poly> gcd_poly = gcd_upoly(*numer_poly, *denom_poly);
 
-  // Dividing both by common factors
-  divides_upoly(*gcd_poly, *numer_poly, outArg(*result_numer));
-  divides_upoly(*gcd_poly, *denom_poly, outArg(*result_denom));
-  *common = gcd_poly;
-
+    // Dividing both by common factors
+    divides_upoly(*gcd_poly, *numer_poly, outArg(*result_numer));
+    divides_upoly(*gcd_poly, *denom_poly, outArg(*result_denom));
+    *common = gcd_poly;
 }
 }
 #endif // HAVE_SYMENGINE_FLINT
