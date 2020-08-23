b416f7126b6f6f84a5aa596c9bef9908749cc7dd
Liam Keegan <liam@keegan.ch>
2020-08-23 09:27:12 +0200

    add longer timeout & retry to curl in travis: codecov.io can be flaky

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -168,7 +168,7 @@ ctest --output-on-failure
 
 if [[ "${WITH_COVERAGE}" == "yes" ]]; then
     echo "=== Collecting coverage data"
-    curl -L https://codecov.io/bash -o codecov.sh
+    curl --connect-timeout 10 --retry 5 -L https://codecov.io/bash -o codecov.sh
     bash codecov.sh -x $GCOV_EXECUTABLE 2>&1 | grep -v "has arcs to entry block" | grep -v "has arcs from exit block"
     exit 0;
 fi
