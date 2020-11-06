efa0c1ee7da6c593eb77b4d7cf3723305f27f22d
Isuru Fernando <isuruf@gmail.com>
2020-11-06 04:24:03 -0600

    test serialize

---
+++ symengine/tests/basic/test_serialize.cpp
@@ -0,0 +1,23 @@
+#include <symengine/serialize.h>
+#include <cereal/archives/binary.hpp>
+
+int main()
+{
+    SymEngine::RCP<const SymEngine::Basic> x = SymEngine::parse("y**x");
+    std::ostringstream oss;
+    cereal::BinaryOutputArchive boa{oss};
+    boa(x);
+    auto data = oss.str();
+    std::cout << data << std::endl;
+
+    std::istringstream iss(data);
+    SymEngine::RCP<const SymEngine::Basic> y;
+    {
+        cereal::BinaryInputArchive iar{iss};
+        iar(y);
+    }
+    std::cout << y->use_count() << std::endl;
+    // logic to save to string and load from string omitted since it already
+    // fails to compile
+    return 0;
+}
