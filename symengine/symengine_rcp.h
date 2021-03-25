01a3e0fc54663fbf4526aa7d9dc16fb61e31b4c5
Alec Edgington <alec.edgington@cambridgequantum.com>
2021-03-25 16:07:56 +0000

    Make operator== const too.

--- symengine/symengine_rcp.h
+++ symengine/symengine_rcp.h
@@ -169,7 +169,7 @@ public:
         return ptr_ == nullptr;
     }
     template <class T2>
-    bool operator==(const RCP<T2> &p2)
+    bool operator==(const RCP<T2> &p2) const
     {
         return ptr_ == p2.ptr_;
     }
