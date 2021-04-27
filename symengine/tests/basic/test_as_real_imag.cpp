a1486d193fb42f8edc93326585be59cce5ad0695
Isuru Fernando <isuruf@gmail.com>
2021-04-27 14:21:13 -0500

    use an expression that is not auto simplified

--- symengine/tests/basic/test_as_real_imag.cpp
+++ symengine/tests/basic/test_as_real_imag.cpp
@@ -120,7 +120,8 @@ TEST_CASE("RealImag: Pow", "[as_real_imag]")
     REQUIRE(eq(*re, *div(one, i2)));
     REQUIRE(eq(*im, *div(sqrt(integer(3)), i2)));
 
-    as_real_imag(pow(add(i2, I), 2), outArg(re), outArg(im));
+    as_real_imag(pow(sub(add(i2, sqrt(2)), sub(sqrt(2), I)), 2),
+            outArg(re), outArg(im));
     REQUIRE(eq(*re, *integer(3)));
     REQUIRE(eq(*im, *integer(4)));
 
