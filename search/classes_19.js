a4148baf6a9fdb9be68c7fe938bd1c63cd8655d5
rikardn <rikardn@users.noreply.github.com>
2023-02-19 15:53:49 +0000

    deploy: e4e73f6a8ab67d91566907584e64d5e0503bf7eb

--- search/classes_19.js
+++ search/classes_19.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['zerovisitor_0',['ZeroVisitor',['../classSymEngine_1_1ZeroVisitor.html',1,'SymEngine']]],
-  ['zeta_1',['Zeta',['../classSymEngine_1_1Zeta.html',1,'SymEngine']]],
-  ['zetta_2',['zetta',['http://en.cppreference.com/w/cpp/numeric/ratio/ratio.html',1,'std']]]
+  ['zeromatrix_0',['ZeroMatrix',['../classSymEngine_1_1ZeroMatrix.html',1,'SymEngine']]],
+  ['zerovisitor_1',['ZeroVisitor',['../classSymEngine_1_1ZeroVisitor.html',1,'SymEngine']]],
+  ['zeta_2',['Zeta',['../classSymEngine_1_1Zeta.html',1,'SymEngine']]],
+  ['zetta_3',['zetta',['http://en.cppreference.com/w/cpp/numeric/ratio/ratio.html',1,'std']]]
 ];
--- search/classes_7.js
+++ search/classes_7.js
@@ -1,12 +1,13 @@
 var searchData=
 [
-  ['has_5fvirtual_5fdestructor_0',['has_virtual_destructor',['http://en.cppreference.com/w/cpp/types/has_virtual_destructor.html',1,'std']]],
-  ['hash_1',['hash',['http://en.cppreference.com/w/cpp/utility/hash.html',1,'std']]],
-  ['hash_3c_20symengine_3a_3abasic_20_3e_2',['hash&lt; SymEngine::Basic &gt;',['../structstd_1_1hash_3_01SymEngine_1_1Basic_01_4.html',1,'std']]],
-  ['hassymbolvisitor_3',['HasSymbolVisitor',['../classSymEngine_1_1HasSymbolVisitor.html',1,'SymEngine']]],
-  ['hecto_4',['hecto',['http://en.cppreference.com/w/cpp/numeric/ratio/ratio.html',1,'std']]],
-  ['high_5fresolution_5fclock_5',['high_resolution_clock',['http://en.cppreference.com/w/cpp/chrono/high_resolution_clock.html',1,'std::chrono']]],
-  ['hours_6',['hours',['http://en.cppreference.com/w/cpp/chrono/duration.html',1,'std::chrono']]],
-  ['hyperbolicbase_7',['HyperbolicBase',['../classSymEngine_1_1HyperbolicBase.html',1,'SymEngine']]],
-  ['hyperbolicfunction_8',['HyperbolicFunction',['../classSymEngine_1_1HyperbolicFunction.html',1,'SymEngine']]]
+  ['hadamardproduct_0',['HadamardProduct',['../classSymEngine_1_1HadamardProduct.html',1,'SymEngine']]],
+  ['has_5fvirtual_5fdestructor_1',['has_virtual_destructor',['http://en.cppreference.com/w/cpp/types/has_virtual_destructor.html',1,'std']]],
+  ['hash_2',['hash',['http://en.cppreference.com/w/cpp/utility/hash.html',1,'std']]],
+  ['hash_3c_20symengine_3a_3abasic_20_3e_3',['hash&lt; SymEngine::Basic &gt;',['../structstd_1_1hash_3_01SymEngine_1_1Basic_01_4.html',1,'std']]],
+  ['hassymbolvisitor_4',['HasSymbolVisitor',['../classSymEngine_1_1HasSymbolVisitor.html',1,'SymEngine']]],
+  ['hecto_5',['hecto',['http://en.cppreference.com/w/cpp/numeric/ratio/ratio.html',1,'std']]],
+  ['high_5fresolution_5fclock_6',['high_resolution_clock',['http://en.cppreference.com/w/cpp/chrono/high_resolution_clock.html',1,'std::chrono']]],
+  ['hours_7',['hours',['http://en.cppreference.com/w/cpp/chrono/duration.html',1,'std::chrono']]],
+  ['hyperbolicbase_8',['HyperbolicBase',['../classSymEngine_1_1HyperbolicBase.html',1,'SymEngine']]],
+  ['hyperbolicfunction_9',['HyperbolicFunction',['../classSymEngine_1_1HyperbolicFunction.html',1,'SymEngine']]]
 ];
